9601f7a3ab3a6d2bc3f5ea9897d4f92bf2afbc63
Tatu Saloranta <tsaloranta@gmail.com>
2012-01-30 18:53:38 -0800

    Fix [JACKSON-773]

--- src/main/java/com/fasterxml/jackson/databind/ser/impl/SimpleFilterProvider.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/impl/SimpleFilterProvider.java
@@ -37,14 +37,14 @@ public class SimpleFilterProvider extends FilterProvider
      */
     
     public SimpleFilterProvider() {
-        _filtersById = new HashMap<String,BeanPropertyFilter>();
+        this(new HashMap<String,BeanPropertyFilter>());
     }
 
     /**
      * @param mapping Mapping from id to filter; used as is, no copy is made.
      */
     public SimpleFilterProvider(Map<String,BeanPropertyFilter> mapping) {
-        _filtersById = new HashMap<String,BeanPropertyFilter>();
+        _filtersById = mapping;
     }
     
     /**

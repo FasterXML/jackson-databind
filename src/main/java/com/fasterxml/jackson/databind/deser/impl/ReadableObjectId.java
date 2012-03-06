fc21370eefec8210fd2902e3cbbf4ea13210f44c
Tatu Saloranta <tsaloranta@gmail.com>
2012-03-05 22:36:54 -0800

    ...

--- src/main/java/com/fasterxml/jackson/databind/deser/impl/ReadableObjectId.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/impl/ReadableObjectId.java
@@ -1,35 +1,20 @@
 package com.fasterxml.jackson.databind.deser.impl;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-
-import com.fasterxml.jackson.databind.deser.SettableBeanProperty;
 
 /**
  * Simple value container for containing information about single
- * Object Id during deserialization
+ * Object Id during deserialization.
  */
 public class ReadableObjectId
 {
     public final Object id;
     
     public Object item;
-
-    private LinkedList<Referring> _referringProperties;
     
     public ReadableObjectId(Object id)
     {
         this.id = id;
-    }
-
-    public void appendReferring(Object pojo, SettableBeanProperty prop)
-    {
-        if (_referringProperties == null) {
-            _referringProperties = new LinkedList<Referring>();
-        }
-        _referringProperties.add(new Referring(pojo, prop));
     }
 
     /**
@@ -42,44 +27,5 @@ public class ReadableObjectId
             throw new IllegalStateException("Already had POJO for id ("+id.getClass().getName()+") ["+id+"]");
         }
         item = ob;
-        if (_referringProperties != null) {
-            Iterator<Referring> it = _referringProperties.iterator();
-            _referringProperties = null;
-            while (it.hasNext()) {
-                Referring ref = it.next();
-                ref.property.set(ref.pojo, ob);
-            }
-        }
-    }
-    
-    public boolean hasReferringProperties() {
-        return (_referringProperties != null) && !_referringProperties.isEmpty();
-    }
-
-    public Iterator<Referring> referringProperties()
-    {
-        if (_referringProperties == null) {
-            return Collections.<Referring>emptyList().iterator();
-        }
-        return _referringProperties.iterator();
-    }
-
-    /*
-    /**********************************************************
-    /* Helper classes
-    /**********************************************************
-     */
-
-    public final static class Referring
-    {
-        public final SettableBeanProperty property;
-        
-        public final Object pojo;
-
-        public Referring(Object ob, SettableBeanProperty prop)
-        {
-            property = prop;
-            pojo = ob;
-        }
     }
 }

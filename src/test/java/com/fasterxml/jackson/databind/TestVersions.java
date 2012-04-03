8683a0ad41cf4faa364e56a499939dd170cc25f3
Tatu Saloranta <tsaloranta@gmail.com>
2012-04-02 21:28:48 -0700

    fix a unit test

--- src/test/java/com/fasterxml/jackson/databind/TestVersions.java
+++ src/test/java/com/fasterxml/jackson/databind/TestVersions.java
@@ -41,7 +41,7 @@ public class TestVersions extends com.fasterxml.jackson.test.BaseTest
         assertEquals(MAJOR_VERSION, v.getMajorVersion());
         assertEquals(MINOR_VERSION, v.getMinorVersion());
         // Check patch level initially, comment out for maint versions
-        assertEquals(0, v.getPatchLevel());
+//        assertEquals(0, v.getPatchLevel());
         assertEquals(GROUP_ID, v.getGroupId());
         assertEquals(ARTIFACT_ID, v.getArtifactId());
     }

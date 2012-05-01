89644045bd3d729e1c1fd86d7c5f47ea8ed1aeb9
Tatu Saloranta <tsaloranta@gmail.com>
2012-04-30 19:16:07 -0700

    ...

--- src/test/java/com/fasterxml/jackson/databind/node/TestMissingNode.java
+++ src/test/java/com/fasterxml/jackson/databind/node/TestMissingNode.java
@@ -7,6 +7,7 @@ public class TestMissingNode extends NodeTestBase
     public void testMissing()
     {
         MissingNode n = MissingNode.getInstance();
+        assertTrue(n.isMissingNode());
         assertEquals(JsonToken.NOT_AVAILABLE, n.asToken());
         // as per [JACKSON-775]
         assertEquals("", n.asText());
@@ -17,6 +18,11 @@ public class TestMissingNode extends NodeTestBase
          * that default values are served.
          */
         assertNodeNumbersForNonNumeric(n);
-    }
 
+        // [JACKSON-823]
+        assertTrue(n.asBoolean(true));
+        assertEquals(4, n.asInt(4));
+        assertEquals(5L, n.asLong(5));
+        assertEquals(0.25, n.asDouble(0.25));
+    }
 }

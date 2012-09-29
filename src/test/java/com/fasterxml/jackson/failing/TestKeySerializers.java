b3112cd77e0b904bf61fec29be30e514d3964ed4
Tatu Saloranta <tsaloranta@gmail.com>
2012-09-28 20:09:06 -0700

    add tentative test for [Issue#75]

---
+++ src/test/java/com/fasterxml/jackson/failing/TestKeySerializers.java
@@ -0,0 +1,63 @@
+package com.fasterxml.jackson.failing;
+
+import java.io.IOException;
+import java.util.*;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+public class TestKeySerializers extends BaseMapTest
+{
+    public static class KarlSerializer extends JsonSerializer<String>
+    {
+        @Override
+        public void serialize(String value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeFieldName("Karl");
+        }
+    }
+
+    public static class NotKarlBean
+    {
+        public Map<String,Integer> map = new HashMap<String,Integer>();
+        {
+            map.put("Not Karl", 1);
+        }
+    }
+
+    public static class KarlBean
+    {
+        @JsonSerialize(keyUsing = KarlSerializer.class)
+        public Map<String,Integer> map = new HashMap<String,Integer>();
+        {
+            map.put("Karl", 1);
+        }
+    }
+
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+    
+    public void testNotKarl() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final String serialized = mapper.writeValueAsString(new NotKarlBean());
+        assertEquals("{\"map\":{\"Not Karl\":1}}", serialized);
+    }
+
+    public void testKarl() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final String serialized = mapper.writeValueAsString(new KarlBean());
+        assertEquals("{\"map\":{\"Karl\":1}}", serialized);
+    }
+
+    // [Issue#75]: caching of KeySerializers
+    public void testBoth() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final String value1 = mapper.writeValueAsString(new NotKarlBean());
+        assertEquals("{\"map\":{\"Not Karl\":1}}", value1);
+        final String value2 = mapper.writeValueAsString(new KarlBean());
+        assertEquals("{\"map\":{\"Karl\":1}}", value2);
+    }
+}

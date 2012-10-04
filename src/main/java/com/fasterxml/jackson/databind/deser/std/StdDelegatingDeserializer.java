06a23ce6d096ec9a803a21dc23690ebdb47da93a
Tatu Saloranta <tsaloranta@salesforce.com>
2012-10-03 17:18:47 -0700

    complete [Issue#87], standard delegating serializer, deserializer impls

---
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/StdDelegatingDeserializer.java
@@ -0,0 +1,143 @@
+package com.fasterxml.jackson.databind.deser.std;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
+
+/**
+ * Deserializer implementation where given Java type is first deserialized
+ * by a standard Jackson deserializer into a delegate type; and then
+ * this delegate type is converted using a configured
+ * {@link Converter} into desired target type.
+ * Common delegate types to use are {@link java.util.Map}
+ * and {@link com.fasterxml.jackson.databind.JsonNode}.
+ *<p>
+ * Note that although types (delegate, target) may be related, they must not be same; trying
+ * to do this will result in an exception.
+ * 
+ * @param <T> Target type to convert to, from delegate type
+ * 
+ * @since 2.1
+ */
+public class StdDelegatingDeserializer<T>
+    extends StdDeserializer<T>
+    implements ContextualDeserializer
+{
+    protected final Converter<Object,T> _converter;
+    
+    /**
+     * Fully resolved delegate type, with generic information if any available.
+     */
+    protected final JavaType _delegateType;
+    
+    /**
+     * Underlying serializer for type <code>T<.code>.
+     */
+    protected final JsonDeserializer<Object> _delegateDeserializer;
+    
+    /*
+    /**********************************************************
+    /* Life-cycle
+    /**********************************************************
+     */
+
+    @SuppressWarnings("unchecked")
+    public StdDelegatingDeserializer(Converter<?,T> converter)
+    {
+        super(Object.class);
+        _converter = (Converter<Object,T>)converter;
+        _delegateType = null;
+        _delegateDeserializer = null;
+    }
+    
+    @SuppressWarnings("unchecked")
+    protected StdDelegatingDeserializer(Converter<Object,T> converter,
+            JavaType delegateType, JsonDeserializer<?> delegateDeserializer)
+    {
+        super(delegateType);
+        _converter = converter;
+        _delegateType = delegateType;
+        _delegateDeserializer = (JsonDeserializer<Object>) delegateDeserializer;
+    }
+
+    /**
+     * Method used for creating resolved contextual instances. Must be
+     * overridden when sub-classing.
+     */
+    protected StdDelegatingDeserializer<T> withDelegate(Converter<Object,T> converter,
+            JavaType delegateType, JsonDeserializer<?> delegateDeserializer)
+    {
+        if (getClass() != StdDelegatingDeserializer.class) {
+            throw new IllegalStateException("Sub-class "+getClass().getName()+" must override 'withDelegate'");
+        }
+        return new StdDelegatingDeserializer<T>(converter, delegateType, delegateDeserializer);
+    }
+
+    /*
+    /**********************************************************
+    /* Contextualization
+    /**********************************************************
+     */
+    
+
+    public JsonDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property)
+        throws JsonMappingException
+    {
+        // First: figure out what is the fully generic delegate type:
+        TypeFactory tf = ctxt.getTypeFactory();
+        JavaType implType = tf.constructType(_converter.getClass());
+        JavaType[] params = tf.findTypeParameters(implType, Converter.class);
+        if (params == null || params.length != 2) {
+            throw new JsonMappingException("Could not determine Converter parameterization for "
+                    +implType);
+        }
+        // and then we can find serializer to delegate to, construct a new instance:
+        JavaType delegateType = params[0];
+        return withDelegate(_converter, delegateType,
+                ctxt.findContextualValueDeserializer(delegateType, property));
+    }
+
+    /*
+    /**********************************************************
+    /* Serialization
+    /**********************************************************
+     */
+    
+    @Override
+    public T deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
+        Object delegateValue = _delegateDeserializer.deserialize(jp, ctxt);
+        return (delegateValue == null) ? null : _converter.convert(delegateValue);
+    }
+
+    @Override
+    public Object deserializeWithType(JsonParser jp, DeserializationContext ctxt,
+            TypeDeserializer typeDeserializer)
+        throws IOException, JsonProcessingException
+    {
+        /* 03-Oct-2012, tatu: This is actually unlikely to work ok... but for now,
+         *    let's give it a chance?
+         */
+        Object delegateValue = _delegateDeserializer.deserializeWithType(jp, ctxt,
+                typeDeserializer);
+        return (delegateValue == null) ? null : _converter.convert(delegateValue);
+    }
+
+    /*
+    /**********************************************************
+    /* Serialization
+    /**********************************************************
+     */
+
+    @Override
+    public JsonDeserializer<?> getDelegatee() {
+        return _delegateDeserializer;
+    }
+}
---
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestCustomDeserializers.java
@@ -0,0 +1,173 @@
+package com.fasterxml.jackson.databind.deser;
+
+import java.io.*;
+import java.util.*;
+
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDelegatingDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.util.Converter;
+
+/**
+ * Test to check that customizations work as expected.
+ */
+public class TestCustomDeserializers
+    extends BaseMapTest
+{
+    /*
+    /**********************************************************
+    /* Helper classes
+    /**********************************************************
+     */
+
+    static class DummyDeserializer<T>
+        extends StdDeserializer<T>
+    {
+        final T value;
+
+        public DummyDeserializer(T v, Class<T> cls) {
+            super(cls);
+            value = v;
+        }
+
+        @Override
+        public T deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException
+        {
+            // need to skip, if structured...
+            jp.skipChildren();
+            return value;
+        }
+    }
+
+    static class TestBeans {
+        public List<TestBean> beans;
+    }
+    static class TestBean {
+        public CustomBean c;
+        public String d;
+    }
+    @JsonDeserialize(using=CustomBeanDeserializer.class)
+    static class CustomBean {
+        protected final int a, b;
+        public CustomBean(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    static class CustomBeanDeserializer extends JsonDeserializer<CustomBean>
+    {
+        @Override
+        public CustomBean deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException
+        {
+            int a = 0, b = 0;
+            JsonToken t = jp.getCurrentToken();
+            if (t == JsonToken.START_OBJECT) {
+                t = jp.nextToken();
+            } else if (t != JsonToken.FIELD_NAME) {
+                throw new Error();
+            }
+            while(t == JsonToken.FIELD_NAME) {
+                final String fieldName = jp.getCurrentName();
+                t = jp.nextToken();
+                if (t != JsonToken.VALUE_NUMBER_INT) {
+                    throw new JsonParseException("expecting number got "+ t, jp.getCurrentLocation());
+                }
+                if (fieldName.equals("a")) {
+                    a = jp.getIntValue();
+                } else if (fieldName.equals("b")) {
+                    b = jp.getIntValue();
+                } else {
+                    throw new Error();
+                }
+                t = jp.nextToken();
+            }
+            return new CustomBean(a, b);
+        }
+    }
+
+    public static class Immutable {
+        protected int x, y;
+        
+        public Immutable(int x0, int y0) {
+            x = x0;
+            y = y0;
+        }
+    }
+    
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+
+    public void testCustomBeanDeserializer() throws Exception
+    {
+
+        final ObjectMapper map = new ObjectMapper();
+        String json = "{\"beans\":[{\"c\":{\"a\":10,\"b\":20},\"d\":\"hello, tatu\"}]}";
+        TestBeans beans = map.readValue(json, TestBeans.class);
+
+        assertNotNull(beans);
+        List<TestBean> results = beans.beans;
+        assertNotNull(results);
+        assertEquals(1, results.size());
+        TestBean bean = results.get(0);
+        assertEquals("hello, tatu", bean.d);
+        CustomBean c = bean.c;
+        assertNotNull(c);
+        assertEquals(10, c.a);
+        assertEquals(20, c.b);
+
+        json = "{\"beans\":[{\"c\":{\"b\":3,\"a\":-4},\"d\":\"\"},"
+            +"{\"d\":\"abc\", \"c\":{\"b\":15}}]}";
+        beans = map.readValue(json, TestBeans.class);
+
+        assertNotNull(beans);
+        results = beans.beans;
+        assertNotNull(results);
+        assertEquals(2, results.size());
+
+        bean = results.get(0);
+        assertEquals("", bean.d);
+        c = bean.c;
+        assertNotNull(c);
+        assertEquals(-4, c.a);
+        assertEquals(3, c.b);
+
+        bean = results.get(1);
+        assertEquals("abc", bean.d);
+        c = bean.c;
+        assertNotNull(c);
+        assertEquals(0, c.a);
+        assertEquals(15, c.b);
+    }
+
+    // [Issue#87]: delegating deserializer
+    public void testDelegating() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(Immutable.class,
+            new StdDelegatingDeserializer<Immutable>(
+                new Converter<JsonNode, Immutable>() {
+                    //@Override
+                    public Immutable convert(JsonNode value)
+                    {
+                        int x = value.path("x").asInt();
+                        int y = value.path("y").asInt();
+                        return new Immutable(x, y);
+                    }
+                }
+                ));
+
+        mapper.registerModule(module);
+        Immutable imm = mapper.readValue("{\"x\":3,\"y\":7}", Immutable.class);
+        assertEquals(3, imm.x);
+        assertEquals(7, imm.y);
+    }
+}

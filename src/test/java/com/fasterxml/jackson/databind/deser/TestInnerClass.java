f1332ae019103968e839eb185bc9a8cb49c64f53
Tatu Saloranta <tsaloranta@gmail.com>
2012-10-26 18:23:06 -0700

    move failing test

--- src/test/java/com/fasterxml/jackson/databind/deser/TestInnerClass.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestInnerClass.java
@@ -1,8 +1,5 @@
 package com.fasterxml.jackson.databind.deser;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.databind.*;
 
 public class TestInnerClass extends BaseMapTest
@@ -48,38 +45,5 @@ public class TestInnerClass extends BaseMapTest
         assertEquals("Smurf", output.brain.parentName());
         output.name = "Foo";
         assertEquals("Foo", output.brain.parentName());
-    }
-
-    // core/[Issue#32]
-    public void testInnerList() throws Exception
-    {
-        Dog2 dog = new Dog2();
-        dog.name = "Spike";
-        dog.legs = new ArrayList<Dog2.Leg>();
-        dog.legs.add(dog.new Leg());
-        dog.legs.add(dog.new Leg());
-        dog.legs.get(0).length = 5;
-        dog.legs.get(1).length = 4;
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        String dogJson = mapper.writeValueAsString(dog);
-        System.out.println(dogJson);
-      // output: {"name":"Spike","legs":[{length: 5}, {length: 4}]}
-
-        // currently throws JsonMappingException
-        Dog2 dogCopy = mapper.readValue(dogJson, Dog2.class);
-        assertEquals(dogCopy.legs.get(1).length, 4);
-        // prefer fully populated Dog instance
-    }
-
-    public static class Dog2
-    {
-      public String name;
-      public List<Leg> legs;
-
-      public class Leg {
-        public int length;
-      }
     }
 }

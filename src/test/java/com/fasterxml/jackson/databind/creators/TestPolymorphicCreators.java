2a13a9f6cc48b329365b9346fd9733c44483934e
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-17 11:34:14 -0800

    more test fixes

--- src/test/java/com/fasterxml/jackson/databind/creators/TestPolymorphicCreators.java
+++ src/test/java/com/fasterxml/jackson/databind/creators/TestPolymorphicCreators.java
@@ -19,18 +19,18 @@ public class TestPolymorphicCreators
 
     static class Animal
     {
-	// All animals have names, for our demo purposes...
-	public String name;
+        // All animals have names, for our demo purposes...
+        public String name;
 
-	protected Animal() { }
+        protected Animal() { }
 
-	/**
-	 * Creator method that can instantiate instances of
-	 * appropriate polymoprphic type
-	 */
+        /**
+         * Creator method that can instantiate instances of
+         * appropriate polymoprphic type
+         */
         @JsonCreator
-	public static Animal create(@JsonProperty("type") String type)
-	{
+        public static Animal create(@JsonProperty("type") String type)
+        {
 	    if ("dog".equals(type)) {
 		return new Dog();
 	    }
@@ -38,22 +38,22 @@ public class TestPolymorphicCreators
 		return new Cat();
 	    }
 	    throw new IllegalArgumentException("No such animal type ('"+type+"')");
-	}
+        }
     }
 
     static class Dog extends Animal
     {
-	double barkVolume; // in decibels
-	public Dog() { }
-	public void setBarkVolume(double v) { barkVolume = v; }
+        double barkVolume; // in decibels
+        public Dog() { }
+        public void setBarkVolume(double v) { barkVolume = v; }
     }
 
     static class Cat extends Animal
     {
-	boolean likesCream;
-	public int lives;
-	public Cat() { }
-	public void setLikesCream(boolean likesCreamSurely) { likesCream = likesCreamSurely; }
+        boolean likesCream;
+        public int lives;
+        public Cat() { }
+        public void setLikesCream(boolean likesCreamSurely) { likesCream = likesCreamSurely; }
     }
 
     abstract static class AbstractRoot
@@ -81,69 +81,67 @@ public class TestPolymorphicCreators
     }
 
     static final class One extends AbstractRoot {
-            private One(String opt) {
-                    super(opt);
-            }
+        private One(String opt) {
+            super(opt);
+        }
 
-            @Override public int getWhich() {
-                    return 1;
-            }
+        @Override public int getWhich() {
+            return 1;
+        }
     }
     
     /*
-    **********************************************
-    * Actual tests
-    **********************************************
+    /**********************************************************
+    /* Actual tests
+    /**********************************************************
      */
 
+    private final ObjectMapper MAPPER = new ObjectMapper();
+    
     /**
      * Simple test to verify that it is possible to implement polymorphic
      * deserialization manually.
      */
     public void testManualPolymorphicDog() throws Exception
     {
-	ObjectMapper mapper = new ObjectMapper();
-	// first, a dog, start with type
-	Animal animal = mapper.readValue("{ \"type\":\"dog\", \"name\":\"Fido\", \"barkVolume\" : 95.0 }", Animal.class);
-	assertEquals(Dog.class, animal.getClass());
-	assertEquals("Fido", animal.name);
-	assertEquals(95.0, ((Dog) animal).barkVolume);
+        // first, a dog, start with type
+        Animal animal = MAPPER.readValue("{ \"type\":\"dog\", \"name\":\"Fido\", \"barkVolume\" : 95.0 }", Animal.class);
+        assertEquals(Dog.class, animal.getClass());
+        assertEquals("Fido", animal.name);
+        assertEquals(95.0, ((Dog) animal).barkVolume);
     }
 
     public void testManualPolymorphicCatBasic() throws Exception
     {
-	ObjectMapper mapper = new ObjectMapper();
-	// and finally, lactose-intolerant, but otherwise robust super-cat:
-	Animal animal = mapper.readValue("{ \"name\" : \"Macavity\", \"type\":\"cat\", \"lives\":18, \"likesCream\":false }", Animal.class);
-	assertEquals(Cat.class, animal.getClass());
-	assertEquals("Macavity", animal.name); // ... there's no one like Macavity!
-	Cat cat = (Cat) animal;
-	assertEquals(18, cat.lives);
-	// ok, he can't drink dairy products. Let's verify:
-	assertEquals(false, cat.likesCream);
+        // and finally, lactose-intolerant, but otherwise robust super-cat:
+        Animal animal = MAPPER.readValue("{ \"name\" : \"Macavity\", \"type\":\"cat\", \"lives\":18, \"likesCream\":false }", Animal.class);
+        assertEquals(Cat.class, animal.getClass());
+        assertEquals("Macavity", animal.name); // ... there's no one like Macavity!
+        Cat cat = (Cat) animal;
+        assertEquals(18, cat.lives);
+        // ok, he can't drink dairy products. Let's verify:
+        assertEquals(false, cat.likesCream);
     }
 
     public void testManualPolymorphicCatWithReorder() throws Exception
     {
-	ObjectMapper mapper = new ObjectMapper();
-	// Then cat; shuffle order to mandate buffering
-	Animal animal = mapper.readValue("{ \"likesCream\":true, \"name\" : \"Venla\", \"type\":\"cat\" }", Animal.class);
-	assertEquals(Cat.class, animal.getClass());
-	assertEquals("Venla", animal.name);
-	// bah, of course cats like cream. But let's ensure Jackson won't mess with laws of nature!
-	assertTrue(((Cat) animal).likesCream);
+        // Then cat; shuffle order to mandate buffering
+        Animal animal = MAPPER.readValue("{ \"likesCream\":true, \"name\" : \"Venla\", \"type\":\"cat\" }", Animal.class);
+        assertEquals(Cat.class, animal.getClass());
+        assertEquals("Venla", animal.name);
+        // bah, of course cats like cream. But let's ensure Jackson won't mess with laws of nature!
+        assertTrue(((Cat) animal).likesCream);
     }
 
     public void testManualPolymorphicWithNumbered() throws Exception
     {
-         final ObjectMapper m = new ObjectMapper();
-         final ObjectWriter w = m.writerWithType(AbstractRoot.class);
-         final ObjectReader r = m.reader(AbstractRoot.class);
+         final ObjectWriter w = MAPPER.writerWithType(AbstractRoot.class);
+         final ObjectReader r = MAPPER.reader(AbstractRoot.class);
 
-        AbstractRoot input = AbstractRoot.make(1, "oh hai!");
-        String json = w.writeValueAsString(input);
-        AbstractRoot result = r.readValue(json);
-        assertNotNull(result);
-        assertEquals("oh hai!", result.getOpt());
+         AbstractRoot input = AbstractRoot.make(1, "oh hai!");
+         String json = w.writeValueAsString(input);
+         AbstractRoot result = r.readValue(json);
+         assertNotNull(result);
+         assertEquals("oh hai!", result.getOpt());
     }
 }

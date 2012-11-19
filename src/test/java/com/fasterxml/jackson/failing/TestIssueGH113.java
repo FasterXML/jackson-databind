7e1bee3df937175e64a5e2cc9ca751ee4a833e47
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-19 12:33:05 -0800

    Add a unit test for [Issue#113]

---
+++ src/test/java/com/fasterxml/jackson/failing/TestIssueGH113.java
@@ -0,0 +1,83 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.annotation.*;
+
+import com.fasterxml.jackson.databind.*;
+
+public class TestIssueGH113 extends BaseMapTest
+{
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "_class")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(Cat.class),
+            @JsonSubTypes.Type(Dog.class)
+    })
+    public static abstract class Animal {
+        public final static String ID = "id";
+
+        private String id;
+        public Animal() {
+        }
+
+        @JsonCreator
+        public Animal(@JsonProperty(ID) String id) {
+            this.id = id;
+        }
+
+        @JsonProperty(ID)
+        public String getId() {
+            return id;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Cat extends Animal {
+        public Cat() {
+            super();
+        }
+
+        @JsonCreator
+        public Cat(@JsonProperty(ID) String id) {
+            super(id);
+        }
+
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Dog extends Animal {
+        public Dog() {
+            super();
+        }
+
+        @JsonCreator
+        public Dog(@JsonProperty(ID) String id) {
+            super(id);
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AnimalWrapper {
+        public static final String ANIMAL = "animal";
+        private Animal animal;
+
+        @JsonCreator
+        public AnimalWrapper(@JsonProperty(ANIMAL) Animal animal) {
+            this.animal = animal;
+        }
+
+        @JsonProperty(ANIMAL)
+        public Animal getAnimal() {
+            return animal;
+        }
+    }
+
+    public void testSubtypes() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String id = "nice dogy";
+        String serializedDog = mapper.writeValueAsString(new AnimalWrapper(new Dog(id)));
+        AnimalWrapper wrapper = mapper.readValue(serializedDog, AnimalWrapper.class);
+        assertEquals(id, wrapper.getAnimal().getId());
+    }
+}

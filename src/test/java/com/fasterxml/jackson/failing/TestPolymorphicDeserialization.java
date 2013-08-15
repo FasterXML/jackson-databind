d1a3cb149f888418a8eb2b8cf8fafbd032658d25
Tatu Saloranta <tatu.saloranta@iki.fi>
2013-08-15 13:36:36 -0700

    Add a notes wrt #283

--- src/test/java/com/fasterxml/jackson/failing/TestPolymorphicDeserialization.java
+++ src/test/java/com/fasterxml/jackson/failing/TestPolymorphicDeserialization.java
@@ -8,59 +8,60 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
+ * Reproduction of [https://github.com/FasterXML/jackson-databind/issues/283],
+ * contributed by Eric T.
  */
 public class TestPolymorphicDeserialization extends BaseMapTest
 {
-  @Test
-  public void testName() throws Exception
-  {
-    ObjectMapper mapper = objectMapper();
-
-    Assert.assertEquals("A", mapper.readValue("{\"type\": \"a\"}", SomeInterface.class).get());
-    Assert.assertEquals("A", mapper.readValue("{}", SomeInterface.class).get());
-    Assert.assertEquals("B", mapper.readValue("{\"type\": \"b\"}", SomeInterface.class).get());
-    Assert.assertEquals("A", mapper.readValue("{\"type\": \"c\"}", SomeInterface.class).get());
-
-    Assert.assertEquals("A", mapper.readValue("{\"type\": \"a\"}", ClassA.class).get());
-    Assert.assertEquals("B", mapper.readValue("{\"type\": \"b\"}", ClassB.class).get());
-    Assert.assertEquals("C", mapper.readValue("{\"type\": \"c\"}", ClassC.class).get());
-  }
-
-  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = ClassA.class)
-  @JsonSubTypes({
-          @JsonSubTypes.Type(name = "a", value = ClassA.class),
-          @JsonSubTypes.Type(name = "b", value = ClassB.class)
-  })
-  public static interface SomeInterface
-  {
-    public String get();
-  }
-
-  public static class ClassA implements SomeInterface
-  {
-    @Override
-    public String get()
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = ClassA.class)
+    @JsonSubTypes({
+            @JsonSubTypes.Type(name = "a", value = ClassA.class),
+            @JsonSubTypes.Type(name = "b", value = ClassB.class)
+    })
+    public static interface SomeInterface
     {
-      return "A";
+      public String get();
     }
-  }
 
-  public static class ClassB implements SomeInterface
-  {
-    @Override
-    public String get()
+    public static class ClassA implements SomeInterface
     {
-      return "B";
+      @Override
+      public String get()
+      {
+        return "A";
+      }
     }
-  }
 
-  public static class ClassC implements SomeInterface
-  {
-    @Override
-    public String get()
+    public static class ClassB implements SomeInterface
     {
-      return "C";
+      @Override
+      public String get()
+      {
+        return "B";
+      }
     }
-  }
 
+    public static class ClassC implements SomeInterface
+    {
+      @Override
+      public String get()
+      {
+        return "C";
+      }
+    }
+
+    @Test
+    public void testName() throws Exception
+    {
+      ObjectMapper mapper = objectMapper();
+
+      Assert.assertEquals("A", mapper.readValue("{\"type\": \"a\"}", SomeInterface.class).get());
+      Assert.assertEquals("A", mapper.readValue("{}", SomeInterface.class).get());
+      Assert.assertEquals("B", mapper.readValue("{\"type\": \"b\"}", SomeInterface.class).get());
+      Assert.assertEquals("A", mapper.readValue("{\"type\": \"c\"}", SomeInterface.class).get());
+
+      Assert.assertEquals("A", mapper.readValue("{\"type\": \"a\"}", ClassA.class).get());
+      Assert.assertEquals("B", mapper.readValue("{\"type\": \"b\"}", ClassB.class).get());
+      Assert.assertEquals("C", mapper.readValue("{\"type\": \"c\"}", ClassC.class).get());
+    }
 }

5f58a1bfa8c983b4cd99a2f92b2dc4d69ad44e39
Tatu Saloranta <tatu.saloranta@iki.fi>
2013-10-22 19:55:42 -0700

    Add more consistency checks for type id generation

--- src/main/java/com/fasterxml/jackson/databind/jsontype/impl/TypeSerializerBase.java
+++ src/main/java/com/fasterxml/jackson/databind/jsontype/impl/TypeSerializerBase.java
@@ -33,10 +33,22 @@ public abstract class TypeSerializerBase extends TypeSerializer
      */
 
     protected String idFromValue(Object value) {
-        return _idResolver.idFromValue(value);
+        String id = _idResolver.idFromValue(value);
+        if (id == null) {
+            String typeDesc = (value == null) ? "NULL" : value.getClass().getName();
+            throw new IllegalArgumentException("Can not resolve type id for "
+                    +typeDesc+" (using "+_idResolver.getClass().getName()+")");
+        }
+        return id;
     }
 
     protected String idFromValueAndType(Object value, Class<?> type) {
-        return _idResolver.idFromValueAndType(value, type);
+        String id = _idResolver.idFromValueAndType(value, type);
+        if (id == null) {
+            String typeDesc = (value == null) ? "NULL" : value.getClass().getName();
+            throw new IllegalArgumentException("Can not resolve type id for "
+                    +typeDesc+" (using "+_idResolver.getClass().getName()+")");
+        }
+        return id;
     }
 }

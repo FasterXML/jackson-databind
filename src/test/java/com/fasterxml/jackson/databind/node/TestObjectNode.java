b9df1a64652f935413b2c2f750a918b0962deb38
Tatu Saloranta <tatu.saloranta@iki.fi>
2013-12-11 22:26:38 -0800

    improve testing a bit

--- src/test/java/com/fasterxml/jackson/databind/node/TestObjectNode.java
+++ src/test/java/com/fasterxml/jackson/databind/node/TestObjectNode.java
@@ -269,4 +269,24 @@ public class TestObjectNode
         assertEquals(3, root3.size());
         assertEquals(1, root3.path("a").intValue());
     }
+
+    public void testEqualityWrtOrder() throws Exception
+    {
+        ObjectNode ob1 = MAPPER.createObjectNode();
+        ObjectNode ob2 = MAPPER.createObjectNode();
+
+        // same contents, different insertion order; should not matter
+        
+        ob1.put("a", 1);
+        ob1.put("b", 2);
+        ob1.put("c", 3);
+
+        ob2.put("b", 2);
+        ob2.put("c", 3);
+        ob2.put("a", 1);
+
+        assertTrue(ob1.equals(ob2));
+        assertTrue(ob2.equals(ob1));
+    }
+    
 }

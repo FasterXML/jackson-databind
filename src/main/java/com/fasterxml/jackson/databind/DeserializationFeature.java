72e590f80883551fe212bfdabf65993eaf66fa1e
yinzara <yinzara@gmail.com>
2014-01-11 11:57:54 -0800

    Adding @since 2.4 and removing unused null check

--- src/main/java/com/fasterxml/jackson/databind/DeserializationFeature.java
+++ src/main/java/com/fasterxml/jackson/databind/DeserializationFeature.java
@@ -209,7 +209,9 @@ public enum DeserializationFeature implements ConfigFeature
      * values to the corresponding value type.  This is basically the opposite of the {@link #ACCEPT_SINGLE_VALUE_AS_ARRAY}
      * feature.  If more than one value is found in the array, a JsonMappingException is thrown.
      * <p>
+     * 
      * Feature is disabled by default
+     * @since 2.4
      */
     UNWRAP_SINGLE_VALUE_ARRAYS(false),
 

d352f0cbe16d7000546bc674a1b70fed7988f4a1
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-01-11 19:54:02 -0800

    Move #88 test case out of 'failing' (passes now)

--- src/test/java/com/fasterxml/jackson/databind/jsontype/TestDefaultForTreeNodes.java
+++ src/test/java/com/fasterxml/jackson/databind/jsontype/TestDefaultForTreeNodes.java
@@ -1,9 +1,9 @@
-package com.fasterxml.jackson.failing;
+package com.fasterxml.jackson.databind.jsontype;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.*;
 
-public class TestNodeTypingIssue88 extends BaseMapTest
+public class TestDefaultForTreeNodes extends BaseMapTest
 {
     public static class Foo {
         public String bar;
@@ -17,26 +17,25 @@ public class TestNodeTypingIssue88 extends BaseMapTest
     /* Unit tests
     /**********************************************************
      */
+
+    private final ObjectMapper DEFAULT_MAPPER = new ObjectMapper();
+    {
+        DEFAULT_MAPPER.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+    }
     
     public void testValueAsStringWithDefaultTyping() throws Exception
     {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-
         Foo foo = new Foo("baz");
-        String json = mapper.writeValueAsString(foo);
+        String json = DEFAULT_MAPPER.writeValueAsString(foo);
 
-        JsonNode jsonNode = mapper.readTree(json);
+        JsonNode jsonNode = DEFAULT_MAPPER.readTree(json);
         assertEquals(jsonNode.get("bar").textValue(), foo.bar);
     }
 
     public void testValueToTreeWithDefaultTyping() throws Exception
     {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-
         Foo foo = new Foo("baz");
-        JsonNode jsonNode = mapper.valueToTree(foo);
+        JsonNode jsonNode = DEFAULT_MAPPER.valueToTree(foo);
         assertEquals(jsonNode.get("bar").textValue(), foo.bar);
     }
 }

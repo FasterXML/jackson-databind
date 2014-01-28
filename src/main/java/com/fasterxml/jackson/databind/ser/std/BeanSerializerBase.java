06cc19bc7bf762836d625c75e63ec336723a2d0b
Pascal Gélinas <pascal.gelinas@nuecho.com>
2014-01-28 10:07:58 -0500

    Support for custom object id serialization by subclasses. Mainly to support FasterXML/jackson-dataformat-xml#81.

--- src/main/java/com/fasterxml/jackson/databind/ser/std/BeanSerializerBase.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/std/BeanSerializerBase.java
@@ -7,9 +7,7 @@ import java.util.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.ObjectIdGenerator;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import com.fasterxml.jackson.core.*;
-
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
@@ -581,6 +579,15 @@ public abstract class BeanSerializerBase
             w.serializer.serialize(id, jgen, provider);
             return;
         }
+
+        _serializeObjectId(bean, jgen, provider, typeSer, objectId);
+    }
+
+    protected  void _serializeObjectId(Object bean,
+            JsonGenerator jgen,SerializerProvider provider,
+            TypeSerializer typeSer, WritableObjectId objectId)
+        throws IOException, JsonProcessingException, JsonGenerationException {
+        final ObjectIdWriter w = _objectIdWriter;
         String typeStr = (_typeId == null) ? null :_customTypeId(bean);
         if (typeStr == null) {
             typeSer.writeTypePrefixForObject(bean, jgen);

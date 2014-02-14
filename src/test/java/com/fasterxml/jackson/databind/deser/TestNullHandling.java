e602790de2c22593576fd1ec88202e8e820a7a4e
Will Palmeri <wpalmeri@paypal.com>
2014-02-14 15:23:52 -0800

    tests for custom null deser with collections unit tests showing custom null deserialization of values in arrays and maps

--- src/test/java/com/fasterxml/jackson/databind/deser/TestNullHandling.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestNullHandling.java
@@ -1,9 +1,13 @@
 package com.fasterxml.jackson.databind.deser;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -50,5 +54,53 @@ public class TestNullHandling extends BaseMapTest
         str = reader.readValue("null");
         assertNotNull(str);
         assertEquals("funny", str);
+    }
+
+    // Test for [jackson-databind-407]
+    public void testListOfNulls() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(String.class, new FunnyNullDeserializer());
+        mapper.registerModule(module);
+
+        List<String> list = Arrays.asList("funny");
+        JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, String.class);
+
+        // should get non-default null directly:
+        List deser = mapper.readValue("[null]", type);
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals(list.get(0), deser.get(0));
+
+        // as well as via ObjectReader
+        ObjectReader reader = mapper.reader(type);
+        deser = reader.readValue("[null]");
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals(list.get(0), deser.get(0));
+    }
+
+    // Test for [jackson-databind-407]
+    public void testMapOfNulls() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule("test", Version.unknownVersion());
+        module.addDeserializer(String.class, new FunnyNullDeserializer());
+        mapper.registerModule(module);
+
+        JavaType type = mapper.getTypeFactory().constructMapType(Map.class, String.class, String.class);
+        // should get non-default null directly:
+        Map deser = mapper.readValue("{\"key\":null}", type);
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals("funny", deser.get("key"));
+
+        // as well as via ObjectReader
+        ObjectReader reader = mapper.reader(type);
+        deser = mapper.readValue("{\"key\":null}", type);
+        assertNotNull(deser);
+        assertEquals(1, deser.size());
+        assertEquals("funny", deser.get("key"));
     }
 }

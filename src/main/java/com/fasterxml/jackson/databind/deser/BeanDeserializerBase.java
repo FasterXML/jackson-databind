c3590d740385fb13005d3c62c925d7e3c445d327
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-03-04 20:18:09 -0800

    Fix #420 for 2.3

--- src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerBase.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerBase.java
@@ -895,7 +895,7 @@ public abstract class BeanDeserializerBase
         throws IOException, JsonProcessingException;
 
     @Override
-    public final Object deserializeWithType(JsonParser jp, DeserializationContext ctxt,
+    public Object deserializeWithType(JsonParser jp, DeserializationContext ctxt,
             TypeDeserializer typeDeserializer)
         throws IOException, JsonProcessingException
     {

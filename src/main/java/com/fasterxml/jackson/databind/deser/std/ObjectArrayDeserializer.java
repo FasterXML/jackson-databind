1b2411acce0be634aaf033c97156e25886d19492
Tatu <tsaloranta@gmail.com>
2014-03-21 11:40:21 -0700

    another minor fix, now for Object[]

--- src/main/java/com/fasterxml/jackson/databind/deser/std/ObjectArrayDeserializer.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/ObjectArrayDeserializer.java
@@ -144,7 +144,7 @@ public class ObjectArrayDeserializer
             Object value;
             
             if (t == JsonToken.VALUE_NULL) {
-                value = null;
+                value = _elementDeserializer.getNullValue();
             } else if (typeDeser == null) {
                 value = _elementDeserializer.deserialize(jp, ctxt);
             } else {
@@ -178,7 +178,7 @@ public class ObjectArrayDeserializer
          */
         return (Object[]) typeDeserializer.deserializeTypedFromArray(jp, ctxt);
     }
-    
+
     /*
     /**********************************************************
     /* Internal methods
@@ -225,7 +225,7 @@ public class ObjectArrayDeserializer
         Object value;
         
         if (t == JsonToken.VALUE_NULL) {
-            value = null;
+            value = _elementDeserializer.getNullValue();
         } else if (_elementTypeDeserializer == null) {
             value = _elementDeserializer.deserialize(jp, ctxt);
         } else {

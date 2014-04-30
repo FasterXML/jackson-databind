bb6b1c26c783e939be4af4b8ff42558b44d5b705
Christopher Currie <christopher@currie.com>
2014-04-29 21:21:52 -0700

    Failing test for scala style props

---
+++ src/test/java/com/fasterxml/jackson/failing/TestScalaLikeImplicitProperties.java
@@ -0,0 +1,79 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Tests Scala-style JVM naming patterns for properties.
+ *
+ * Scala uses identifiers that are legal JVM names, but not legal Java names:
+ *
+ * <ul>
+ *     <li><code>prop␣</code> (trailing space) for fields</li>
+ *     <li><code>prop</code> for getters</li>
+ *     <li><code>prop_=</code> for setters</li>
+ * </ul>
+ *
+ * Scala sources turn property accesses into method calls in most cases; the
+ * backing field and the particulars of the method names are implementation details.
+ *
+ * Since I can't reproduce them in Java, I've substituted legal but uncommonly
+ * used characters as placeholders.
+ */
+public class TestScalaLikeImplicitProperties extends BaseMapTest
+{
+    static class ValProperty
+    {
+        public String prop‿ = "val";
+        public String prop() { return prop‿; }
+    }
+
+    static class VarProperty
+    {
+        public String prop‿ = "var";
+        public String prop() { return prop‿; }
+        public void prop_⁀(String p) { prop‿ = p; }
+    }
+
+    static class GetterSetterProperty
+    {
+        // Different name to represent an arbitrary implementation, not necessarily local to this class.
+        private String _prop_impl = "get/set";
+        public String prop() { return _prop_impl; }
+        public void prop_⁀(String p) { _prop_impl = p; }
+    }
+
+
+    public void testValProperty() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+
+        // TODO: Activate whatever handler implements the property detection style
+
+        assertEquals("{\"prop\":\"val\"}", m.writeValueAsString(new ValProperty()));
+    }
+
+
+    public void testVarProperty() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+
+        // TODO: Activate whatever handler implements the property detection style
+
+        assertEquals("{\"prop\":\"var\"}", m.writeValueAsString(new VarProperty()));
+        VarProperty result = m.readValue("{\"prop\":\"read\"}", VarProperty.class);
+        assertEquals("read", result.prop());
+    }
+
+
+    public void testGetterSetterProperty() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+
+        // TODO: Activate whatever handler implements the property detection style
+
+        assertEquals("{\"prop\":\"get/set\"}", m.writeValueAsString(new GetterSetterProperty()));
+        GetterSetterProperty result = m.readValue("{\"prop\":\"read\"}", GetterSetterProperty.class);
+        assertEquals("read", result.prop());
+    }
+}

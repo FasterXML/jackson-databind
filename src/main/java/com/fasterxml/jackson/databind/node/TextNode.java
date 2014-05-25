e3be8a68ea16b4e02c6aa7eee401cb5d9a6776b2
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-05-24 20:55:53 -0700

    minor cleanup

--- src/main/java/com/fasterxml/jackson/databind/node/TextNode.java
+++ src/main/java/com/fasterxml/jackson/databind/node/TextNode.java
@@ -167,6 +167,7 @@ public class TextNode
         return _value;
     }
 
+    @Override
     public String asText(String defaultValue) {
         return (_value == null) ? defaultValue : _value;
     }

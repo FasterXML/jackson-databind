bcccd3e82241db25a2f34ed655dab88395089f39
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-10-28 20:32:47 -0700

    Fix #592

--- src/main/java/com/fasterxml/jackson/databind/util/TokenBuffer.java
+++ src/main/java/com/fasterxml/jackson/databind/util/TokenBuffer.java
@@ -400,10 +400,25 @@ public class TokenBuffer
      * 
      * @since 2.3
      */
-    public TokenBuffer deserialize(JsonParser jp, DeserializationContext ctxt)
-        throws IOException, JsonProcessingException
+    public TokenBuffer deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException
     {
-        copyCurrentStructure(jp);
+        if (jp.getCurrentTokenId() != JsonToken.FIELD_NAME.id()) {
+            copyCurrentStructure(jp);
+            return this;
+        }
+        /* 28-Oct-2014, tatu: As per #592, need to support a special case of starting from
+         *    FIELD_NAME, which is taken to mean that we are missing START_OBJECT, but need
+         *    to assume one did exist.
+         */
+        JsonToken t;
+        writeStartObject();
+        do {
+            copyCurrentStructure(jp);
+        } while ((t = jp.nextToken()) == JsonToken.FIELD_NAME);
+        if (t != JsonToken.END_OBJECT) {
+            throw ctxt.mappingException("Expected END_OBJECT after copying contents of a JsonParser into TokenBuffer, got "+t);
+        }
+        writeEndObject();
         return this;
     }
     
--- src/test/java/com/fasterxml/jackson/databind/creators/TestCreatorsDelegating.java
+++ src/test/java/com/fasterxml/jackson/databind/creators/TestCreatorsDelegating.java
@@ -2,8 +2,10 @@ package com.fasterxml.jackson.databind.creators;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JacksonInject;
-
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.util.TokenBuffer;
 
 public class TestCreatorsDelegating extends BaseMapTest
 {
@@ -51,7 +53,21 @@ public class TestCreatorsDelegating extends BaseMapTest
             return new FactoryBean711(a, n1, n2);
         }
     }
-    
+
+    static class Value592
+    {
+        protected Object stuff;
+
+        protected Value592(Object ob, boolean bogus) {
+            stuff = ob;
+        }
+        
+        @JsonCreator
+        public static Value592 from(TokenBuffer buffer) {
+            return new Value592(buffer, false);
+        }
+    }
+        
     /*
     /**********************************************************
     /* Unit tests
@@ -103,4 +119,27 @@ public class TestCreatorsDelegating extends BaseMapTest
         assertEquals("Fygar", bean.name1);
         assertEquals("Fygar", bean.name2);
     }
+
+    // [databind#592]
+    public void testDelegateWithTokenBuffer() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Value592 value = mapper.readValue("{\"a\":1,\"b\":2}", Value592.class);
+        assertNotNull(value);
+        Object ob = value.stuff;
+        assertEquals(TokenBuffer.class, ob.getClass());
+        JsonParser jp = ((TokenBuffer) ob).asParser();
+        assertToken(JsonToken.START_OBJECT, jp.nextToken());
+        assertToken(JsonToken.FIELD_NAME, jp.nextToken());
+        assertEquals("a", jp.getCurrentName());
+        assertToken(JsonToken.VALUE_NUMBER_INT, jp.nextToken());
+        assertEquals(1, jp.getIntValue());
+        assertToken(JsonToken.FIELD_NAME, jp.nextToken());
+        assertEquals("b", jp.getCurrentName());
+        assertToken(JsonToken.VALUE_NUMBER_INT, jp.nextToken());
+        assertEquals(2, jp.getIntValue());
+        assertToken(JsonToken.END_OBJECT, jp.nextToken());
+        jp.close();
+    }
+
 }

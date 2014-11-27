e7c5420bfe259e16e6c52a0f85dd379b5f266aa2
Michael Spiegel <spiegel@addthis.com>
2014-11-26 20:40:21 -0500

    Unit test of NoClassDefFoundDeserializer.

---
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestNoClassDefFoundDeserializer.java
@@ -0,0 +1,39 @@
+package com.fasterxml.jackson.databind.deser;
+
+
+import javax.money.MonetaryAmount;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestNoClassDefFoundDeserializer extends BaseMapTest {
+
+    public static class Parent {
+        public List<Child> money;
+    }
+
+    public static class Child {
+        public MonetaryAmount money;
+    }
+
+    public void testClassIsMissing() throws ClassNotFoundException
+    {
+        boolean missing = false;
+        try {
+            Class.forName("javax.money.MonetaryAmount");
+        } catch (ClassNotFoundException ex) {
+            missing = true;
+        }
+        assertTrue("javax.money.MonetaryAmount is not in classpath", missing);
+    }
+
+    public void testDeserialize() throws Exception
+    {
+        ObjectMapper m = new ObjectMapper();
+        Parent result = m.readValue(" { } ", Parent.class);
+        assertNotNull(result);
+    }
+
+}

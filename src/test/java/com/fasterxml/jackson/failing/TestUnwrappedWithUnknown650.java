0e4faee709b7db6ccad181dea37ce1423927aa69
Cowtowncoder <tatu.saloranta@iki.fi>
2014-12-12 09:22:52 -0800

    Add unit test for #650

---
+++ src/test/java/com/fasterxml/jackson/failing/TestUnwrappedWithUnknown650.java
@@ -0,0 +1,31 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.*;
+
+public class TestUnwrappedWithUnknown650 extends BaseMapTest
+{
+    static class A {
+        @JsonUnwrapped
+        public B b;
+    }
+
+    static class B {
+        public String field;
+    }
+
+    private final ObjectMapper MAPPER = new ObjectMapper();
+
+    public void testFailOnUnknownPropertyUnwrapped() throws Exception
+    {
+        assertTrue(MAPPER.isEnabled(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES));
+
+        final String JSON = "{'field': 'value', 'bad':'bad value'}";
+        try {
+            MAPPER.readValue(aposToQuotes(JSON), A.class);
+            fail("Exception was not thrown on unkown property");
+        } catch (JsonMappingException e) {
+            verifyException(e, "Unrecognized field");
+        }
+    }
+}

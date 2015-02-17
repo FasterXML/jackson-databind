bf59bfb434b95d9c71e63eb5f5267f4c8682a243
Cowtowncoder <tatu.saloranta@iki.fi>
2015-02-17 13:03:32 -0800

    More refactoring wrt #706

--- src/main/java/com/fasterxml/jackson/databind/jsonFormatVisitors/JsonFormatVisitorWrapper.java
+++ src/main/java/com/fasterxml/jackson/databind/jsonFormatVisitors/JsonFormatVisitorWrapper.java
@@ -2,10 +2,14 @@ package com.fasterxml.jackson.databind.jsonFormatVisitors;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 /**
  * Interface for visitor callbacks, when type in question can be any of
  * legal JSON types.
+ *<p>
+ * In most cases it will make more sense to extend {@link JsonFormatVisitorWrapper.Base}
+ * instead of directly implementing this interface.
  */
 public interface JsonFormatVisitorWrapper extends JsonFormatVisitorWithSerializerProvider
 {
@@ -56,4 +60,86 @@ public interface JsonFormatVisitorWrapper extends JsonFormatVisitorWithSerialize
      * @since 2.2
      */
     public JsonMapFormatVisitor expectMapFormat(JavaType type) throws JsonMappingException;
+
+    /**
+     * Empty "no-op" implementation of {@link JsonFormatVisitorWrapper}, suitable for
+     * sub-classing. Does implement {@link #setProvider(SerializerProvider)} and
+     * {@link #getProvider()} as expected; other methods simply return null
+     * and do nothing.
+     *
+     * @since 2.5
+     */
+    public static class Base implements JsonFormatVisitorWrapper {
+        protected SerializerProvider _provider;
+
+        public Base() { }
+
+        public Base(SerializerProvider p) {
+             _provider = p;
+        }
+
+        @Override
+        public SerializerProvider getProvider() {
+             return _provider;
+        }
+
+        @Override
+        public void setProvider(SerializerProvider p) {
+             _provider = p;
+        }
+
+        @Override
+        public JsonObjectFormatVisitor expectObjectFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonArrayFormatVisitor expectArrayFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonStringFormatVisitor expectStringFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonNumberFormatVisitor expectNumberFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonIntegerFormatVisitor expectIntegerFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonBooleanFormatVisitor expectBooleanFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonNullFormatVisitor expectNullFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonAnyFormatVisitor expectAnyFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+
+        @Override
+        public JsonMapFormatVisitor expectMapFormat(JavaType type)
+                  throws JsonMappingException {
+             return null;
+        }
+   }
 }

a06883b39043618451a9d59c85361c506f287dc8
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-03-16 21:11:49 -0700

    Combine JsonSerializerMap with ReadOnlyClassToSerializerMap

--- src/main/java/com/fasterxml/jackson/databind/ser/impl/ReadOnlyClassToSerializerMap.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/impl/ReadOnlyClassToSerializerMap.java
@@ -10,39 +10,165 @@ import com.fasterxml.jackson.databind.util.TypeKey;
  * Optimized lookup table for accessing two types of serializers; typed
  * and non-typed. Only accessed from a single thread, so no synchronization
  * needed for accessors.
+ *<p>
+ * Note that before 2.6 this class was much smaller, and referred most
+ * operations to separate <code>JsonSerializerMap</code>, but in 2.6
+ * functions were combined.
  */
 public final class ReadOnlyClassToSerializerMap
 {
-    /**
-     * Actual mappings from type key to serializers
-     */
-    protected final JsonSerializerMap _map;
+    private final Bucket[] _buckets;
 
-    private ReadOnlyClassToSerializerMap(JsonSerializerMap map) {
-        super();
-        _map = map;
+    private final int _size;
+
+    private final int _mask;
+
+    public ReadOnlyClassToSerializerMap(Map<TypeKey,JsonSerializer<Object>> serializers)
+    {
+        int size = findSize(serializers.size());
+        _size = size;
+        _mask = (size-1);
+        Bucket[] buckets = new Bucket[size];
+        for (Map.Entry<TypeKey,JsonSerializer<Object>> entry : serializers.entrySet()) {
+            TypeKey key = entry.getKey();
+            int index = key.hashCode() & _mask;
+            buckets[index] = new Bucket(buckets[index], key, entry.getValue());
+        }
+        _buckets = buckets;
+    }
+    
+    private final static int findSize(int size)
+    {
+        // For small enough results (64 or less), we'll require <= 50% fill rate; otherwise 80%
+        int needed = (size <= 64) ? (size + size) : (size + (size >> 2));
+        int result = 8;
+        while (result < needed) {
+            result += result;
+        }
+        return result;
     }
 
     /**
      * Factory method for constructing an instance.
      */
     public static ReadOnlyClassToSerializerMap from(HashMap<TypeKey, JsonSerializer<Object>> src) {
-        return new ReadOnlyClassToSerializerMap(new JsonSerializerMap(src));
+        return new ReadOnlyClassToSerializerMap(src);
     }
 
-    public JsonSerializer<Object> typedValueSerializer(JavaType type) { 
-        return _map.findTyped(type);
+    /*
+    /**********************************************************
+    /* Public API
+    /**********************************************************
+     */
+
+    public int size() { return _size; }
+    
+    public JsonSerializer<Object> typedValueSerializer(JavaType type)
+    {
+        Bucket bucket = _buckets[TypeKey.typedHash(type) & _mask];
+        if (bucket == null) {
+            return null;
+        }
+        if (bucket.matchesTyped(type)) {
+            return bucket.value;
+        }
+        while ((bucket = bucket.next) != null) {
+            if (bucket.matchesTyped(type)) {
+                return bucket.value;
+            }
+        }
+        return null;
     }
 
-    public JsonSerializer<Object> typedValueSerializer(Class<?> cls) { 
-        return _map.findTyped(cls);
+    public JsonSerializer<Object> typedValueSerializer(Class<?> type)
+    {
+        Bucket bucket = _buckets[TypeKey.typedHash(type) & _mask];
+        if (bucket == null) {
+            return null;
+        }
+        if (bucket.matchesTyped(type)) {
+            return bucket.value;
+        }
+        while ((bucket = bucket.next) != null) {
+            if (bucket.matchesTyped(type)) {
+                return bucket.value;
+            }
+        }
+        return null;
     }
 
-    public JsonSerializer<Object> untypedValueSerializer(JavaType type) { 
-        return _map.findUntyped(type);
+    public JsonSerializer<Object> untypedValueSerializer(JavaType type)
+    {
+        Bucket bucket = _buckets[TypeKey.untypedHash(type) & _mask];
+        if (bucket == null) {
+            return null;
+        }
+        if (bucket.matchesUntyped(type)) {
+            return bucket.value;
+        }
+        while ((bucket = bucket.next) != null) {
+            if (bucket.matchesUntyped(type)) {
+                return bucket.value;
+            }
+        }
+        return null;
     }
 
-    public JsonSerializer<Object> untypedValueSerializer(Class<?> cls) { 
-        return _map.findUntyped(cls);
+    public JsonSerializer<Object> untypedValueSerializer(Class<?> type)
+    {
+        Bucket bucket = _buckets[TypeKey.untypedHash(type) & _mask];
+        if (bucket == null) {
+            return null;
+        }
+        if (bucket.matchesUntyped(type)) {
+            return bucket.value;
+        }
+        while ((bucket = bucket.next) != null) {
+            if (bucket.matchesUntyped(type)) {
+                return bucket.value;
+            }
+        }
+        return null;
+    }    
+    /*
+    /**********************************************************
+    /* Helper classes
+    /**********************************************************
+     */
+
+    private final static class Bucket
+    {
+        public final JsonSerializer<Object> value;
+        public final Bucket next;
+
+        protected final Class<?> _class;
+        protected final JavaType _type;
+
+        protected final boolean _isTyped;
+        
+        public Bucket(Bucket next, TypeKey key, JsonSerializer<Object> value)
+        {
+            this.next = next;
+            this.value = value;
+            _isTyped = key.isTyped();
+            _class = key.getRawType();
+            _type = key.getType();
+        }
+
+        public boolean matchesTyped(Class<?> key) {
+            return (_class == key) && _isTyped;
+        }
+
+        public boolean matchesUntyped(Class<?> key) {
+            return (_class == key) && !_isTyped;
+        }
+
+        public boolean matchesTyped(JavaType key) {
+            return _isTyped && key.equals(_type);
+        }
+
+        public boolean matchesUntyped(JavaType key) {
+            return !_isTyped && key.equals(_type);
+        }
     }
 }

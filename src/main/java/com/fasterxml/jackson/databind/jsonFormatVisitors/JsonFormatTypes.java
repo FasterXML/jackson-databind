c1920637368acc4fbaa9f65f73fcfa843daee892
Cowtowncoder <tatu.saloranta@iki.fi>
2015-04-29 10:27:14 -0700

    minor test addition to ensure non-type-changing converters work

--- src/main/java/com/fasterxml/jackson/databind/jsonFormatVisitors/JsonFormatTypes.java
+++ src/main/java/com/fasterxml/jackson/databind/jsonFormatVisitors/JsonFormatTypes.java
@@ -1,10 +1,12 @@
 package com.fasterxml.jackson.databind.jsonFormatVisitors;
 
+import java.util.*;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-public enum JsonFormatTypes {
-	
+public enum JsonFormatTypes
+{
 	STRING,
 	NUMBER,
 	INTEGER,
@@ -13,15 +15,21 @@ public enum JsonFormatTypes {
 	ARRAY,
 	NULL,
 	ANY;
-	
+
+	private static final Map<String,JsonFormatTypes> _byLCName = new HashMap<String,JsonFormatTypes>();
+	static {
+	    for (JsonFormatTypes t : values()) {
+	        _byLCName.put(t.name().toLowerCase(), t);
+	    }
+	}
 	
 	@JsonValue
 	public String value() {
-		return this.name().toLowerCase();
+		return name().toLowerCase();
 	}
 	
 	@JsonCreator
 	public static JsonFormatTypes forValue(String s) {
-		return valueOf(s.toUpperCase());
+		return _byLCName.get(s);
 	}
 }

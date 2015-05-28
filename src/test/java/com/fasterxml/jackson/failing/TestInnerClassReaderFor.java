b9c831af04d848ddb7b539fd2e0a79b9606827c6
Christopher Currie <codemonkey@usermind.com>
2015-05-28 11:03:00 -0700

    Failing test for reading inner class

---
+++ src/test/java/com/fasterxml/jackson/failing/TestInnerClassReaderFor.java
@@ -0,0 +1,44 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.BaseMapTest;
+
+import java.io.IOException;
+
+public class TestInnerClassReaderFor extends BaseMapTest {
+
+    class X {
+        private String value;
+
+        X(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
+    class Y extends X {
+        Y(@JsonProperty("value") String value) {
+            super(value);
+        }
+    }
+
+    public void testReaderFor() throws IOException {
+
+        X x = new X("dummy");
+        objectMapper().readerForUpdating(x).readValue("{\"value\": \"updatedX\"}");
+        assertEquals(x.getValue(), "updatedX");
+
+        Y y = new Y("dummy");
+        objectMapper().readerForUpdating(y).readValue("{\"value\": \"updatedY\"}");
+        assertEquals(y.getValue(), "updatedY");
+
+    }
+
+}

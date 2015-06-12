b4547f8f10ef102775d5f74df8cb0e9e3ecbf333
Cowtowncoder <tatu.saloranta@iki.fi>
2015-06-12 16:57:39 -0700

    Tinkering with #827 test

--- src/test/java/com/fasterxml/jackson/failing/PolyMapWriter827Test.java
+++ src/test/java/com/fasterxml/jackson/failing/PolyMapWriter827Test.java
@@ -1,23 +1,19 @@
-package com.fasterxml.jackson.databind.seq;
+package com.fasterxml.jackson.failing;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Assert;
-import org.junit.Test;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.type.MapType;
 
 // for [databind#827]
-public class PolyMapWriterTest extends BaseMapTest
+public class PolyMapWriter827Test extends BaseMapTest
 {
     static class CustomKey {
         String a;
@@ -25,24 +21,21 @@ public class PolyMapWriterTest extends BaseMapTest
 
         public String toString() { return "BAD-KEY"; }
     }
-    
+
     public class CustomKeySerializer extends JsonSerializer<CustomKey> {
         @Override
         public void serialize(CustomKey key, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
             jsonGenerator.writeFieldName(key.a + "," + key.b);
         }
     }
-    
-    @Test
-    public void testPolyCustomKeySerializer() throws Exception {
+
+    public void testPolyCustomKeySerializer() throws Exception
+    {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
         mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        
-        SimpleModule module = new SimpleModule("keySerializerModule");
-        module.addKeySerializer(CustomKey.class, new CustomKeySerializer());
-        mapper.registerModule(module);
+
+        mapper.registerModule(new SimpleModule("keySerializerModule")
+            .addKeySerializer(CustomKey.class, new CustomKeySerializer()));
 
         Map<CustomKey, String> map = new HashMap<CustomKey, String>();
         CustomKey key = new CustomKey();
@@ -50,12 +43,8 @@ public class PolyMapWriterTest extends BaseMapTest
         key.b = 1;
         map.put(key, "bar");
 
-        final MapType type = mapper.getTypeFactory().constructMapType(
-                Map.class, CustomKey.class, String.class);
-        final ObjectWriter writer = mapper.writerFor(type);
+        final ObjectWriter writer = mapper.writerFor(new TypeReference<Map<CustomKey,String>>() { });
         String json = writer.writeValueAsString(map);
-               
         Assert.assertEquals("[\"java.util.HashMap\",{\"foo,1\":\"bar\"}]", json);
     }
-
 }

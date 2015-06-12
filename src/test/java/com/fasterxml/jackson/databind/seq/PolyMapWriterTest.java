99fc2ec1e485e40b47a5366b5b4a089b26ef0296
Cowtowncoder <tatu.saloranta@iki.fi>
2015-06-12 16:31:35 -0700

    add unit test for #827 to show the problem

---
+++ src/test/java/com/fasterxml/jackson/databind/seq/PolyMapWriterTest.java
@@ -0,0 +1,61 @@
+package com.fasterxml.jackson.databind.seq;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.type.MapType;
+
+// for [databind#827]
+public class PolyMapWriterTest extends BaseMapTest
+{
+    static class CustomKey {
+        String a;
+        int b;
+
+        public String toString() { return "BAD-KEY"; }
+    }
+    
+    public class CustomKeySerializer extends JsonSerializer<CustomKey> {
+        @Override
+        public void serialize(CustomKey key, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+            jsonGenerator.writeFieldName(key.a + "," + key.b);
+        }
+    }
+    
+    @Test
+    public void testPolyCustomKeySerializer() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        
+        SimpleModule module = new SimpleModule("keySerializerModule");
+        module.addKeySerializer(CustomKey.class, new CustomKeySerializer());
+        mapper.registerModule(module);
+
+        Map<CustomKey, String> map = new HashMap<CustomKey, String>();
+        CustomKey key = new CustomKey();
+        key.a = "foo";
+        key.b = 1;
+        map.put(key, "bar");
+
+        final MapType type = mapper.getTypeFactory().constructMapType(
+                Map.class, CustomKey.class, String.class);
+        final ObjectWriter writer = mapper.writerFor(type);
+        String json = writer.writeValueAsString(map);
+               
+        Assert.assertEquals("[\"java.util.HashMap\",{\"foo,1\":\"bar\"}]", json);
+    }
+
+}

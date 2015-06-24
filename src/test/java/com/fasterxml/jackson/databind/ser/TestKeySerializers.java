02d7218d076e4cb6de05d00d1384090d2bf20f05
zoliszel <zoltan.szel@mahifx.com>
2015-06-24 11:29:32 +0100

    Unit test to ensure custom KeySerializer is indeed invoked when serializing Map keys

--- src/test/java/com/fasterxml/jackson/databind/ser/TestKeySerializers.java
+++ src/test/java/com/fasterxml/jackson/databind/ser/TestKeySerializers.java
@@ -1,13 +1,25 @@
 package com.fasterxml.jackson.databind.ser;
 
-import java.io.IOException;
-import java.util.*;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonGenerator;
-
-import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.jsontype.TypeResolverBuilder;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestKeySerializers extends BaseMapTest
 {
@@ -55,6 +67,34 @@ public class TestKeySerializers extends BaseMapTest
         }
     }
 
+    class BAR<T>{
+        T value;
+
+        public BAR(T value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public T getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName()
+                    + ", value:" + value
+                    ;
+        }
+    }
+
+
+    static class BARSerializer<T> extends JsonSerializer<BAR<T>> {
+        @Override
+        public void serialize(BAR<T> value, JsonGenerator jgen,
+                              SerializerProvider provider) throws IOException {
+            jgen.writeFieldName("xxx"+value.getValue().toString());
+        }
+    }
     /*
     /**********************************************************
     /* Unit tests
@@ -93,5 +133,46 @@ public class TestKeySerializers extends BaseMapTest
 
         String json = mapper.writeValueAsString(new ABCMapWrapper());
         assertEquals("{\"stuff\":{\"xxxB\":\"bar\"}}", json);
+    }
+
+    public void testUnWrappedMap() throws Exception{
+        final ObjectMapper mapper = new ObjectMapper();
+        SimpleModule mod = new SimpleModule("test");
+        mod.addKeySerializer(ABC.class, new ABCSerializer());
+        mapper.registerModule(mod);
+
+        TypeResolverBuilder<?> typer = new ObjectMapper.DefaultTypeResolverBuilder(ObjectMapper.DefaultTyping.NON_FINAL);
+        typer = typer.init(JsonTypeInfo.Id.NAME, null);
+        typer = typer.inclusion(JsonTypeInfo.As.PROPERTY);
+        //typer = typer.typeProperty(TYPE_FIELD);
+        typer = typer.typeIdVisibility(true);
+        mapper.setDefaultTyping(typer);
+
+        Map<ABC,String> stuff = new HashMap<ABC,String>();
+        stuff.put(ABC.B,"bar");
+        String json = mapper.writerWithType(new TypeReference<Map<ABC, String>>() {
+        }).writeValueAsString(stuff);
+        assertEquals("{\"@type\":\"HashMap\",\"xxxB\":\"bar\"}", json);
+    }
+
+    @Test
+    public void testUnWrappedMap2() throws Exception{
+        final ObjectMapper mapper = new ObjectMapper();
+        SimpleModule mod = new SimpleModule("test");
+        mod.addKeySerializer(ABC.class, new ABCSerializer());
+        mod.addSerializer(BAR.class,new BARSerializer());
+        mapper.registerModule(mod);
+
+
+        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.WRITE_NULL_MAP_VALUES);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        Map<ABC,BAR<?>> stuff = new HashMap<ABC,BAR<?>>();
+        stuff.put(ABC.B,new BAR<String>("bar"));
+        String json = mapper.writerWithType(mapper.constructType(new TypeReference<Map<ABC,BAR<?>>>() {
+        }.getType())).writeValueAsString(stuff);
+        Assert.assertEquals("{\"xxxB\":\"bar\"}", json);
     }
 }

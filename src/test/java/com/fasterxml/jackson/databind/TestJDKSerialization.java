73f54a6495fe09fc6840a1193df1d37d76e6d782
Cowtowncoder <tatu.saloranta@iki.fi>
2015-08-14 14:35:08 -0700

    Try to reproduce #899

--- src/test/java/com/fasterxml/jackson/databind/TestJDKSerialization.java
+++ src/test/java/com/fasterxml/jackson/databind/TestJDKSerialization.java
@@ -1,10 +1,7 @@
 package com.fasterxml.jackson.databind;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.*;
 
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.databind.util.LRUMap;
@@ -29,6 +26,17 @@ public class TestJDKSerialization extends BaseMapTest
         
         public int getY() { return y; }
         public void setY(int y) { this.y = y; }
+    }
+
+    static enum ABC {
+        A, B, C;
+    }
+    
+    // for [databind#899]
+    static class EnumPOJO {
+        public ABC abc = ABC.B;
+
+        public Map<String,ABC> stuff = new LinkedHashMap<String,ABC>();
     }
 
     /*
@@ -61,6 +69,42 @@ public class TestJDKSerialization extends BaseMapTest
         SerializationConfig sc = jdkDeserialize(scBytes);
         assertNotNull(sc);
         assertEquals(sc._serFeatures, origSC._serFeatures);
+    }
+
+    // for [databind#899]
+    public void testEnumHandlers() throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        // ensure we have serializers and/or deserializers, first
+        String json = mapper.writerFor(EnumPOJO.class)
+                .writeValueAsString(new EnumPOJO());
+        EnumPOJO result = mapper.reader(EnumPOJO.class)
+                .readValue(json);
+        assertNotNull(result);
+
+        // and then use JDK serialization to freeze/thaw objects
+        byte[] bytes = jdkSerialize(mapper);
+        ObjectMapper mapper2 = jdkDeserialize(bytes);
+        assertNotNull(mapper2);
+
+        bytes = jdkSerialize(mapper.reader(EnumPOJO.class));
+        ObjectReader r = jdkDeserialize(bytes);
+        assertNotNull(r);
+
+        /* 14-Aug-2015, tatu: Looks like pre-loading JsonSerializer is problematic
+         *    at this point; comment out for now. Try to fix later on.
+         */
+        
+//        bytes = jdkSerialize(mapper.writerFor(EnumPOJO.class));
+        bytes = jdkSerialize(mapper.writer());
+        ObjectWriter w = jdkDeserialize(bytes);
+        assertNotNull(w);
+
+        // plus, ensure objects are usable:
+        String json2 = w.writeValueAsString(new EnumPOJO());
+        assertEquals(json, json2);
+        EnumPOJO result2 = r.readValue(json2);
+        assertNotNull(result2);
     }
 
     public void testObjectWriter() throws IOException

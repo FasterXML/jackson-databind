769853ae3cbe7751a38e80eaf7a2612a431e4437
Cowtowncoder <tatu.saloranta@iki.fi>
2015-09-23 12:31:25 -0700

    Add a (failing) test for #938

---
+++ src/test/java/com/fasterxml/jackson/failing/RecursiveType938Test.java
@@ -0,0 +1,57 @@
+package com.fasterxml.jackson.failing;
+
+import java.util.*;
+
+import com.fasterxml.jackson.databind.*;
+
+// for [databind#938]
+public class RecursiveType938Test extends BaseMapTest
+{
+    public static interface Ability<T> { }
+
+    public static final class ImmutablePair<L, R> implements Map.Entry<L, R>, Ability<ImmutablePair<L, R>> {
+        public final L key;
+        public final R value;
+
+        public ImmutablePair(final L key, final R value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public L getKey() {
+            return key;
+        }
+
+        @Override
+        public R getValue() {
+            return value;
+        }
+
+        @Override
+        public R setValue(final R value) {
+            throw new UnsupportedOperationException();
+        }
+      
+        static <L, R> ImmutablePair<L, R> of(final L left, final R right) {
+            return new ImmutablePair<L, R>(left, right);
+        }
+    }
+
+    private final ObjectMapper MAPPER = new ObjectMapper();
+
+    public void testRecursivePair() throws Exception
+    {
+        JavaType t = MAPPER.constructType(ImmutablePair.class);
+
+        assertNotNull(t);
+        assertEquals(ImmutablePair.class, t.getRawClass());
+
+        /*
+        List<ImmutablePair<String, Double>> list = new ArrayList<ImmutablePair<String, Double>>();
+        list.add(ImmutablePair.of("Hello World!", 123d));
+        String json = MAPPER.writeValueAsString(list);
+        assertNotNull(json);
+        */
+    }
+}

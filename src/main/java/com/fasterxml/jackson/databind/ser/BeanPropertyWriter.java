6de5dc849202d964f6c797f33f625da8df9979cf
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-11-26 20:35:50 -0800

    Resolve a minor NPE problem for virtual properties

--- src/main/java/com/fasterxml/jackson/databind/ser/BeanPropertyWriter.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/BeanPropertyWriter.java
@@ -586,7 +586,13 @@ public class BeanPropertyWriter
      */
     @Deprecated
     public Class<?> getPropertyType() {
-        return (_accessorMethod != null) ? _accessorMethod.getReturnType() : _field.getType();
+        if (_accessorMethod != null) {
+            return _accessorMethod.getReturnType();
+        }
+        if (_field != null) {
+            return _field.getType();
+        }
+        return null;
     }
 
     /**

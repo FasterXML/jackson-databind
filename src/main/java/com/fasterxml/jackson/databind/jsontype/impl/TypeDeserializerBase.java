dbf0c6f4c93d922a2ebb4d744661e7090fef2aac
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-01-15 21:02:56 -0800

    Fix #1083

--- src/main/java/com/fasterxml/jackson/databind/jsontype/impl/TypeDeserializerBase.java
+++ src/main/java/com/fasterxml/jackson/databind/jsontype/impl/TypeDeserializerBase.java
@@ -74,8 +74,8 @@ public abstract class TypeDeserializerBase
         // 22-Dec-2015, tatu: as per [databind#1055], avoid NPE
         _typePropertyName = (typePropertyName == null) ? "" : typePropertyName;
         _typeIdVisible = typeIdVisible;
-        // defaults are fine, although concurrency of 4 bit more frugal than 16:
-        _deserializers = new ConcurrentHashMap<String, JsonDeserializer<Object>>(16, 0.75f, 4);
+        // defaults are fine, although shouldn't need much concurrency
+        _deserializers = new ConcurrentHashMap<String, JsonDeserializer<Object>>(16, 0.75f, 2);
         if (defaultImpl == null) {
             _defaultImpl = null;
         } else {
@@ -84,6 +84,8 @@ public abstract class TypeDeserializerBase
              *    seldom (if ever) base types, may be ok.
              */
             // 01-Nov-2015, tatu: Actually this is still exactly wrong. Should fix.
+            // 15-Jan-2016, tatu: ... as witnessed by [databind#1083], patched, but
+            //     fundamentally this call can't be made to work for all cases
             _defaultImpl = baseType.forcedNarrowBy(defaultImpl);
         }
         _property = null;
--- src/main/java/com/fasterxml/jackson/databind/type/SimpleType.java
+++ src/main/java/com/fasterxml/jackson/databind/type/SimpleType.java
@@ -123,7 +123,16 @@ public class SimpleType // note: until 2.6 was final
             return this;
         }
         // Should we check that there is a sub-class relationship?
-        return new SimpleType(subclass, _bindings, _superClass, _superInterfaces,
+        // 15-Jan-2016, tatu: Almost yes, but there are some complications with
+        //    placeholder values, so no.
+        /*
+        if (!_class.isAssignableFrom(subclass)) {
+            throw new IllegalArgumentException("Class "+subclass.getName()+" not sub-type of "
+                    +_class.getName());
+        }
+        */
+        // 15-Jan-2015, tatu: Not correct; should really re-resolve...
+        return new SimpleType(subclass, _bindings, this, _superInterfaces,
                 _valueHandler, _typeHandler, _asStatic);
     }
     

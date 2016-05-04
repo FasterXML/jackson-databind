9b7ffec4bbdaf9a0d1715f2bead58e676365a5e8
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-05-03 22:13:13 -0700

    bit more testing improvements

--- src/test/java/com/fasterxml/jackson/databind/ser/TestExceptionSerialization.java
+++ src/test/java/com/fasterxml/jackson/databind/ser/TestExceptionSerialization.java
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.databind.ser;
 
+import java.io.IOException;
 import java.util.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -19,6 +20,7 @@ public class TestExceptionSerialization
 
         public int bogus2 = 5;
 
+        protected ExceptionWithIgnoral() { }
         public ExceptionWithIgnoral(String msg) {
             super(msg);
         }
@@ -56,13 +58,17 @@ public class TestExceptionSerialization
     }
 
     // for [databind#877]
+    @SuppressWarnings("unchecked")
     public void testIgnorals() throws Exception
     {
+        ExceptionWithIgnoral input = new ExceptionWithIgnoral("foobar");
+        input.initCause(new IOException("surprise!"));
+
         // First, should ignore anything with class annotations
         String json = MAPPER
-                .writeValueAsString(new ExceptionWithIgnoral("foobar"));
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(input);
 
-        @SuppressWarnings("unchecked")
         Map<String,Object> result = MAPPER.readValue(json, Map.class);
         assertEquals("foobar", result.get("message"));
 
@@ -73,7 +79,16 @@ public class TestExceptionSerialization
         ObjectMapper mapper = new ObjectMapper();
         mapper.configOverride(ExceptionWithIgnoral.class)
             .setIgnorals(JsonIgnoreProperties.Value.forIgnoredProperties("bogus2"));
-        assertNull(result.get("bogus1"));
-        assertNotNull(result.get("bogus2"));
+        String json2 = mapper
+                .writeValueAsString(new ExceptionWithIgnoral("foobar"));
+
+        Map<String,Object> result2 = mapper.readValue(json2, Map.class);
+        assertNull(result2.get("bogus1"));
+        assertNull(result2.get("bogus2"));
+
+        // and try to deserialize as well
+        ExceptionWithIgnoral output = mapper.readValue(json2, ExceptionWithIgnoral.class);
+        assertNotNull(output);
+        assertEquals("foobar", output.getMessage());
     }
 }

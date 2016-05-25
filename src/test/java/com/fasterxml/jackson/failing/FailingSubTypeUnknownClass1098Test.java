10685061a38d5a8b70fe90d1244916988f72a807
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-05-24 22:42:56 -0700

    Add a failing test for #1098

---
+++ src/test/java/com/fasterxml/jackson/failing/FailingSubTypeUnknownClass1098Test.java
@@ -0,0 +1,27 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import com.fasterxml.jackson.databind.*;
+
+public class FailingSubTypeUnknownClass1098Test extends BaseMapTest
+{
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY,
+            property = "clazz")
+    abstract static class BaseClass {
+    }    
+
+    static class BaseWrapper {
+        public BaseClass value;
+    }
+    
+    public void testUnknownClassAsSubtype() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
+        BaseWrapper w = mapper.readValue(aposToQuotes
+                ("{'value':{'clazz':'com.foobar.Nothing'}}'"),
+                BaseWrapper.class);
+        assertNotNull(w);
+    }
+}

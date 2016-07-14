26f2669cba008959a9a6604af4c003ddcc3935d1
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-07-13 21:38:56 -0700

    test refactoring

--- src/test/java/com/fasterxml/jackson/databind/creators/EnumCreatorTest.java
+++ src/test/java/com/fasterxml/jackson/databind/creators/EnumCreatorTest.java
@@ -6,7 +6,9 @@ import java.util.*;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.fasterxml.jackson.core.type.TypeReference;
+
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.deser.Deserializers;
 import com.fasterxml.jackson.databind.deser.std.EnumDeserializer;
@@ -140,6 +142,36 @@ public class EnumCreatorTest extends BaseMapTest
         public MyEnum960 value;
     }
 
+    static enum Enum1291 {
+
+        V1("val1"),
+        V2("val2"),
+        V3("val3"),
+        V4("val4"),
+        V5("val5"),
+        V6("val6");
+
+        private final String name;
+
+        Enum1291(String name) {
+            this.name = name;
+        }
+
+        public static Enum1291 fromString(String name) {
+            for (Enum1291 type : Enum1291.values()) {
+                if (type.name.equals(name)) {
+                    return type;
+                }
+            }
+            return Enum1291.valueOf(name.toUpperCase());
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
     /*
     /**********************************************************
     /* Test methods
@@ -230,5 +262,14 @@ public class EnumCreatorTest extends BaseMapTest
     {
         MyEnum960 v = MAPPER.readValue("{\"value\":\"bogus\"}", MyEnum960.class);
         assertEquals(MyEnum960.VALUE, v);
+    }
+
+    // for [databind#1291]
+    public void testEnumCreators1291() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(Enum1291.V2);
+        Enum1291 result = mapper.readValue(json, Enum1291.class);
+        assertSame(Enum1291.V2, result);
     }
 }

34643746bf864c9e3ee5e233f700f45cf2b1621a
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-09-23 20:33:02 -0700

    more cleanup, bit of renaming

---
+++ src/main/java/com/fasterxml/jackson/databind/deser/impl/ErrorThrowingDeserializer.java
@@ -0,0 +1,29 @@
+package com.fasterxml.jackson.databind.deser.impl;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+/**
+ * A deserializer that stores an {@link Error} caught during constructing
+ * of the deserializer, which needs to be deferred and only during actual
+ * attempt to deserialize a value of given type.
+ * Note that null and empty values can be deserialized without error.
+ * 
+ * @since 2.9 Note: prior to this version was named <code>NoClassDefFoundDeserializer</code>
+ */
+public class ErrorThrowingDeserializer extends JsonDeserializer<Object>
+{
+    private final Error _cause;
+
+    public ErrorThrowingDeserializer(NoClassDefFoundError cause) {
+        _cause = cause;
+    }
+
+    @Override
+    public Object deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        throw _cause;
+    }
+}

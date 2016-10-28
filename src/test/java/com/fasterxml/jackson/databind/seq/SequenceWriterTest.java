ad31a7d26ed58defdaddcbd5bfd525c3e1ae9d47
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-10-27 19:09:18 -0700

    ...

--- src/test/java/com/fasterxml/jackson/databind/seq/SequenceWriterTest.java
+++ src/test/java/com/fasterxml/jackson/databind/seq/SequenceWriterTest.java
@@ -1,10 +1,7 @@
 package com.fasterxml.jackson.databind.seq;
 
 import java.io.StringWriter;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;

aba2c0a0c57a738ec3b05b7732088730114694a0
jdmichal <jdmichal@gmail.com>
2016-10-28 17:57:13 -0400

    [databind#1433] Fixed whitespace.

--- src/test/java/com/fasterxml/jackson/databind/ObjectMapperTest.java
+++ src/test/java/com/fasterxml/jackson/databind/ObjectMapperTest.java
@@ -348,9 +348,9 @@ public class ObjectMapperTest extends BaseMapTest
     // [databind#1433]
     public void testConvertValueNullPrimitive() throws Exception
     {
-    	assertEquals(Byte.valueOf((byte) 0), MAPPER.convertValue(null, Byte.TYPE));
-    	assertEquals(Short.valueOf((short) 0), MAPPER.convertValue(null, Short.TYPE));
-    	assertEquals(Integer.valueOf(0), MAPPER.convertValue(null, Integer.TYPE));
+        assertEquals(Byte.valueOf((byte) 0), MAPPER.convertValue(null, Byte.TYPE));
+        assertEquals(Short.valueOf((short) 0), MAPPER.convertValue(null, Short.TYPE));
+        assertEquals(Integer.valueOf(0), MAPPER.convertValue(null, Integer.TYPE));
         assertEquals(Long.valueOf(0L), MAPPER.convertValue(null, Long.TYPE));
         assertEquals(Float.valueOf(0f), MAPPER.convertValue(null, Float.TYPE));
         assertEquals(Double.valueOf(0d), MAPPER.convertValue(null, Double.TYPE));
@@ -360,9 +360,9 @@ public class ObjectMapperTest extends BaseMapTest
     // [databind#1433]
     public void testConvertValueNullBoxed() throws Exception
     {
-    	assertNull(MAPPER.convertValue(null, Byte.class));
-    	assertNull(MAPPER.convertValue(null, Short.class));
-    	assertNull(MAPPER.convertValue(null, Integer.class));
+        assertNull(MAPPER.convertValue(null, Byte.class));
+        assertNull(MAPPER.convertValue(null, Short.class));
+        assertNull(MAPPER.convertValue(null, Integer.class));
         assertNull(MAPPER.convertValue(null, Long.class));
         assertNull(MAPPER.convertValue(null, Float.class));
         assertNull(MAPPER.convertValue(null, Double.class));

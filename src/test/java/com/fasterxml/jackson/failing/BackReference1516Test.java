ee265dbc8a87b594162bc3775fbdbc262100de4e
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-03-30 13:26:47 -0700

    Modify test for #1516 so it now fails as reported

--- src/test/java/com/fasterxml/jackson/failing/BackReference1516Test.java
+++ src/test/java/com/fasterxml/jackson/failing/BackReference1516Test.java
@@ -1,7 +1,6 @@
 package com.fasterxml.jackson.failing;
 
-import java.util.List;
-import java.util.UUID;
+import java.beans.ConstructorProperties;
 
 import com.fasterxml.jackson.annotation.*;
 
@@ -10,39 +9,60 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BackReference1516Test extends BaseMapTest
 {
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    static class ParentObject {
-        public UUID id;
-        public String productId;
-        public String productName;
-        public String companyName;
-        public UUID companyLogoImageId;
-        public String recordNumber;
-        public String revisionNumber;
-        public String jsonString;
+    static class ParentWithCreator {
+        String id, name;
 
-        // Test passes if @JsonManagedReference is removed.
         @JsonManagedReference
-        public List<ChildObject> childSet;
+        ChildObject1 child;
 
-        public ParentObject() { }
+        @ConstructorProperties({ "id", "name", "child" })
+        public ParentWithCreator(String id, String name,
+                ChildObject1 child) {
+            this.id = id;
+            this.name = name;
+            this.child = child;
+        }
     }
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    static class ChildObject
+    static class ChildObject1
     {
-        public UUID id;
-        // Test passes if @JsonBackReference is removed.
-        @JsonBackReference
-        public ParentObject parentObject;
-        public UUID componentId;
-        public int orderNumber;
-        public String orderLabel;
-        public String title;
+        public String id, name;
 
-        public ChildObject() { }
+        @JsonBackReference
+        public ParentWithCreator parent;
+
+        @ConstructorProperties({ "id", "name", "parent" })
+        public ChildObject1(String id, String name,
+                ParentWithCreator parent) {
+            this.id = id;
+            this.name = name;
+            this.parent = parent;
+        }
+    }
+    
+    static class ParentWithoutCreator {
+        public String id, name;
+
+        @JsonManagedReference
+        public ChildObject2 child;
     }
 
+    static class ChildObject2
+    {
+        public String id, name;
+
+        @JsonBackReference
+        public ParentWithoutCreator parent;
+
+        @ConstructorProperties({ "id", "name", "parent" })
+        public ChildObject2(String id, String name,
+                ParentWithoutCreator parent) {
+            this.id = id;
+            this.name = name;
+            this.parent = parent;
+        }
+    }
+    
     /*
     /**********************************************************
     /* Unit tests
@@ -51,24 +71,23 @@ public class BackReference1516Test extends BaseMapTest
 
     private final ObjectMapper MAPPER = new ObjectMapper();
 
-    public void testIssue1516() throws Exception
+    private final String PARENT_CHILD_JSON = aposToQuotes(
+"{ 'id': 'abc',\n"+
+"  'name': 'Bob',\n"+
+"  'child': { 'id': 'def', 'title':'Bert' }\n"+
+"}");
+    
+    public void testWithParentCreator() throws Exception
     {
-        ParentObject result = MAPPER.readValue(aposToQuotes(
-"{\n"+
-"  'companyName': 'My Famke Company',\n"+
-"  'companyLogoImageId': '29a8045e-3d10-4121-9f27-429aa74d00ad',\n"+
-"  'productId': 'ABC-0003',\n"+
-"  'productName': 'Engineering Test',\n"+
-"  'recordNumber': '01',\n"+
-"  'revisionNumber': '1.0',\n"+
-"  'procedureId': '6e6f607e-fb3f-4750-8a0a-2b38220e3328',\n"+
-"  'childSet': [ { 'title': 'Child 1',\n"+
-"       'componentId': '3f7debe1-cddc-4b66-b7a7-49249e0c9d3e',\n"+
-"       'orderLabel': '1',\n"+
-"       'orderNumber': 1\n"+
-"   } ]\n"+
-"}"),
-                ParentObject.class);
+        ParentWithCreator result = MAPPER.readValue(PARENT_CHILD_JSON,
+                ParentWithCreator.class);
+        assertNotNull(result);
+    }
+
+    public void testWithParentNoCreator() throws Exception
+    {
+        ParentWithoutCreator result = MAPPER.readValue(PARENT_CHILD_JSON,
+                ParentWithoutCreator.class);
         assertNotNull(result);
     }
 }

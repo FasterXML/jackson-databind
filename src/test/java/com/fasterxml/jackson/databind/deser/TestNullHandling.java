78512b0abcce39b12b3d7e0fa3d4414f35e53ab3
TRINETX\christopher.ogrady <christopher.ogrady@trinetx.com>
2017-04-12 11:33:54 -0400

    #1601 Null Polymorphic Deserialization fails.

--- src/test/java/com/fasterxml/jackson/databind/deser/TestNullHandling.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestNullHandling.java
@@ -1,14 +1,21 @@
 package com.fasterxml.jackson.databind.deser;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
-
-import com.fasterxml.jackson.core.*;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class TestNullHandling extends BaseMapTest
 {
@@ -95,5 +102,64 @@ public class TestNullHandling extends BaseMapTest
         assertNotNull(deser);
         assertEquals(1, deser.size());
         assertEquals("funny", deser.get("key"));
+    }
+
+    static class RootData {
+        public String name;
+        public String type;
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+                property = "type")
+        @JsonSubTypes({
+                @Type(value = TypeA.class, name = "TypeA"),
+                @Type(value = TypeB.class, name = "TypeB")})
+        public Proxy proxy;
+
+        public RootData() {}
+
+        public RootData(String name, String type, Proxy proxy) {
+            this.name = name;
+            this.type = type;
+            this.proxy = proxy;
+        }
+    }
+
+    static interface Proxy {
+
+    }
+
+    static class TypeA implements Proxy {
+        public String aValue;
+
+        public TypeA() {}
+
+        public TypeA(String a) {
+            this.aValue = a;
+        }
+    }
+
+    static class TypeB implements Proxy {
+        public String bValue;
+
+        public TypeB() {}
+
+        public TypeB(String b) {
+            this.bValue = b;
+        }
+    }
+
+    public void testPolymorphicDataNull() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String typeA =
+                "{\"name\":\"TypeAData\", \"type\":\"TypeA\", \"proxy\":{\"aValue\":\"This works!\"}}";
+        RootData typeAData = mapper.readValue(typeA, RootData.class);
+        assertEquals("No value for aValue!?", "This works!", ((TypeA) typeAData.proxy).aValue);
+        String typeB =
+                "{\"name\":\"TypeBData\", \"type\":\"TypeB\", \"proxy\":{\"bValue\":\"This works too!\"}}";
+        RootData typeBData = mapper.readValue(typeB, RootData.class);
+        assertEquals("No value for bValue!?", "This works too!", ((TypeB) typeBData.proxy).bValue);
+        String typeBNull =
+                "{\"name\":\"TypeBData\", \"type\":\"TypeB\", \"proxy\": null}";
+        RootData typeBNullData = mapper.readValue(typeBNull, RootData.class);
+        assertNull("Proxy should be null!", typeBNullData.proxy);
     }
 }

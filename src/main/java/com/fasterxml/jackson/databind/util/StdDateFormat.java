39785388b05ed0ac447860d96015fce359f1ada6
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-06-10 20:48:03 -0700

    Fix #1651

--- src/main/java/com/fasterxml/jackson/databind/util/StdDateFormat.java
+++ src/main/java/com/fasterxml/jackson/databind/util/StdDateFormat.java
@@ -447,8 +447,11 @@ public class StdDateFormat
             df = _formatISO8601_z;
             formatStr = DATE_FORMAT_STR_ISO8601_Z;
             if (df == null) {
+                // 10-Jun-2017, tatu: As per [databind#1651], when using this format,
+                //    must use UTC, not whatever is configured as default timezone
+                //    (because we know `Z` identifier is used)
                 df = _formatISO8601_z = _cloneFormat(DATE_FORMAT_ISO8601_Z, formatStr,
-                        _timezone, _locale, _lenient);
+                        DEFAULT_TIMEZONE, _locale, _lenient);
             }
             // may be missing milliseconds... if so, add
             if (dateStr.charAt(len-4) == ':') {
@@ -520,8 +523,11 @@ public class StdDateFormat
                 df = _formatISO8601_z;
                 formatStr = DATE_FORMAT_STR_ISO8601_Z;
                 if (df == null) {
+                    // 10-Jun-2017, tatu: As per [databind#1651], when using this format,
+                    //    must use UTC, not whatever is configured as default timezone
+                    //    (because we know `Z` identifier is used)
                     df = _formatISO8601_z = _cloneFormat(DATE_FORMAT_ISO8601_Z, formatStr,
-                            _timezone, _locale, _lenient);
+                            DEFAULT_TIMEZONE, _locale, _lenient);
                 }
             }
         }

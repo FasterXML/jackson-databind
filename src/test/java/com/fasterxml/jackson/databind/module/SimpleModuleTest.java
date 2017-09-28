93b59d7cfae9dd7feb0bd6d8d6e71ee73d14c674
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-09-27 22:03:58 -0700

    Fix Java 9 compilation issues

--- src/test/java/com/fasterxml/jackson/databind/module/SimpleModuleTest.java
+++ src/test/java/com/fasterxml/jackson/databind/module/SimpleModuleTest.java
@@ -146,7 +146,7 @@ public class SimpleModuleTest extends BaseMapTest
         }
     }
 
-    protected static class ContextVerifierModule extends Module
+    protected static class ContextVerifierModule extends com.fasterxml.jackson.databind.Module
     {
         @Override
         public String getModuleName() { return "x"; }
@@ -241,7 +241,7 @@ public class SimpleModuleTest extends BaseMapTest
         SimpleModule mod = new SimpleModule("test", Version.unknownVersion());
         mod.addSerializer(new BaseSerializer());
         // and another variant here too
-        List<Module> mods = Arrays.asList((Module) mod);
+        List<SimpleModule> mods = Arrays.asList(mod);
         mapper.registerModules(mods);
         assertEquals(quote("Base:1"), mapper.writeValueAsString(new Impl1()));
         assertEquals(quote("Base:2"), mapper.writeValueAsString(new Impl2()));
@@ -340,7 +340,7 @@ public class SimpleModuleTest extends BaseMapTest
 
     public void testAutoDiscovery() throws Exception
     {
-        List<Module> mods = ObjectMapper.findModules();
+        List<?> mods = ObjectMapper.findModules();
         assertEquals(0, mods.size());
     }
 }

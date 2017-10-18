eb21a9ea1b90cf1faf38bfd07e74950ae9b51e71
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-10-18 15:02:04 -0700

    More type fixes wrt #1604 verification

--- src/test/java/com/fasterxml/jackson/failing/NestedTypes1604Test.java
+++ src/test/java/com/fasterxml/jackson/failing/NestedTypes1604Test.java
@@ -38,12 +38,15 @@ public class NestedTypes1604Test extends BaseMapTest
         }
     }
 
-    public static class RefinedDataList<T> extends Data<List<T>> {
+    // And then add one level between types
+    public static class RefinedDataList<T> extends DataList<T> {
         public RefinedDataList(List<T> data) {
             super(data);
         }
     }
 
+    // And/or add another type parameter that is not relevant (less common
+    // but potential concern)
     public static class SneakyDataList<BOGUS,T> extends Data<List<T>> {
         public SneakyDataList(List<T> data) {
             super(data);

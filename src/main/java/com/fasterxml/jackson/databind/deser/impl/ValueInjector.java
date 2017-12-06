38df039194e44f279295da49e25e2cc257d85d32
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-12-05 18:43:05 -0800

    Remove temporarily re-added deprecated ctor of `ValueInjector`

--- src/main/java/com/fasterxml/jackson/databind/deser/impl/ValueInjector.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/impl/ValueInjector.java
@@ -28,17 +28,6 @@ public class ValueInjector
         _valueId = valueId;
     }
 
-    /**
-     * @deprecated in 2.9 (remove from 3.0)
-     */
-    @Deprecated // see [databind#1835]
-    public ValueInjector(PropertyName propName, JavaType type,
-            com.fasterxml.jackson.databind.util.Annotations contextAnnotations, // removed from later versions
-            AnnotatedMember mutator, Object valueId)
-    {
-        this(propName, type, mutator, valueId);
-    }
-
     public Object findValue(DeserializationContext context, Object beanInstance)
         throws JsonMappingException
     {

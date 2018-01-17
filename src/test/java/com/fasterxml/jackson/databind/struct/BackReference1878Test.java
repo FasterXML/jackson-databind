4e71662bbe0e3776dadd4ee76dfeb197be951291
Tatu Saloranta <tatu.saloranta@iki.fi>
2018-01-16 21:43:14 -0800

    Fix #1878

--- src/test/java/com/fasterxml/jackson/databind/struct/BackReference1878Test.java
+++ src/test/java/com/fasterxml/jackson/databind/struct/BackReference1878Test.java
@@ -1,18 +1,16 @@
-package com.fasterxml.jackson.failing;
+package com.fasterxml.jackson.databind.struct;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import com.fasterxml.jackson.databind.BaseMapTest;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * Created on 09/01/18.
- *
  * @author Reda.Housni-Alaoui
  */
-public class BackReference1878Test extends BaseMapTest {
-
+public class BackReference1878Test extends BaseMapTest
+{
     static class Child {
         @JsonBackReference
         public Parent b;
@@ -26,9 +24,7 @@ public class BackReference1878Test extends BaseMapTest {
     private final ObjectMapper MAPPER = new ObjectMapper();
 
     public void testChildDeserialization() throws Exception {
-        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         Child child = MAPPER.readValue("{\"b\": {}}", Child.class);
         assertNotNull(child.b);
     }
-
 }

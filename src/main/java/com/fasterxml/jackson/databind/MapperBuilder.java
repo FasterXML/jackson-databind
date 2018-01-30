347c8f5315e717c4017b035d08922f1449100516
Tatu Saloranta <tatu.saloranta@iki.fi>
2018-01-29 22:09:20 -0800

    Start slow buildup of ObjectMapper builder() functionality

--- src/main/java/com/fasterxml/jackson/databind/MapperBuilder.java
+++ src/main/java/com/fasterxml/jackson/databind/MapperBuilder.java
@@ -1,6 +1,9 @@
 package com.fasterxml.jackson.databind;
 
 import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.deser.*;
+import com.fasterxml.jackson.databind.ser.*;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 
 /**
  * Since {@link ObjectMapper} instances are immutable in  Jackson 3.x for full thread-safety,
@@ -10,7 +13,7 @@ import com.fasterxml.jackson.core.*;
  * @since 3.0
  */
 public abstract class MapperBuilder<M extends ObjectMapper,
-    T extends MapperBuilder<M,T>>
+    B extends MapperBuilder<M,B>>
 {
     /*
     /**********************************************************
@@ -25,11 +28,42 @@ public abstract class MapperBuilder<M extends ObjectMapper,
 
     /*
     /**********************************************************
-    /* Various factories
+    /* Factories for framework itself, general
     /**********************************************************
      */
 
+    /**
+     * Underlying stream factory
+     */
     protected final TokenStreamFactory _streamFactory;
+
+    protected TypeFactory _typeFactory;
+
+    /*
+    /**********************************************************
+    /* Factories for framework itself, serialization
+    /**********************************************************
+     */
+    
+    protected SerializerFactory _serializerFactory;
+
+    /**
+     * Prototype {@link SerializerProvider} to use for creating per-operation providers.
+     */
+    protected DefaultSerializerProvider _serializerProvider;
+
+    /*
+    /**********************************************************
+    /* Factories for framework itself, deserialization
+    /**********************************************************
+     */
+
+    protected DeserializerFactory _deserializerFactory;
+    
+    /**
+     * Prototype (about same as factory) to use for creating per-operation contexts.
+     */
+    protected DefaultDeserializationContext _deserializationContext;
 
     /*
     /**********************************************************
@@ -43,18 +77,28 @@ public abstract class MapperBuilder<M extends ObjectMapper,
     /**********************************************************
      */
 
-    protected MapperBuilder(TokenStreamFactory streamFactory) {
+    protected MapperBuilder(TokenStreamFactory streamFactory)
+    {
         _streamFactory = streamFactory;
-//        _mapperFeatures = MapperFeature;
+        _typeFactory = TypeFactory.defaultInstance();
+
+        _serializerFactory = BeanSerializerFactory.instance;
+        _deserializerFactory = BeanDeserializerFactory.instance;
+
+        //        _mapperFeatures = MapperFeature;
     }
 
-    /*
-    protected ObjectMapperBuilder(TokenStreamFactory base)
+    protected MapperBuilder(MapperBuilder<?,?> base)
     {
-        this(base._factoryFeatures,
-                base.getParserFeatures(), base.getGeneratorFeatures());
+        _streamFactory = base._streamFactory;
+        _typeFactory = base._typeFactory;
+
+        _serializerFactory = base._serializerFactory;
+        _serializerProvider = base._serializerProvider;
+
+        _deserializerFactory = base._deserializerFactory;
+        _deserializationContext = base._deserializationContext;
     }
-*/
 
     /**
      * Method to call to create an initialize actual mapper instance
@@ -63,12 +107,51 @@ public abstract class MapperBuilder<M extends ObjectMapper,
 
     /*
     /**********************************************************
-    /* Accessors
+    /* Accessors for framework factories
     /**********************************************************
      */
 
     public TokenStreamFactory streamFactory() {
         return _streamFactory;
+    }
+
+    public TypeFactory getTypeFactory() {
+        return _typeFactory;
+    }
+
+    public SerializerFactory serializerFactory() {
+        return _serializerFactory;
+    }
+
+    public DefaultSerializerProvider serializerProvider() {
+        return (_serializerProvider != null) ? _serializerProvider
+                : defaultSerializerProvider();
+    }
+
+    /**
+     * Overridable method for changing default {@link SerializerProvider} prototype
+     * to use.
+     */
+    protected DefaultSerializerProvider defaultSerializerProvider() {
+        return new DefaultSerializerProvider.Impl(_streamFactory);
+    }
+
+    public DeserializerFactory deserializerFactory() {
+        return _deserializerFactory;
+    }
+
+    protected DefaultDeserializationContext deserializationContext() {
+        return (_deserializationContext != null) ? _deserializationContext
+                : defaultDeserializationContext();
+    }
+
+    /**
+     * Overridable method for changing default {@link SerializerProvider} prototype
+     * to use.
+     */
+    protected DefaultDeserializationContext defaultDeserializationContext() {
+        return new DefaultDeserializationContext.Impl(deserializerFactory(),
+                _streamFactory);
     }
 
     /*
@@ -77,4 +160,44 @@ public abstract class MapperBuilder<M extends ObjectMapper,
     /**********************************************************
      */
 
+    /*
+    /**********************************************************
+    /* Changing factories
+    /**********************************************************
+     */
+
+    public B typeFactory(TypeFactory f) {
+        _typeFactory = f;
+        return _this();
+    }
+
+    public B serializerFactory(SerializerFactory f) {
+        _serializerFactory = f;
+        return _this();
+    }
+
+    public B serializerProvider(DefaultSerializerProvider prov) {
+        _serializerProvider = prov;
+        return _this();
+    }
+
+    public B deserializerFactory(DeserializerFactory f) {
+        _deserializerFactory = f;
+        return _this();
+    }
+
+    protected B deserializationContext(DefaultDeserializationContext ctxt) {
+        _deserializationContext = ctxt;
+        return _this();
+    }
+
+    /*
+    /**********************************************************
+    /* Other helper methods
+    /**********************************************************
+     */
+    
+    // silly convenience cast method we need
+    @SuppressWarnings("unchecked")
+    protected final B _this() { return (B) this; }
 }

ff834690c1df3b90c2f89d362d5c77035820bc88
Tatu Saloranta <tatu.saloranta@iki.fi>
2018-02-03 12:07:26 -0800

    Backporting minor Java 9 fix

--- src/main/java/com/fasterxml/jackson/databind/deser/std/StackTraceElementDeserializer.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/StackTraceElementDeserializer.java
@@ -50,7 +50,8 @@ public class StackTraceElementDeserializer
                     moduleName = p.getText();
                 } else if ("moduleVersion".equals(propName)) {
                     moduleVersion = p.getText();
-                } else if ("declaringClass".equals(propName)) {
+                } else if ("declaringClass".equals(propName)
+                        || "format".equals(propName)) {
                     // 01-Nov-2017: [databind#1794] Not sure if we should but... let's prune it for now
                     ;
                 } else {

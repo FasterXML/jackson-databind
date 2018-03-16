6f3d9bbf4819252ccdeba678b323f796aef05ee6
cowtowncoder <tsaloranta@gmail.com>
2018-03-16 13:44:42 -0700

    fix minor flaw in sub-classing verification

--- src/main/java/com/fasterxml/jackson/databind/ser/BeanSerializerFactory.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/BeanSerializerFactory.java
@@ -93,7 +93,7 @@ public class BeanSerializerFactory
          *    Instead, let's actually just throw an error if this method is called when subtype
          *    has not properly overridden this method; this to indicate problem as soon as possible.
          */
-        ClassUtil.verifyMustOverride(BeanSerializerFactory.class, config, "withConfig");
+        ClassUtil.verifyMustOverride(BeanSerializerFactory.class, this, "withConfig");
         return new BeanSerializerFactory(config);
     }
 

bb870ae3ee5d982a9f0b80596b13a75e80de9f68
Tatu Saloranta <tatu.saloranta@iki.fi>
2018-05-23 14:38:47 -0700

    Fix #2043 (refactoring to avoid mixin lookup)

--- src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedFieldCollector.java
+++ src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedFieldCollector.java
@@ -12,32 +12,26 @@ import com.fasterxml.jackson.databind.util.ClassUtil;
 public class AnnotatedFieldCollector
     extends CollectorBase
 {
-    // // // Configuration
-
-    private final TypeFactory _typeFactory;
     private final MixInResolver _mixInResolver;
 
-    // // // Collected state
-
-    AnnotatedFieldCollector(AnnotationIntrospector intr,
-            TypeFactory types, MixInResolver mixins)
+    AnnotatedFieldCollector(AnnotationIntrospector intr, MixInResolver mixins)
     {
         super(intr);
-        _typeFactory = types;
         _mixInResolver = (intr == null) ? null : mixins;
     }
 
     public static List<AnnotatedField> collectFields(AnnotationIntrospector intr,
-            TypeResolutionContext tc,
-            MixInResolver mixins, TypeFactory types,
-            JavaType type)
+            TypeResolutionContext tc, MixInResolver mixins, TypeFactory typeFactory,
+            JavaType type, Class<?> primaryMixIn)
     {
-        return new AnnotatedFieldCollector(intr, types, mixins).collect(tc, type);
+        return new AnnotatedFieldCollector(intr, mixins).collect(tc, typeFactory,
+                type, primaryMixIn);
     }
 
-    List<AnnotatedField> collect(TypeResolutionContext tc, JavaType type)
+    List<AnnotatedField> collect(TypeResolutionContext tc, TypeFactory typeFactory,
+            JavaType type, Class<?> primaryMixIn)
     {
-        Map<String,FieldBuilder> foundFields = _findFields(tc, type, null);
+        Map<String,FieldBuilder> foundFields = _findFields(tc, typeFactory, type, primaryMixIn, null);
         if (foundFields == null) {
             return Collections.emptyList();
         }
@@ -48,21 +42,26 @@ public class AnnotatedFieldCollector
         return result;
     }
 
-    private Map<String,FieldBuilder> _findFields(TypeResolutionContext tc,
-            JavaType type, Map<String,FieldBuilder> fields)
+    private Map<String,FieldBuilder> _findFields(TypeResolutionContext tc, TypeFactory typeFactory,
+            JavaType type, Class<?> mixin,
+            Map<String,FieldBuilder> fields)
     {
         // First, a quick test: we only care for regular classes (not interfaces,
         //primitive types etc), except for Object.class. A simple check to rule out
         // other cases is to see if there is a super class or not.
-        JavaType parent = type.getSuperClass();
-        if (parent == null) {
+        final JavaType parentType = type.getSuperClass();
+        if (parentType == null) {
             return fields;
         }
-        final Class<?> cls = type.getRawClass();
         // Let's add super-class' fields first, then ours.
-        fields = _findFields(new TypeResolutionContext.Basic(_typeFactory, parent.getBindings()),
-                parent, fields);
-        for (Field f : ClassUtil.getDeclaredFields(cls)) {
+        {
+            Class<?> parentMixin = (_mixInResolver == null) ? null
+                    : _mixInResolver.findMixInClassFor(parentType.getRawClass());
+            fields = _findFields(new TypeResolutionContext.Basic(typeFactory, parentType.getBindings()),
+                    typeFactory, parentType, parentMixin, fields);
+        }
+        final Class<?> rawType = type.getRawClass();
+        for (Field f : ClassUtil.getDeclaredFields(rawType)) {
             // static fields not included (transients are at this point, filtered out later)
             if (!_isIncludableField(f)) {
                 continue;
@@ -80,11 +79,8 @@ public class AnnotatedFieldCollector
             fields.put(f.getName(), b);
         }
         // And then... any mix-in overrides?
-        if (_mixInResolver != null) {
-            Class<?> mixin = _mixInResolver.findMixInClassFor(cls);
-            if (mixin != null) {
-                _addFieldMixIns(mixin, cls, fields);
-            }
+        if (mixin != null) {
+            _addFieldMixIns(mixin, rawType, fields);
         }
         return fields;
     }

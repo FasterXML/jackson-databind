FasterXML/jackson-databind

commit 75098b73d4669da85e2814ab0f7b011facba7b77

Fix #1351

src/main/java/com/fasterxml/jackson/databind/ser/PropertyBuilder.java
@@ -16,16 +16,9 @@ public class PropertyBuilder
 {
     // @since 2.7
     private final static Object NO_DEFAULT_MARKER = Boolean.FALSE;
-    
+
     final protected SerializationConfig _config;
     final protected BeanDescription _beanDesc;
-
-    /**
-     * Default inclusion mode for properties of the POJO for which
-     * properties are collected; possibly overridden on
-     * per-property basis.
-     */
-    final protected JsonInclude.Value _defaultInclusion;
 
     final protected AnnotationIntrospector _annotationIntrospector;
 
@@ -40,14 +33,41 @@ public class PropertyBuilder
      */
     protected Object _defaultBean;
 
+    /**
+     * Default inclusion mode for properties of the POJO for which
+     * properties are collected; possibly overridden on
+     * per-property basis. Combines global inclusion defaults and
+     * per-type (annotation and type-override) inclusion overrides.
+     */
+    final protected JsonInclude.Value _defaultInclusion;
+
+    /**
+     * Marker flag used to indicate that "real" default values are to be used
+     * for properties, as per per-type value inclusion of type <code>NON_DEFAULT</code>
+     *
+     * @since 2.8
+     */
+    final protected boolean _useRealPropertyDefaults;
+    
     public PropertyBuilder(SerializationConfig config, BeanDescription beanDesc)
     {
         _config = config;
         _beanDesc = beanDesc;
-        // NOTE: this includes global defaults and defaults of POJO that contains property,
-        // but not defaults for types of properties referenced
-        _defaultInclusion = beanDesc.findPropertyInclusion(
-                config.getDefaultPropertyInclusion(beanDesc.getBeanClass()));
+        // 08-Sep-2016, tatu: This gets tricky, with 3 levels of definitions:
+        //  (a) global default inclusion
+        //  (b) per-type default inclusion (from annotation or config overrides;
+        //     latter having precedence
+        //  Cc) per-property override
+        //
+        //  and not only requiring merging, but also considering special handling
+        //  for NON_DEFAULT in case of (b) (vs (a) or (c))
+        JsonInclude.Value inclPerType = JsonInclude.Value.merge(
+                beanDesc.findPropertyInclusion(JsonInclude.Value.empty()),
+                config.getDefaultPropertyInclusion(beanDesc.getBeanClass(),
+                        JsonInclude.Value.empty()));
+        _defaultInclusion = JsonInclude.Value.merge(config.getDefaultPropertyInclusion(),
+                inclPerType);
+        _useRealPropertyDefaults = inclPerType.getValueInclusion() == JsonInclude.Include.NON_DEFAULT;
         _annotationIntrospector = _config.getAnnotationIntrospector();
     }
 
@@ -123,8 +143,8 @@ public class PropertyBuilder
             //    so that if enclosing class has this, we may need to access values of property,
             //    whereas for global defaults OR per-property overrides, we have more
             //    static definition. Sigh.
-            // First: case of class specifying it; try to find POJO property defaults
-            if (_defaultInclusion.getValueInclusion() == JsonInclude.Include.NON_DEFAULT) {
+            // First: case of class/type specifying it; try to find POJO property defaults
+            if (_useRealPropertyDefaults) {
                 // 07-Sep-2016, tatu: may also need to front-load access forcing now
                 if (prov.isEnabled(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS)) {
                     am.fixAccess(_config.isEnabled(MapperFeature.OVERRIDE_PUBLIC_ACCESS_MODIFIERS));
@@ -132,6 +152,7 @@ public class PropertyBuilder
                 valueToSuppress = getPropertyDefaultValue(propDef.getName(), am, actualType);
             } else {
                 valueToSuppress = getDefaultValue(actualType);
+                suppressNulls = true;
             }
             if (valueToSuppress == null) {
                 suppressNulls = true;
@@ -140,7 +161,6 @@ public class PropertyBuilder
                     valueToSuppress = ArrayBuilders.getArrayComparator(valueToSuppress);
                 }
             }
-
             break;
         case NON_ABSENT: // new with 2.6, to support Guava/JDK8 Optionals
             // always suppress nulls

src/test/java/com/fasterxml/jackson/databind/filter/JsonIncludeTest.java
@@ -261,4 +261,23 @@ public class JsonIncludeTest
         assertEquals(aposToQuotes("{'map':{}}"),
                 mapper.writeValueAsString(empty));
     }
+
+    static class Issue1351Bean
+    {
+        public final String first;
+        public final double second;
+
+        public Issue1351Bean(String first, double second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    
+    public void testIssue1351() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
+        assertEquals(aposToQuotes("{}"),
+                mapper.writeValueAsString(new Issue1351Bean(null, (double) 0)));
+    }
 }

release-notes/CREDITS
@@ -530,6 +530,10 @@ Andrew Snare (asnare@github)
   * Reported #1315: Binding numeric values can BigDecimal lose precision
    (2.8.2)
 
+Gili Tzabari (cowwoc@github)
+  * Reported #1351: `@JsonInclude(NON_DEFAULT)` doesn't omit null fields
+   (2.8.3)
+
 Connor Kuhn (ckuhn@github)
   * Contributed #1341: FAIL_ON_MISSING_EXTERNAL_TYPE_ID_PROPERTY
    (2.9.0)

release-notes/VERSION
@@ -11,6 +11,8 @@ Project: jackson-databind
 
 2.8.3 (not yet released)
 
+#1351: `@JsonInclude(NON_DEFAULT)` doesn't omit null fields
+ (reported by Gili T)
 #1353: Improve error-handling for `java.net.URL` deserialization
 #1361: Change `TokenBuffer` to use new `writeEmbeddedObject()` if possible
 

src/main/java/com/fasterxml/jackson/databind/BeanDescription.java
@@ -195,6 +195,12 @@ public abstract class BeanDescription
      */
 
     /**
+     * Method for finding annotation-indicated inclusion definition (if any);
+     * possibly overriding given default value.
+     *<p>
+     * NOTE: does NOT use global inclusion default settings as the base, unless
+     * passed as `defValue`.
+     *
      * @since 2.7
      */
     public abstract JsonInclude.Value findPropertyInclusion(JsonInclude.Value defValue);

src/main/java/com/fasterxml/jackson/databind/introspect/BasicBeanDescription.java
@@ -391,8 +391,7 @@ public class BasicBeanDescription extends BeanDescription
         if (_annotationIntrospector != null) {
             JsonInclude.Value incl = _annotationIntrospector.findPropertyInclusion(_classInfo);
             if (incl != null) {
-                return (defValue == null) ? incl
-                        : defValue.withOverrides(incl);
+                return (defValue == null) ? incl : defValue.withOverrides(incl);
             }
         }
         return defValue;

commit 022cd19d2bb77bd739654037fc00f8fcef603959

Fix #1757 and #2105: honor global default inclusion setting
This fixes a regression that was introduced with 75098b73d4669da85e2814ab0f7b011facba7b77.

src/main/java/com/fasterxml/jackson/databind/ser/PropertyBuilder.java
@@ -69,7 +69,7 @@ public class PropertyBuilder
                         JsonInclude.Value.empty()));
         _defaultInclusion = JsonInclude.Value.merge(config.getDefaultPropertyInclusion(),
                 inclPerType);
-        _useRealPropertyDefaults = inclPerType.getValueInclusion() == JsonInclude.Include.NON_DEFAULT;
+        _useRealPropertyDefaults = _defaultInclusion.getValueInclusion() == JsonInclude.Include.NON_DEFAULT;
         _annotationIntrospector = _config.getAnnotationIntrospector();
     }
 

src/test/java/com/fasterxml/jackson/databind/ser/filter/JsonIncludeTest.java
@@ -60,7 +60,31 @@ public class JsonIncludeTest
             this.z = z;
         }
     }
-    
+
+    // [databind#1757], [databind#2105]
+    @JsonPropertyOrder({ "a", "b", "c" })
+    static class NonDefaultBeanWithoutIncludeAnnotation
+    {
+        String a = "a";
+        boolean b = true;
+        Integer c = 123;
+        Boolean d;
+
+        NonDefaultBeanWithoutIncludeAnnotation() { }
+
+        NonDefaultBeanWithoutIncludeAnnotation(String a, boolean b, Integer c, Boolean d) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+
+        public String getA() { return a; }
+        public boolean isB() { return b; }
+        public Integer getC() { return c; }
+        public Boolean getD() { return d; }
+    }
+
     @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     static class MixedBean
     {
@@ -340,4 +364,28 @@ public class JsonIncludeTest
         assertEquals("{}", 
                 MAPPER.writeValueAsString(new NonDefaultCalendar(input)));
     }
+
+    // [databind#1757], [databind#2105]
+    public void testNonDefaultInclusion() throws IOException {
+        ObjectMapper nonDefaultMapper = new ObjectMapper();
+        nonDefaultMapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
+
+        assertEquals(aposToQuotes("{'a':'a','b':true,'c':123,'d':null}"),
+                MAPPER.writeValueAsString(new NonDefaultBeanWithoutIncludeAnnotation()));
+
+        assertEquals(aposToQuotes("{}"),
+                nonDefaultMapper.writeValueAsString(new NonDefaultBeanWithoutIncludeAnnotation()));
+
+        assertEquals(aposToQuotes("{'a':'x','b':false,'c':456,'d':true}"),
+                nonDefaultMapper.writeValueAsString(
+                        new NonDefaultBeanWithoutIncludeAnnotation("x", false, 456, true)));
+
+        ObjectReader reader = nonDefaultMapper.readerFor(NonDefaultBeanWithoutIncludeAnnotation.class);
+        NonDefaultBeanWithoutIncludeAnnotation defaultValue = reader.readValue("{}");
+        assertEquals("a", defaultValue.getA());
+        assertTrue(defaultValue.isB());
+        assertEquals(Integer.valueOf(123), defaultValue.getC());
+        assertNull(defaultValue.getD());
+    }
+
 }
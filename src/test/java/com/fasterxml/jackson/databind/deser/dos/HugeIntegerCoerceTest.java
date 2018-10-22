a66bd0d4343638c30afd5c7513a6cc414b7d70b3
Tatu Saloranta <tatu.saloranta@iki.fi>
2018-10-21 20:00:54 -0700

    first test for #2157 (fix in `jackson-core` tho)

---
+++ src/test/java/com/fasterxml/jackson/databind/deser/dos/HugeIntegerCoerceTest.java
@@ -0,0 +1,35 @@
+package com.fasterxml.jackson.databind.deser.dos;
+
+import com.fasterxml.jackson.core.*;
+
+import com.fasterxml.jackson.databind.*;
+
+// for [databind#2157]
+public class HugeIntegerCoerceTest extends BaseMapTest
+{
+    private final static int BIG_NUM_LEN = 199999;
+    private final static String BIG_POS_INTEGER;
+    static {
+        StringBuilder sb = new StringBuilder(BIG_NUM_LEN);
+        for (int i = 0; i < BIG_NUM_LEN; ++i) {
+            sb.append('9');
+        }
+        BIG_POS_INTEGER = sb.toString();
+    }
+
+    private final ObjectMapper MAPPER = objectMapper(); // shared is fine
+    
+    public void testMaliciousLongForEnum() throws Exception
+    {
+        // Note: due to [jackson-core#488], fix verified with streaming over multiple
+        // parser types. Here we focus on databind-level
+        
+        try {
+            /*ABC value =*/ MAPPER.readValue(BIG_POS_INTEGER, ABC.class);
+            fail("Should not pass");
+        } catch (JsonParseException e) {
+            verifyException(e, "out of range of int");
+            verifyException(e, "Integer with "+BIG_NUM_LEN+" digits");
+        }
+    }    
+}

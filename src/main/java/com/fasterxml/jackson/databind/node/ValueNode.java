69d1f572677519b949e9ff65b762617e514a47bf
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-01-11 20:13:38 -0800

    Minor cleanup wrt addition of `missingNode()` factory method

--- src/main/java/com/fasterxml/jackson/databind/node/ValueNode.java
+++ src/main/java/com/fasterxml/jackson/databind/node/ValueNode.java
@@ -17,13 +17,15 @@ import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 public abstract class ValueNode
     extends BaseJsonNode
 {
+    protected final static JsonNode MISSING = MissingNode.getInstance();
+
     protected ValueNode() { }
 
     @Override
     protected JsonNode _at(JsonPointer ptr) {
         // will only allow direct matches, but no traversal through
         // (base class checks for direct match)
-        return MissingNode.getInstance();
+        return MISSING;
     }
 
     /**
@@ -66,7 +68,7 @@ public abstract class ValueNode
     public final JsonNode get(int index) { return null; }
 
     @Override
-    public final JsonNode path(int index) { return MissingNode.getInstance(); }
+    public final JsonNode path(int index) { return MISSING; }
 
     @Override
     public final boolean has(int index) { return false; }
@@ -78,7 +80,7 @@ public abstract class ValueNode
     public final JsonNode get(String fieldName) { return null; }
 
     @Override
-    public final JsonNode path(String fieldName) { return MissingNode.getInstance(); }
+    public final JsonNode path(String fieldName) { return MISSING; }
 
     @Override
     public final boolean has(String fieldName) { return false; }

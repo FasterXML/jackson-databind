e9f5f4d1c10d190c5caae4ac8968642f648b5b76
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-09-04 16:50:00 -0700

    ...

--- src/test/java/com/fasterxml/jackson/failing/Alias2378Test.java
+++ src/test/java/com/fasterxml/jackson/failing/Alias2378Test.java
@@ -1,11 +1,8 @@
 package com.fasterxml.jackson.failing;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import com.fasterxml.jackson.databind.BaseMapTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,24 +10,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 // for [databind#2378]
 public class Alias2378Test extends BaseMapTest
 {
-    @JsonTypeName("user")
     static abstract class UserEventContext {
-
-         public abstract String getId();
+        public abstract String getId();
 
         public abstract String getPartitionId();
 
-        public abstract String getAccountId();
-
-        public abstract AtomicReference<String> getDeviceId();
-
         @JsonCreator
         public static UserEventContext create(@JsonProperty("partitionId") String partitionId,
-                @JsonProperty("accountId") String accountId,
-                @JsonProperty("id") @JsonAlias("userId") String userId,
-                @JsonProperty("deviceId") String deviceId) {
-            return new AutoValue_UserEventContext(userId, partitionId, accountId,
-                    new AtomicReference<>(deviceId));
+                @JsonProperty("id") @JsonAlias("userId") String userId) {
+            return new AutoValue_UserEventContext(userId, partitionId);
         }
     }
 
@@ -40,31 +28,15 @@ public class Alias2378Test extends BaseMapTest
 
         private final String partitionId;
 
-        private final String accountId;
-
-        private final AtomicReference<String> deviceId;
-
-        AutoValue_UserEventContext(
-            String id,
-            String partitionId,
-            String accountId,
-            AtomicReference<String> deviceId) {
+        AutoValue_UserEventContext(String id, String partitionId) {
           if (id == null) {
-            throw new NullPointerException("Null id");
+              throw new NullPointerException("Null id");
           }
           this.id = id;
           if (partitionId == null) {
-            throw new NullPointerException("Null partitionId");
+              throw new NullPointerException("Null partitionId");
           }
           this.partitionId = partitionId;
-          if (accountId == null) {
-            throw new NullPointerException("Null accountId");
-          }
-          this.accountId = accountId;
-          if (deviceId == null) {
-            throw new NullPointerException("Null deviceId");
-          }
-          this.deviceId = deviceId;
         }
 
         @Override
@@ -76,16 +48,6 @@ public class Alias2378Test extends BaseMapTest
         public String getPartitionId() {
           return partitionId;
         }
-
-        @Override
-        public String getAccountId() {
-          return accountId;
-        }
-
-        @Override
-        public AtomicReference<String> getDeviceId() {
-          return deviceId;
-        }
       }
 
     private final ObjectMapper MAPPER = newJsonMapper();
@@ -93,9 +55,8 @@ public class Alias2378Test extends BaseMapTest
     public void testIssue2378() throws Exception
     {
         UserEventContext value = MAPPER.readValue(
-                aposToQuotes(
-//"{'userId' : 'abc', 'partitionId' : '123', 'accountId' : '1', 'deviceId':'x' }"
-"{'id' : 'abc', 'partitionId' : '123', 'accountId' : '1', 'deviceId':'x' }"
+                aposToQuotes("{'userId' : 'abc', 'partitionId' : '123' }"
+//                aposToQuotes("{'id' : 'abc', 'partitionId' : '123' }"
                 ), UserEventContext.class);
         assertNotNull(value);
     }

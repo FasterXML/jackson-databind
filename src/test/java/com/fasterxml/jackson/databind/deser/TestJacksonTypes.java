e07a706edf5d54b47464aab98d88f1f53210fcff
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-09-15 17:03:01 -0700

    Test refactoring

--- src/test/java/com/fasterxml/jackson/databind/deser/TestJacksonTypes.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestJacksonTypes.java
@@ -13,14 +13,15 @@ import com.fasterxml.jackson.databind.util.TokenBuffer;
 public class TestJacksonTypes
     extends com.fasterxml.jackson.databind.BaseMapTest
 {
+    private final ObjectMapper MAPPER = sharedMapper();
+
     public void testJsonLocation() throws Exception
     {
-        ObjectMapper m = new ObjectMapper();
         // note: source reference is untyped, only String guaranteed to work
         JsonLocation loc = new JsonLocation("whatever",  -1, -1, 100, 13);
         // Let's use serializer here; goal is round-tripping
-        String ser = serializeAsString(m, loc);
-        JsonLocation result = m.readValue(ser, JsonLocation.class);
+        String ser = MAPPER.writeValueAsString(loc);
+        JsonLocation result = MAPPER.readValue(ser, JsonLocation.class);
         assertNotNull(result);
         assertEquals(loc.getSourceRef(), result.getSourceRef());
         assertEquals(loc.getByteOffset(), result.getByteOffset());
@@ -35,10 +36,23 @@ public class TestJacksonTypes
         JsonLocation loc = new JsonLocation(null,  -1, -1, 100, 13);
         assertTrue(loc.equals(loc));
         assertFalse(loc.equals(null));
-        assertFalse(loc.equals("abx"));
+        final Object value = "abx";
+        assertFalse(loc.equals(value));
 
         // should we check it's not 0?
         loc.hashCode();
+    }
+
+    public void testJavaType() throws Exception
+    {
+        TypeFactory tf = TypeFactory.defaultInstance();
+        // first simple type:
+        String json = MAPPER.writeValueAsString(tf.constructType(String.class));
+        assertEquals(quote(java.lang.String.class.getName()), json);
+        // and back
+        JavaType t = MAPPER.readValue(json, JavaType.class);
+        assertNotNull(t);
+        assertEquals(String.class, t.getRawClass());
     }
 
     /**
@@ -47,9 +61,8 @@ public class TestJacksonTypes
      */
     public void testTokenBufferWithSample() throws Exception
     {
-        ObjectMapper m = new ObjectMapper();
         // First, try standard sample doc:
-        TokenBuffer result = m.readValue(SAMPLE_DOC_JSON_SPEC, TokenBuffer.class);
+        TokenBuffer result = MAPPER.readValue(SAMPLE_DOC_JSON_SPEC, TokenBuffer.class);
         verifyJsonSpecSampleDoc(result.asParser(), true);
         result.close();
     }
@@ -57,13 +70,12 @@ public class TestJacksonTypes
     @SuppressWarnings("resource")
     public void testTokenBufferWithSequence() throws Exception
     {
-        ObjectMapper m = new ObjectMapper();
         // and then sequence of other things
         JsonParser jp = createParserUsingReader("[ 32, [ 1 ], \"abc\", { \"a\" : true } ]");
         assertToken(JsonToken.START_ARRAY, jp.nextToken());
 
         assertToken(JsonToken.VALUE_NUMBER_INT, jp.nextToken());
-        TokenBuffer buf = m.readValue(jp, TokenBuffer.class);
+        TokenBuffer buf = MAPPER.readValue(jp, TokenBuffer.class);
 
         // check manually...
         JsonParser bufParser = buf.asParser();
@@ -72,7 +84,7 @@ public class TestJacksonTypes
         assertNull(bufParser.nextToken());
 
         // then bind to another
-        buf = m.readValue(jp, TokenBuffer.class);
+        buf = MAPPER.readValue(jp, TokenBuffer.class);
         bufParser = buf.asParser();
         assertToken(JsonToken.START_ARRAY, bufParser.nextToken());
         assertToken(JsonToken.VALUE_NUMBER_INT, bufParser.nextToken());
@@ -81,13 +93,13 @@ public class TestJacksonTypes
         assertNull(bufParser.nextToken());
 
         // third one, with automatic binding
-        buf = m.readValue(jp, TokenBuffer.class);
-        String str = m.readValue(buf.asParser(), String.class);
+        buf = MAPPER.readValue(jp, TokenBuffer.class);
+        String str = MAPPER.readValue(buf.asParser(), String.class);
         assertEquals("abc", str);
 
         // and ditto for last one
-        buf = m.readValue(jp, TokenBuffer.class);
-        Map<?,?> map = m.readValue(buf.asParser(), Map.class);
+        buf = MAPPER.readValue(jp, TokenBuffer.class);
+        Map<?,?> map = MAPPER.readValue(buf.asParser(), Map.class);
         assertEquals(1, map.size());
         assertEquals(Boolean.TRUE, map.get("a"));
         
@@ -95,16 +107,42 @@ public class TestJacksonTypes
         assertNull(jp.nextToken());
     }
 
-    public void testJavaType() throws Exception
+    // 10k does it, 5k not, but use bit higher values just in case
+    private final static int RECURSION_2398 = 25000;
+
+    // [databind#2398]
+    public void testDeeplyNestedArrays() throws Exception
     {
-        ObjectMapper mapper = new ObjectMapper();
-        TypeFactory tf = TypeFactory.defaultInstance();
-        // first simple type:
-        String json = mapper.writeValueAsString(tf.constructType(String.class));
-        assertEquals(quote(java.lang.String.class.getName()), json);
-        // and back
-        JavaType t = mapper.readValue(json, JavaType.class);
-        assertNotNull(t);
-        assertEquals(String.class, t.getRawClass());
+        try (JsonParser p = MAPPER.tokenStreamFactory().createParser(_createNested(RECURSION_2398 * 2,
+                "[", " 123 ", "]"))) {
+            p.nextToken();
+            TokenBuffer b = new TokenBuffer(p);
+            b.copyCurrentStructure(p);
+            b.close();
+        }
+    }
+
+    public void testDeeplyNestedObjects() throws Exception
+    {
+        try (JsonParser p = MAPPER.tokenStreamFactory().createParser(_createNested(RECURSION_2398,
+                "{\"a\":", "42", "}"))) {
+            p.nextToken();
+            TokenBuffer b = new TokenBuffer(p);
+            b.copyCurrentStructure(p);
+            b.close();
+        }
+    }
+
+    private String _createNested(int nesting, String open, String middle, String close) 
+    {
+        StringBuilder sb = new StringBuilder(2 * nesting);
+        for (int i = 0; i < nesting; ++i) {
+            sb.append(open);
+        }
+        sb.append(middle);
+        for (int i = 0; i < nesting; ++i) {
+            sb.append(close);
+        }
+        return sb.toString();
     }
 }

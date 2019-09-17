f49de7f0a64746663f1a3ae3b12da63838e81d07
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-09-17 12:10:53 -0700

    ...

--- src/test/java/com/fasterxml/jackson/databind/util/UnlimitedLookupCacheTest.java
+++ src/test/java/com/fasterxml/jackson/databind/util/UnlimitedLookupCacheTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class UnlimitedLookupCacheTest {

3ac186c506f4180e22ac4e75b7cfd5bc5e6c6c2c
Joe Koshakow <koshy44@gmail.com>
2019-10-25 00:14:05 -0400

    Allow registering the same subtype multiple times
    Before this commit NamedType hashes only on it's class and not on the
    name. This only allowed you to register a class once using
    ObjectMapper.registerSubtypes(NamedType... types). This commit now
    uses name field to hash NamedTypes.
    
    This successfully allows you to deserialize objects of the same type
    but different name.
    
    Serializing objects of the same type but different name (for example
    fields of a POJO) still has some issues.
    1. SerializerProvider caches Serializers based on class and doesn't
    take name into account.
    2. TypeIdResolver has no method to resolve an id that takes name into
    account. Therefore when resolving an id we only have the value and
    type.
    3. TypeNameIdResolver stores type and id information as a
    Map<String, String> that maps type to id, ignoring name
    
    Fixes #2515

--- src/main/java/com/fasterxml/jackson/databind/jsontype/impl/StdSubtypeResolver.java
+++ src/main/java/com/fasterxml/jackson/databind/jsontype/impl/StdSubtypeResolver.java
@@ -112,7 +112,8 @@ public class StdSubtypeResolver
             AnnotatedClass type)
     {
         final AnnotationIntrospector ai = config.getAnnotationIntrospector();
-        HashMap<NamedType, NamedType> subtypes = new HashMap<NamedType, NamedType>();
+        HashMap<NamedType, NamedType> subtypes = new HashMap<>();
+
         // then consider registered subtypes (which have precedence over annotations)
         if (_registeredSubtypes != null) {
             Class<?> rawBase = type.getRawType();
@@ -223,19 +224,23 @@ public class StdSubtypeResolver
             }
         }
 
+        //For Serialization we only want to return a single NamedType per class so it's
+        //unambiguous what name we use.
+        NamedType typeOnlyNamedType = new NamedType(namedType.getType());
+
         // First things first: is base type itself included?
-        if (collectedSubtypes.containsKey(namedType)) {
+        if (collectedSubtypes.containsKey(typeOnlyNamedType)) {
             // if so, no recursion; however, may need to update name?
             if (namedType.hasName()) {
-                NamedType prev = collectedSubtypes.get(namedType);
+                NamedType prev = collectedSubtypes.get(typeOnlyNamedType);
                 if (!prev.hasName()) {
-                    collectedSubtypes.put(namedType, namedType);
+                    collectedSubtypes.put(typeOnlyNamedType, namedType);
                 }
             }
             return;
         }
         // if it wasn't, add and check subtypes recursively
-        collectedSubtypes.put(namedType, namedType);
+        collectedSubtypes.put(typeOnlyNamedType, namedType);
         Collection<NamedType> st = ai.findSubtypes(annotatedType);
         if (st != null && !st.isEmpty()) {
             for (NamedType subtype : st) {

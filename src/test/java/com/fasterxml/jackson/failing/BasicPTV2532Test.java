0e644b8f15f4a96135a0715bf7a71d0b7e3cd1ea
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-11-04 21:53:02 -0800

    ... test changes

--- src/test/java/com/fasterxml/jackson/failing/BasicPTV2532Test.java
+++ src/test/java/com/fasterxml/jackson/failing/BasicPTV2532Test.java
@@ -13,48 +13,27 @@ import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
  * Tests for the main user-configurable {@code PolymorphicTypeValidator},
  * {@link BasicPolymorphicTypeValidator}.
  */
-public class BasicPTV2523Test extends BaseMapTest
+public class BasicPTV2532Test extends BaseMapTest
 {
     // // // Value types
 
-    static abstract class BaseValue {
+    static abstract class Base2532 {
         public int x = 3;
     }
 
-    static class ValueA extends BaseValue {
-        protected ValueA() { }
-        public ValueA(int x) {
+    static class Good2532 extends Base2532 {
+        protected Good2532() { }
+        public Good2532(int x) {
             super();
             this.x = x;
         }
     }
 
-    static class ValueB extends BaseValue {
-        protected ValueB() { }
-        public ValueB(int x) {
+    static class Bad2532 extends Base2532 {
+        protected Bad2532() { }
+        public Bad2532(int x) {
             super();
             this.x = x;
-        }
-    }
-
-    // // // Value types
-
-    // make this type `final` to avoid polymorphic handling
-    static final class BaseValueWrapper {
-        public BaseValue value;
-
-        protected BaseValueWrapper() { }
-
-        public static BaseValueWrapper withA(int x) {
-            BaseValueWrapper w = new BaseValueWrapper();
-            w.value = new ValueA(x);
-            return w;
-        }
-
-        public static BaseValueWrapper withB(int x) {
-            BaseValueWrapper w = new BaseValueWrapper();
-            w.value = new ValueB(x);
-            return w;
         }
     }
 
@@ -65,53 +44,49 @@ public class BasicPTV2523Test extends BaseMapTest
         public ObjectWrapper(Object v) { value = v; }
     }
 
-    static final class NumberWrapper {
-        public Number value;
-
-        protected NumberWrapper() { }
-        public NumberWrapper(Number v) { value = v; }
-    }
-
     /*
     /**********************************************************************
     /* Test methods: structured types
     /**********************************************************************
      */
 
-    // [databind#2523]: handle Java array-types appropriately wrt validation
+    // [databind#2532]: handle Java array-types appropriately wrt validation
     public void testAllowBySubClassInArray() throws Exception {
-        final PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-//                .allowIfSubType(BaseValue[].class)
-                .allowIfSubType(BaseValue.class)
-                .allowIfSubType(ValueB.class)
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Good2532.class)
                 .build();
         ObjectMapper mapper = jsonMapperBuilder()
                 .activateDefaultTyping(ptv, DefaultTyping.NON_FINAL)
                 .build();        
 
-        // First, test accepted case
-        final ObjectWrapper input = new ObjectWrapper(new BaseValue[] { new ValueB(42) });
+        final ObjectWrapper input = new ObjectWrapper(new Base2532[] { new Good2532(42) });
         final String json = mapper.writeValueAsString(input);
-System.err.println("DEBUG: json == "+json);
+
+System.err.println("JSON: "+json);
+        // First test blocked case:
+        try {
+            mapper.readValue(json, ObjectWrapper.class);
+            fail("Should not pass");
+        } catch (InvalidTypeIdException e) {
+            verifyException(e, "Could not resolve type id '[Lcom.fasterxml.jackson.");
+            verifyException(e, "as a subtype of");
+        }
+
+        // and then accepted:
+        ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Good2532.class)
+                .build();
+        mapper = jsonMapperBuilder()
+                .activateDefaultTyping(ptv, DefaultTyping.NON_FINAL)
+                .build();        
+
         ObjectWrapper w = mapper.readValue(json, ObjectWrapper.class);
         assertNotNull(w);
         assertNotNull(w.value);
-        assertEquals(BaseValue[].class, w.value.getClass());
-        BaseValue[] arrayOut = (BaseValue[]) w.value;
+        assertEquals(Base2532[].class, w.value.getClass());
+        Base2532[] arrayOut = (Base2532[]) w.value;
         assertEquals(1, arrayOut.length);
 
         assertEquals(42, arrayOut[0].x);
-
-        // then non-accepted
-        try {
-            final ObjectWrapper input2 = new ObjectWrapper(new BaseValue[] { new ValueA(43) });
-            ObjectWrapper output2 = mapper.readValue(mapper.writeValueAsString(input2),
-                    ObjectWrapper.class);
-System.err.println("Output2 == "+output2);
-            fail("Should not pass");
-        } catch (InvalidTypeIdException e) {
-            verifyException(e, "Could not resolve type id 'com.fasterxml.jackson.");
-            verifyException(e, "as a subtype of");
-        }
     }
 }

c535955a24971ae14e84c9e1c69c3d3652d66d24
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-12-06 14:12:49 -0800

    ...

--- src/main/java/com/fasterxml/jackson/databind/jsontype/NoneShallPassValidator.java
+++ src/main/java/com/fasterxml/jackson/databind/jsontype/NoneShallPassValidator.java
@@ -53,7 +53,9 @@ public class NoneShallPassValidator
 
         // But aside from that actually only allow if base type not too generic
         final Class<?> rawBase = baseType.getRawClass();
-        if ((rawBase == Object.class)) {
+        if ((rawBase == Object.class)
+                // 06-Dec-2019, tatu: !!! TODO: maybe `java.io.Serializable`, `java.lang.Cloneable` too?
+                ) {
             return Validity.DENIED;
         }
 

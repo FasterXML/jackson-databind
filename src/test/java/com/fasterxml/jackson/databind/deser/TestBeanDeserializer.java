20a119c810928a03c4e8c682ea2176ff4481eec3
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-06-03 19:53:24 -0700

    Test refactoring

--- src/test/java/com/fasterxml/jackson/databind/deser/TestBeanDeserializer.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestBeanDeserializer.java
@@ -322,7 +322,7 @@ public class TestBeanDeserializer extends BaseMapTest
     /********************************************************
      */
 
-    private final ObjectMapper MAPPER = new ObjectMapper();
+    private final ObjectMapper MAPPER = newJsonMapper();
 
     /**
      * Test to verify details of how trying to deserialize into
@@ -368,25 +368,6 @@ public class TestBeanDeserializer extends BaseMapTest
 
         // there are 2 properties
         assertEquals(2, Issue476Deserializer.propCount);
-    }
-
-    public void testPOJOFromEmptyString() throws Exception
-    {
-        // first, verify default settings which do not accept empty String:
-        assertFalse(MAPPER.isEnabled(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT));
-        try {
-            MAPPER.readValue(quote(""), Bean.class);
-            fail("Should not accept Empty String for POJO");
-        } catch (JsonProcessingException e) {
-            verifyException(e, "Cannot deserialize value of type ");
-            verifyException(e, " from empty String ");
-            assertValidLocation(e.getLocation());
-        }
-        // should be ok to enable dynamically
-        ObjectReader r = MAPPER.readerFor(Bean.class)
-                .with(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-        Bean result = r.readValue(quote(""));
-        assertNull(result);
     }
 
     // [databind#120]

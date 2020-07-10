1369c51957f81470579272c962f1ca755e51470f
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-07-09 21:00:23 -0700

    Fixed #2784 (and #2644)

--- src/test/java/com/fasterxml/jackson/databind/deser/jdk/JDKNumberDeserTest.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/jdk/JDKNumberDeserTest.java
@@ -7,7 +7,10 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.*;
+
 import com.fasterxml.jackson.core.*;
+
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
@@ -35,6 +38,42 @@ public class JDKNumberDeserTest extends BaseMapTest
         public BigDecimal decimal;
         public MyBeanValue() { this(null); }
         public MyBeanValue(BigDecimal d) { this.decimal = d; }
+    }
+
+    // [databind#2644]
+    static class NodeRoot2644 {
+        public String type;
+
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "type")
+        @JsonSubTypes(value = {
+                @JsonSubTypes.Type(value = NodeParent2644.class, name = "NodeParent")
+        })
+        public Node2644 node;
+    }
+
+    public static class NodeParent2644 extends Node2644 { }
+
+    public static abstract class Node2644 {
+        @JsonProperty("amount")
+        BigDecimal val;
+
+        public BigDecimal getVal() {
+            return val;
+        }
+
+        public void setVal(BigDecimal val) {
+            this.val = val;
+        }
+    }
+
+    // [databind#2784]
+    static class BigDecimalHolder2784 {
+        public BigDecimal value;
+    }
+
+    static class NestedBigDecimalHolder2784 {
+        @JsonUnwrapped
+        public BigDecimalHolder2784 holder;
     }
 
     /*
@@ -298,5 +337,29 @@ public class JDKNumberDeserTest extends BaseMapTest
             fail("Expected LongNode, got: "+node.getClass().getName());
         }
         assertEquals(42, node.asInt());
+    }
+
+    // [databind#2644]
+    public void testBigDecimalSubtypes() throws Exception
+    {
+        ObjectMapper mapper = jsonMapperBuilder()
+                .registerSubtypes(NodeParent2644.class)
+                .build();
+        NodeRoot2644 root = mapper.readValue(
+                "{\"type\": \"NodeParent\",\"node\": {\"amount\": 9999999999999999.99} }",
+                NodeRoot2644.class
+        );
+
+        assertEquals(new BigDecimal("9999999999999999.99"), root.node.getVal());
+    }
+
+    // [databind#2784]
+    public void testBigDecimalUnwrapped() throws Exception
+    {
+        final ObjectMapper mapper = newJsonMapper();
+        // mapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+        final String JSON = "{\"value\": 5.00}";
+        NestedBigDecimalHolder2784 result = mapper.readValue(JSON, NestedBigDecimalHolder2784.class);
+        assertEquals(new BigDecimal("5.00"), result.holder.value);
     }
 }

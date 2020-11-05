dd8d755f6c828c2300ea69d5b517bbd8c5b9165c
Carter Kozak <ckozak@ckozak.net>
2020-11-04 22:20:33 -0500

    Remove java8 API invocation from AnnotatedConstructor (#2918)

--- src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedConstructor.java
+++ src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedConstructor.java
@@ -165,7 +165,8 @@ public final class AnnotatedConstructor
 
     @Override
     public String toString() {
-        final int argCount = _constructor.getParameterCount();
+        // 03-Nov-2020 ckozak: This can use _constructor.getParameterCount() once java 8 is required.
+        final int argCount = _constructor.getParameterTypes().length;
         return String.format("[constructor for %s (%d arg%s), annotations: %s",
                 ClassUtil.nameOf(_constructor.getDeclaringClass()), argCount,
                 (argCount == 1) ? "" : "s", _annotations);

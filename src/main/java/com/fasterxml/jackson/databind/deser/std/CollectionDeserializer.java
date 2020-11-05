FasterXML/jackson-databind

commit fb72f93f4f4503caf22b693c8df1441c70341662

Complete refactoring to move empty-String-defaulting out of (Std)ValueInstantiator

src/main/java/com/fasterxml/jackson/databind/deser/std/CollectionDeserializer.java
@@ -247,10 +247,7 @@ _containerType,
         // there is also possibility of "auto-wrapping" of single-element arrays.
         // Hence we only accept empty String here.
         if (p.hasToken(JsonToken.VALUE_STRING)) {
-            String str = p.getText();
-            if (str.length() == 0) {
-                return (Collection<Object>) _valueInstantiator.createFromString(ctxt, str);
-            }
+            return _deserializeFromString(p, ctxt);
         }
         return handleNonArray(p, ctxt, createDefaultInstance(ctxt));
     }

src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerBase.java
@@ -1424,7 +1424,7 @@ public abstract class BeanDeserializerBase
                 return bean;
             }
         }
-        return _valueInstantiator.createFromString(ctxt, p.getText());
+        return _deserializeFromString(p, ctxt);
     }
 
     /**

src/main/java/com/fasterxml/jackson/databind/deser/ValueInstantiator.java
@@ -258,9 +258,12 @@ public abstract class ValueInstantiator
     /* (String, Number, Boolean)
     /**********************************************************
      */
-    
+
     public Object createFromString(DeserializationContext ctxt, String value) throws IOException {
-        return _createFromStringFallbacks(ctxt, value);
+        return ctxt.handleMissingInstantiator(getValueClass(), this, ctxt.getParser(),
+                "no String-argument constructor/factory method to deserialize from String value ('%s')",
+                value);
+
     }
 
     public Object createFromInt(DeserializationContext ctxt, int value) throws IOException {
@@ -344,7 +347,9 @@ public abstract class ValueInstantiator
 
     /**
      * @since 2.4 (demoted from <code>StdValueInstantiator</code>)
+     * @deprecated Since 2.12 should not handle coercions here
      */
+    @Deprecated // since 2.12
     protected Object _createFromStringFallbacks(DeserializationContext ctxt, String value)
             throws IOException
     {

src/main/java/com/fasterxml/jackson/databind/deser/std/ContainerDeserializerBase.java
@@ -126,14 +126,6 @@ public abstract class ContainerDeserializerBase<T>
      */
     public abstract JsonDeserializer<Object> getContentDeserializer();
 
-    /**
-     * @since 2.9
-     */
-    @Override
-    public ValueInstantiator getValueInstantiator() {
-        return null;
-    }
-
     @Override // since 2.9
     public AccessPattern getEmptyAccessPattern() {
         // 02-Feb-2017, tatu: Empty containers are usually constructed as needed

src/main/java/com/fasterxml/jackson/databind/deser/std/EnumMapDeserializer.java
@@ -217,7 +217,11 @@ public class EnumMapDeserializer
         return _valueDeserializer;
     }
 
-    // Must override since we do not expose ValueInstantiator
+    @Override
+    public ValueInstantiator getValueInstantiator() {
+        return _valueInstantiator;
+    }
+
     @Override // since 2.9
     public Object getEmptyValue(DeserializationContext ctxt) throws JsonMappingException {
         return constructMap(ctxt);
@@ -248,7 +252,7 @@ public class EnumMapDeserializer
         }
         // (empty) String may be ok however; or single-String-arg ctor
         if (t == JsonToken.VALUE_STRING) {
-            return (EnumMap<?,?>) _valueInstantiator.createFromString(ctxt, p.getText());
+            return _deserializeFromString(p, ctxt);
         }
         // Empty array, or single-value wrapped in array?
         if (t == JsonToken.START_ARRAY) {

src/main/java/com/fasterxml/jackson/databind/deser/std/MapDeserializer.java
@@ -370,7 +370,7 @@ public class MapDeserializer
 
         // (empty) String may be ok however; or single-String-arg ctor
         if (t == JsonToken.VALUE_STRING) {
-            return (Map<Object,Object>) _valueInstantiator.createFromString(ctxt, p.getText());
+            return _deserializeFromString(p, ctxt);
         }
         // Empty array, or single-value wrapped in array?
         if (t == JsonToken.START_ARRAY) {

src/main/java/com/fasterxml/jackson/databind/deser/std/MapEntryDeserializer.java
@@ -162,7 +162,10 @@ public class MapEntryDeserializer
     public JsonDeserializer<Object> getContentDeserializer() {
         return _valueDeserializer;
     }
-    
+
+    // 31-May-2020, tatu: Should probably define but we don't have it yet
+//    public ValueInstantiator getValueInstantiator() { }
+
     /*
     /**********************************************************
     /* JsonDeserializer API

src/main/java/com/fasterxml/jackson/databind/deser/std/StdDeserializer.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.core.io.NumberInput;
 
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JacksonStdImpl;
+import com.fasterxml.jackson.databind.cfg.CoercionAction;
+import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerBase;
 import com.fasterxml.jackson.databind.deser.NullValueProvider;
 import com.fasterxml.jackson.databind.deser.SettableBeanProperty;
@@ -21,6 +23,7 @@ import com.fasterxml.jackson.databind.deser.impl.NullsConstantProvider;
 import com.fasterxml.jackson.databind.deser.impl.NullsFailProvider;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
+import com.fasterxml.jackson.databind.type.LogicalType;
 import com.fasterxml.jackson.databind.util.AccessPattern;
 import com.fasterxml.jackson.databind.util.ClassUtil;
 import com.fasterxml.jackson.databind.util.Converter;
@@ -239,6 +242,51 @@ public abstract class StdDeserializer<T>
             }
         }
         return (T) ctxt.handleUnexpectedToken(getValueType(ctxt), p);
+    }
+
+    /**
+     * Helper method to call in case deserializer does not support native automatic
+     * use of incoming String values, but there may be standard coercions to consider.
+     *
+     * @since 2.12
+     */
+    @SuppressWarnings("unchecked")
+    protected T _deserializeFromString(JsonParser p, DeserializationContext ctxt)
+            throws IOException
+    {
+        final ValueInstantiator inst = getValueInstantiator();
+        final String value = p.getValueAsString();
+
+        if ((inst != null) && inst.canCreateFromString()) {
+            return (T) inst.createFromString(ctxt, value);
+        }
+
+        if (value.length() == 0) {
+            if (ctxt.isEnabled(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)) {
+                return null;
+            }
+        }
+        
+        /* 28-Sep-2011, tatu: Ok this is not clean at all; but since there are legacy
+         *   systems that expect conversions in some cases, let's just add a minimal
+         *   patch (note: same could conceivably be used for numbers too).
+         */
+        if ((inst != null) && inst.canCreateFromBoolean()) {
+            // 29-May-2020, tatu: With 2.12 can and should use CoercionConfig so:
+            if (ctxt.findCoercionAction(LogicalType.Boolean, Boolean.class,
+                    CoercionInputShape.String) == CoercionAction.TryConvert) {
+                String str = value.trim();
+                if ("true".equals(str)) {
+                    return (T) inst.createFromBoolean(ctxt, true);
+                }
+                if ("false".equals(str)) {
+                    return (T) inst.createFromBoolean(ctxt, false);
+                }
+            }
+        }
+        return (T) ctxt.handleMissingInstantiator(handledType(), inst, ctxt.getParser(),
+                "no String-argument constructor/factory method to deserialize from String value ('%s')",
+                value);
     }
 
     /**

commit aa5bdaded9de146ba1e4d12df2ad7853949a50e4

Fix unwrapped string value deserialization regression
This fixes a regresion introduced by
fb72f93f4f4503caf22b693c8df1441c70341662 which caused unexpected
interactions between ACCEPT_SINGLE_VALUE_AS_ARRAY and handling
for ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, causing unwrapped string
values to fail deserialization.

src/main/java/com/fasterxml/jackson/databind/deser/std/CollectionDeserializer.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.core.*;
 
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JacksonStdImpl;
+import com.fasterxml.jackson.databind.cfg.CoercionAction;
+import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
 import com.fasterxml.jackson.databind.deser.*;
 import com.fasterxml.jackson.databind.deser.impl.ReadableObjectId.Referring;
 import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
@@ -246,7 +248,23 @@ _containerType,
         // there is also possibility of "auto-wrapping" of single-element arrays.
         // Hence we only accept empty String here.
         if (p.hasToken(JsonToken.VALUE_STRING)) {
-            return _deserializeFromString(p, ctxt);
+            // 05-Nov-2020, ckozak: As per [jackson-databind#2922] string values may be handled
+            // using handleNonArray, however empty strings may result in a null or empty collection
+            // depending on configuration.
+            final CoercionAction act = ctxt.findCoercionAction(logicalType(), handledType(),
+                    CoercionInputShape.EmptyString);
+            // 05-Nov-2020, ckozak: Unclear if TryConvert should return the default
+            // conversion (null) or fall through to handleNonArray.
+            if (act != null
+                    // handleNonArray may successfully deserialize the result (if
+                    // ACCEPT_SINGLE_VALUE_AS_ARRAY is enabled, for example) otherwise it
+                    // is capable of failing just as well as _deserializeFromEmptyString.
+                    && act != CoercionAction.Fail
+                    // getValueAsString call is ordered last to avoid unnecessarily building a string value.
+                    && p.getValueAsString().isEmpty()) {
+                return (Collection<Object>) _deserializeFromEmptyString(
+                        p, ctxt, act, handledType(), "empty String (\"\")");
+            }
         }
         return handleNonArray(p, ctxt, createDefaultInstance(ctxt));
     }

src/test/java/com/fasterxml/jackson/databind/convert/CoerceContainersTest.java
@@ -161,7 +161,7 @@ public class CoerceContainersTest extends BaseMapTest
             fail("Should not pass");
         } catch (Exception e) {
             verifyException(e, "Cannot deserialize value of type");
-            verifyException(e, "from empty String");
+            verifyException(e, "from empty String", "from String value (token `JsonToken.VALUE_STRING`)");
         }
     }
 
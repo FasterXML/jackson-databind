e0ee058cef28f7c2a147d14bb5ae17c702e98200
Carter Kozak <ckozak@ckozak.net>
2020-11-12 17:17:54 -0500

    Revert #2805 which unnecessarily wraps json processing exceptions
    While exceptions reading the input into tokens are impossible,
    it's very possible that the requested type does not match the
    provided JsonNode, in which case a JsonProcessingException subtype
    is thrown.

--- src/main/java/com/fasterxml/jackson/databind/ObjectMapper.java
+++ src/main/java/com/fasterxml/jackson/databind/ObjectMapper.java
@@ -3227,7 +3227,7 @@ public class ObjectMapper
     @Override
     public <T> T treeToValue(TreeNode n, Class<T> valueType)
         throws IllegalArgumentException,
-            JsonProcessingException // is NOT actually thrown but retained as per #2878
+            JsonProcessingException
     {
         if (n == null) {
             return null;
@@ -3255,6 +3255,8 @@ public class ObjectMapper
                 }
             }
             return readValue(treeAsTokens(n), valueType);
+        } catch (JsonProcessingException e) {
+            throw e;
         } catch (IOException e) { // should not occur, no real i/o...
             throw new IllegalArgumentException(e.getMessage(), e);
         }

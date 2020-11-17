e9099d2002c3be4b04e7110fa58d4a6f1a338794
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-11-16 21:19:53 -0800

    Test reorg

--- src/test/java/com/fasterxml/jackson/databind/jsontype/jdk/TestScalars.java
+++ src/test/java/com/fasterxml/jackson/databind/jsontype/jdk/TestScalars.java
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.databind.jsontype;
+package com.fasterxml.jackson.databind.jsontype.jdk;
 
 import java.io.Serializable;
 import java.util.ArrayList;
--- src/test/java/com/fasterxml/jackson/databind/jsontype/jdk/TestTypedArraySerialization.java
+++ src/test/java/com/fasterxml/jackson/databind/jsontype/jdk/TestTypedArraySerialization.java
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.databind.jsontype;
+package com.fasterxml.jackson.databind.jsontype.jdk;
 
 import java.util.*;
 

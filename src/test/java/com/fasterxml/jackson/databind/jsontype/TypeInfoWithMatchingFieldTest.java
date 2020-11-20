FasterXML/jackson-databind

commit cdd743049dbb61ae8c8e27c268c247b0c61d8c0b

Last cleanup wrt #2909

src/main/java/com/fasterxml/jackson/databind/ser/BasicSerializerFactory.java
@@ -237,7 +237,8 @@ public abstract class BasicSerializerFactory
                                 ClassUtil.checkAndFixAccess(am.getMember(),
                                         config.isEnabled(MapperFeature.OVERRIDE_PUBLIC_ACCESS_MODIFIERS));
                             }
-                            ser = new JsonValueSerializer(am, delegate);
+                            // null -> no TypeSerializer for key-serializer use case
+                            ser = new JsonValueSerializer(am, null, delegate);
                         } else {
                             ser = StdKeySerializers.getFallbackKeySerializer(config, keyType.getRawClass());
                         }
@@ -400,8 +401,16 @@ public abstract class BasicSerializerFactory
                 ClassUtil.checkAndFixAccess(valueAccessor.getMember(),
                         prov.isEnabled(MapperFeature.OVERRIDE_PUBLIC_ACCESS_MODIFIERS));
             }
-            JsonSerializer<Object> ser = findSerializerFromAnnotation(prov, valueAccessor);
-            return new JsonValueSerializer(valueAccessor, ser);
+            final JavaType valueType = valueAccessor.getType();
+            JsonSerializer<Object> valueSerializer = findSerializerFromAnnotation(prov, valueAccessor);
+            if (valueSerializer == null) {
+                valueSerializer = valueType.getValueHandler();
+            }
+            TypeSerializer typeSerializer = valueType.getTypeHandler();
+            if (typeSerializer == null) {
+                typeSerializer = createTypeSerializer(prov.getConfig(), valueType);
+            }
+            return new JsonValueSerializer(valueAccessor, typeSerializer, valueSerializer);
         }
         // No well-known annotations...
         return null;

src/main/java/com/fasterxml/jackson/databind/ser/std/JsonValueSerializer.java
@@ -48,6 +48,11 @@ public class JsonValueSerializer
      */
     protected final AnnotatedMember _accessor;
 
+    /**
+     * @since 2.12
+     */
+    protected final TypeSerializer _valueTypeSerializer;
+
     protected final JsonSerializer<Object> _valueSerializer;
 
     protected final BeanProperty _property;
@@ -86,29 +91,40 @@ public class JsonValueSerializer
      *    occurs if and only if the "value method" was annotated with
      *    {@link com.fasterxml.jackson.databind.annotation.JsonSerialize#using}), otherwise
      *    null
-     *    
-     * @since 2.8 Earlier method took "raw" Method, but that does not work with access
-     *    to information we need
+     *
+     * @since 2.12 added {@link TypeSerializer} since 2.11
      */
     @SuppressWarnings("unchecked")
-    public JsonValueSerializer(AnnotatedMember accessor, JsonSerializer<?> ser)
+    public JsonValueSerializer(AnnotatedMember accessor,
+            TypeSerializer vts, JsonSerializer<?> ser)
     {
         super(accessor.getType());
         _accessor = accessor;
         _valueType = accessor.getType();
+        _valueTypeSerializer = vts;
         _valueSerializer = (JsonSerializer<Object>) ser;
         _property = null;
         _forceTypeInformation = true; // gets reconsidered when we are contextualized
         _dynamicSerializers = PropertySerializerMap.emptyForProperties();
     }
 
+    /**
+     * @deprecated Since 2.12
+     */
+    @Deprecated
+    public JsonValueSerializer(AnnotatedMember accessor, JsonSerializer<?> ser) {
+        this(accessor, null, ser);
+    }
+
+    // @since 2.12
     @SuppressWarnings("unchecked")
     public JsonValueSerializer(JsonValueSerializer src, BeanProperty property,
-            JsonSerializer<?> ser, boolean forceTypeInfo)
+            TypeSerializer vts, JsonSerializer<?> ser, boolean forceTypeInfo)
     {
         super(_notNullClass(src.handledType()));
         _accessor = src._accessor;
         _valueType = src._valueType;
+        _valueTypeSerializer = vts;
         _valueSerializer = (JsonSerializer<Object>) ser;
         _property = property;
         _forceTypeInformation = forceTypeInfo;
@@ -120,14 +136,15 @@ public class JsonValueSerializer
         return (cls == null) ? Object.class : (Class<Object>) cls;
     }
 
-    public JsonValueSerializer withResolved(BeanProperty property,
-            JsonSerializer<?> ser, boolean forceTypeInfo)
+    protected JsonValueSerializer withResolved(BeanProperty property,
+            TypeSerializer vts, JsonSerializer<?> ser, boolean forceTypeInfo)
     {
-        if (_property == property && _valueSerializer == ser
-                && forceTypeInfo == _forceTypeInformation) {
+        if ((_property == property)
+                && (_valueTypeSerializer == vts) && (_valueSerializer == ser)
+                && (forceTypeInfo == _forceTypeInformation)) {
             return this;
         }
-        return new JsonValueSerializer(this, property, ser, forceTypeInfo);
+        return new JsonValueSerializer(this, property, vts, ser, forceTypeInfo);
     }
 
     /*
@@ -147,7 +164,7 @@ public class JsonValueSerializer
         JsonSerializer<Object> ser = _valueSerializer;
         if (ser == null) {
             try {
-                ser = _findDynamicSerializer(ctxt, referenced);
+                ser = _findDynamicSerializer(ctxt, referenced.getClass());
             } catch (JsonMappingException e) {
                 throw new RuntimeJsonMappingException(e);
             }
@@ -170,6 +187,10 @@ public class JsonValueSerializer
             BeanProperty property)
         throws JsonMappingException
     {
+        TypeSerializer typeSer = _valueTypeSerializer;
+        if (typeSer != null) {
+            typeSer = typeSer.forProperty(property);
+        }
         JsonSerializer<?> ser = _valueSerializer;
         if (ser == null) {
             // Can only assign serializer statically if the declared type is final:
@@ -188,16 +209,16 @@ public class JsonValueSerializer
                  *   using standard serializer
                  */
                 boolean forceTypeInformation = isNaturalTypeWithStdHandling(_valueType.getRawClass(), ser);
-                return withResolved(property, ser, forceTypeInformation);
+                return withResolved(property, typeSer, ser, forceTypeInformation);
             }
             // [databind#2822]: better hold on to "property", regardless
             if (property != _property) {
-                return withResolved(property, ser, _forceTypeInformation);
+                return withResolved(property, typeSer, ser, _forceTypeInformation);
             }
         } else {
             // 05-Sep-2013, tatu: I _think_ this can be considered a primary property...
             ser = ctxt.handlePrimaryContextualization(ser, property);
-            return withResolved(property, ser, _forceTypeInformation);
+            return withResolved(property, typeSer, ser, _forceTypeInformation);
         }
         return this;
     }
@@ -221,13 +242,17 @@ public class JsonValueSerializer
 
         if (value == null) {
             ctxt.defaultSerializeNull(gen);
-            return;
+        } else {
+            JsonSerializer<Object> ser = _valueSerializer;
+            if (ser == null) {
+                ser = _findDynamicSerializer(ctxt, value.getClass());
+            }
+            if (_valueTypeSerializer != null) {
+                ser.serializeWithType(value, gen, ctxt, _valueTypeSerializer);
+            } else {
+                ser.serialize(value, gen, ctxt);
+            }
         }
-        JsonSerializer<Object> ser = _valueSerializer;
-        if (ser == null) {
-            ser = _findDynamicSerializer(ctxt, value);
-        }
-        ser.serialize(value, gen, ctxt);
     }
 
     @Override
@@ -250,7 +275,7 @@ public class JsonValueSerializer
         }
         JsonSerializer<Object> ser = _valueSerializer;
         if (ser == null) { // no serializer yet? Need to fetch
-            ser = _findDynamicSerializer(ctxt, value);
+            ser = _findDynamicSerializer(ctxt, value.getClass());
         } else {
             // 09-Dec-2010, tatu: To work around natural type's refusal to add type info, we do
             //    this (note: type is for the wrapper type, not enclosed value!)
@@ -379,15 +404,26 @@ public class JsonValueSerializer
 
     // @since 2.12
     protected JsonSerializer<Object> _findDynamicSerializer(SerializerProvider ctxt,
-            Object value) throws JsonMappingException
+            Class<?> valueClass) throws JsonMappingException
     {
-        Class<?> cc = value.getClass();
-        JsonSerializer<Object> serializer = _dynamicSerializers.serializerFor(cc);
-        if (serializer != null) {
-            return serializer;
+        JsonSerializer<Object> serializer = _dynamicSerializers.serializerFor(valueClass);
+        if (serializer == null) {
+            if (_valueType.hasGenericTypes()) {
+                final JavaType fullType = ctxt.constructSpecializedType(_valueType, valueClass);
+                serializer = ctxt.findPrimaryPropertySerializer(fullType, _property);
+                PropertySerializerMap.SerializerAndMapResult result = _dynamicSerializers.addSerializer(fullType, serializer);
+                _dynamicSerializers = result.map;
+            } else {
+                serializer = ctxt.findPrimaryPropertySerializer(valueClass, _property);
+                PropertySerializerMap.SerializerAndMapResult result = _dynamicSerializers.addSerializer(valueClass, serializer);
+                _dynamicSerializers = result.map;
+            }
         }
+        return serializer;
+
+        /*
         if (_valueType.hasGenericTypes()) {
-            JavaType fullType = ctxt.constructSpecializedType(_valueType, cc);
+            JavaType fullType = ctxt.constructSpecializedType(_valueType, valueClass);
             // 31-Oct-2020, tatu: Should not get typed/root serializer, but for now has to do:
             serializer = ctxt.findTypedValueSerializer(fullType, false, _property);
             PropertySerializerMap.SerializerAndMapResult result = _dynamicSerializers.addSerializer(fullType, serializer);
@@ -396,12 +432,13 @@ public class JsonValueSerializer
             return serializer;
         } else {
             // 31-Oct-2020, tatu: Should not get typed/root serializer, but for now has to do:
-            serializer = ctxt.findTypedValueSerializer(cc, false, _property);
-            PropertySerializerMap.SerializerAndMapResult result = _dynamicSerializers.addSerializer(cc, serializer);
+            serializer = ctxt.findTypedValueSerializer(valueClass, false, _property);
+            PropertySerializerMap.SerializerAndMapResult result = _dynamicSerializers.addSerializer(valueClass, serializer);
             // did we get a new map of serializers? If so, start using it
             _dynamicSerializers = result.map;
             return serializer;
         }
+        */
     }
 
     /*

commit af62639cf9757c8f147fe4b204a6c718dd070a6c

Test coverage for TypeInfo regression
This appears to be an problem related to some combination of:
* JsonTypeInfo.As.EXISTING_PROPERTY on the default subtype
* @JsonAnyGetter providing the existing property on the default subtype

Regression was introduced in cdd743049dbb61ae8c8e27c268c247b0c61d8c0b

src/test/java/com/fasterxml/jackson/databind/jsontype/TypeInfoWithMatchingFieldTest.java
@@ -1,0 +1,214 @@
+package com.fasterxml.jackson.databind.jsontype;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class TypeInfoWithMatchingFieldTest extends BaseMapTest
+{
+
+    public static final class SingleUnion {
+        private final Base value;
+
+        @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+        private SingleUnion(Base value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        private Base getValue() {
+            return value;
+        }
+
+        public static SingleUnion foo(String value) {
+            return new SingleUnion(new FooWrapper(value));
+        }
+
+        public <T> T accept(Visitor<T> visitor) {
+            return value.accept(visitor);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other || (other instanceof SingleUnion && equalTo((SingleUnion) other));
+        }
+
+        private boolean equalTo(SingleUnion other) {
+            return this.value.equals(other.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(this.value);
+        }
+
+        @Override
+        public String toString() {
+            return "SingleUnion{value: " + value + '}';
+        }
+
+        public interface Visitor<T> {
+            T visitFoo(String value);
+
+            T visitUnknown(String unknownType);
+        }
+
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true, defaultImpl = UnknownWrapper.class)
+        @JsonSubTypes(@JsonSubTypes.Type(FooWrapper.class))
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        private interface Base {
+            <T> T accept(Visitor<T> visitor);
+        }
+
+        @JsonTypeName("foo")
+        private static final class FooWrapper implements Base {
+            private final String value;
+
+            @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+            private FooWrapper(@JsonSetter("foo") String value) {
+                Objects.requireNonNull(value, "foo cannot be null");
+                this.value = value;
+            }
+
+            @JsonProperty("foo")
+            private String getValue() {
+                return value;
+            }
+
+            @Override
+            public <T> T accept(Visitor<T> visitor) {
+                return visitor.visitFoo(value);
+            }
+
+            @Override
+            public boolean equals(Object other) {
+                return this == other || (other instanceof FooWrapper && equalTo((FooWrapper) other));
+            }
+
+            private boolean equalTo(FooWrapper other) {
+                return this.value.equals(other.value);
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hashCode(this.value);
+            }
+
+            @Override
+            public String toString() {
+                return "FooWrapper{value: " + value + '}';
+            }
+        }
+
+        @JsonTypeInfo(
+                use = JsonTypeInfo.Id.NAME,
+                include = JsonTypeInfo.As.EXISTING_PROPERTY,
+                property = "type",
+                visible = true)
+        private static final class UnknownWrapper implements Base {
+            private final String type;
+
+            private final Map<String, Object> value;
+
+            @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+            private UnknownWrapper(@JsonProperty("type") String type) {
+                this(type, new HashMap<String, Object>());
+            }
+
+            private UnknownWrapper(String type, Map<String, Object> value) {
+                this.type = Objects.requireNonNull(type, "type cannot be null");
+                this.value = Objects.requireNonNull(value, "value cannot be null");
+            }
+
+            @JsonProperty
+            private String getType() {
+                return type;
+            }
+
+            @JsonAnyGetter
+            private Map<String, Object> getValue() {
+                return value;
+            }
+
+            @JsonAnySetter
+            private void put(String key, Object val) {
+                value.put(key, val);
+            }
+
+            @Override
+            public <T> T accept(Visitor<T> visitor) {
+                return visitor.visitUnknown(type);
+            }
+
+            @Override
+            public boolean equals(Object other) {
+                return this == other || (other instanceof UnknownWrapper && equalTo((UnknownWrapper) other));
+            }
+
+            private boolean equalTo(UnknownWrapper other) {
+                return this.type.equals(other.type) && this.value.equals(other.value);
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(this.type, this.value);
+            }
+
+            @Override
+            public String toString() {
+                return "UnknownWrapper{type: " + type + ", value: " + value + '}';
+            }
+        }
+    }
+
+    public void testUnionSerialization_string() throws IOException {
+        ObjectMapper mapper = objectMapper();
+        assertEquals(
+                aposToQuotes("{'type':'foo','foo':'string value'}"),
+                mapper.writeValueAsString(SingleUnion.foo("string value")));
+    }
+
+    public void testUnionDeserialization_string() throws IOException {
+        ObjectMapper mapper = objectMapper();
+        assertEquals(
+                SingleUnion.foo("string value"),
+                mapper.readValue(aposToQuotes("{'type':'foo','foo':'string value'}"), SingleUnion.class));
+    }
+
+    public void testDefaultUnknownVariant() throws IOException {
+        ObjectMapper mapper = objectMapper();
+        String originalJson = aposToQuotes("{'type':'notknown','notknown':'ignored value'}");
+        SingleUnion deserialized = mapper.readValue(
+                originalJson,
+                SingleUnion.class);
+        String type = deserialized.accept(new SingleUnion.Visitor<String>() {
+
+            @Override
+            public String visitFoo(String value) {
+                throw new AssertionError("Unexpected foo");
+            }
+
+            @Override
+            public String visitUnknown(String unknownType) {
+                return unknownType;
+            }
+        });
+        assertEquals("notknown", type);
+        String serialized = mapper.writeValueAsString(deserialized);
+        assertEquals(originalJson, serialized);
+    }
+}
8140c9ec63971cdfef6a65b20e69645217699540
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-03-01 14:44:42 -0800

    Minor cleanup, javadoc update

--- src/main/java/com/fasterxml/jackson/databind/ObjectMapper.java
+++ src/main/java/com/fasterxml/jackson/databind/ObjectMapper.java
@@ -3281,26 +3281,29 @@ public class ObjectMapper
     }
 
     /**
-     * Reverse of {@link #treeToValue}; given a value (usually bean), will
-     * construct equivalent JSON Tree representation. Functionally similar
-     * to serializing value into JSON and parsing JSON as tree, but
-     * more efficient.
+     * Method that is reverse of {@link #treeToValue}: it
+     * will convert given Java value (usually bean) into its
+     * equivalent Tree mode {@link JsonNode} representation.
+     * Functionally similar to serializing value into token stream and parsing that
+     * stream back as tree model node,
+     * but more efficient as {@link TokenBuffer} is used to contain the intermediate
+     * representation instead of fully serialized contents.
      *<p>
      * NOTE: while results are usually identical to that of serialization followed
      * by deserialization, this is not always the case. In some cases serialization
      * into intermediate representation will retain encapsulation of things like
      * raw value ({@link com.fasterxml.jackson.databind.util.RawValue}) or basic
      * node identity ({@link JsonNode}). If so, result is a valid tree, but values
-     * are not re-constructed through actual JSON representation. So if transformation
-     * requires actual materialization of JSON (or other data format that this mapper
-     * produces), it will be necessary to do actual serialization.
+     * are not re-constructed through actual format representation. So if transformation
+     * requires actual materialization of encoded content,
+     * it will be necessary to do actual serialization.
      * 
      * @param <T> Actual node type; usually either basic {@link JsonNode} or
      *  {@link com.fasterxml.jackson.databind.node.ObjectNode}
-     * @param fromValue Bean value to convert
+     * @param fromValue Java value to convert
      *
-     * @return (non-null) Root node of the resulting JSON tree: in case of {@code null} value,
-     *    node for which {@link JsonNode#isNull()} returns {@code true}.
+     * @return (non-null) Root node of the resulting content tree: in case of
+     *   {@code null} value node for which {@link JsonNode#isNull()} returns {@code true}.
      */
     @SuppressWarnings({ "unchecked", "resource" })
     public <T extends JsonNode> T valueToTree(Object fromValue)
@@ -3314,16 +3317,14 @@ public class ObjectMapper
         if (isEnabled(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS)) {
             buf = buf.forceUseOfBigDecimal(true);
         }
-        JsonNode result;
         try {
             writeValue(buf, fromValue);
-            JsonParser p = buf.asParser();
-            result = readTree(p);
-            p.close();
+            try (JsonParser p = buf.asParser()) {
+                return readTree(p);
+            }
         } catch (IOException e) { // should not occur, no real i/o...
             throw new IllegalArgumentException(e.getMessage(), e);
         }
-        return (T) result;
     }
 
     /*

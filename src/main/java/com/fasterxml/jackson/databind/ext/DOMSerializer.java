1c4f8108736fe327e45794d95de7cd4a1bbad64e
Morten Andersen-Gott <morten.andersengott@gmail.com>
2021-06-10 19:11:14 +0200

    Use XML Transformer for xml serialization to fix missing default namespace when serializing xml

--- src/main/java/com/fasterxml/jackson/databind/ext/DOMSerializer.java
+++ src/main/java/com/fasterxml/jackson/databind/ext/DOMSerializer.java
@@ -1,43 +1,53 @@
 package com.fasterxml.jackson.databind.ext;
 
-import java.io.IOException;
-
-import org.w3c.dom.Node;
-import  org.w3c.dom.bootstrap.DOMImplementationRegistry;
-import  org.w3c.dom.ls.DOMImplementationLS;
-import  org.w3c.dom.ls.LSSerializer;
-
-import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWrapper;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.w3c.dom.Node;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringWriter;
 @SuppressWarnings("serial")
 public class DOMSerializer extends StdSerializer<Node>
 {
-    protected final DOMImplementationLS _domImpl;
+
+    private final TransformerFactory transformerFactory;
 
     public DOMSerializer() {
         super(Node.class);
-        DOMImplementationRegistry registry;
         try {
-            registry = DOMImplementationRegistry.newInstance();
+            transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
         } catch (Exception e) {
-            throw new IllegalStateException("Could not instantiate DOMImplementationRegistry: "+e.getMessage(), e);
+            throw new IllegalStateException("Could not instantiate TransformerFactory: "+e.getMessage(), e);
         }
-        _domImpl = (DOMImplementationLS)registry.getDOMImplementation("LS");
     }
     
     @Override
     public void serialize(Node value, JsonGenerator jgen, SerializerProvider provider)
         throws IOException
     {
-        if (_domImpl == null) throw new IllegalStateException("Could not find DOM LS");    	
-        LSSerializer writer = _domImpl.createLSSerializer();
-        jgen.writeString(writer.writeToString(value));
+        try {
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "no");
+            StreamResult result = new StreamResult(new StringWriter());
+            DOMSource source = new DOMSource(value);
+            transformer.transform(source, result);
+            jgen.writeString(result.getWriter().toString());
+        } catch (TransformerConfigurationException e) {
+            throw new IllegalStateException("Could not create XML Transformer: "+e.getMessage(), e);
+        } catch (TransformerException e) {
+            provider.reportMappingProblem(e,"XML Transformation failed: %s", e.getMessage());
+        }
     }
 
 	@Override

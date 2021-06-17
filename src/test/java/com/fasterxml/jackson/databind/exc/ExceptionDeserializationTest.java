9b5771585c290df60f2ec58a07cfbf74a6e5bee9
Klaas Dellschaft <klaas.dellschaft@dbschenker.com>
2021-06-17 10:26:36 +0200

    Test coverage: Add a dedicated test covering the serialization and deserialization of Throwable.getCause()

--- src/test/java/com/fasterxml/jackson/databind/exc/ExceptionDeserializationTest.java
+++ src/test/java/com/fasterxml/jackson/databind/exc/ExceptionDeserializationTest.java
@@ -20,7 +20,7 @@ public class ExceptionDeserializationTest
 
         protected String myMessage;
         protected HashMap<String,Object> stuff = new HashMap<String, Object>();
-        
+
         @JsonCreator
         MyException(@JsonProperty("message") String msg, @JsonProperty("value") int v)
         {
@@ -30,7 +30,7 @@ public class ExceptionDeserializationTest
         }
 
         public int getValue() { return value; }
-        
+
         public String getFoo() { return "bar"; }
 
         @JsonAnySetter public void setter(String key, Object value)
@@ -52,7 +52,7 @@ public class ExceptionDeserializationTest
      */
 
     private final ObjectMapper MAPPER = new ObjectMapper();
-    
+
     public void testIOException() throws IOException
     {
         IOException ioe = new IOException("TEST");
@@ -96,9 +96,10 @@ public class ExceptionDeserializationTest
         Exception exc = MAPPER.readValue("{\"suppressed\":[]}", IOException.class);
         assertNotNull(exc);
     }
-    
+
     // [databind#381]
-    public void testSingleValueArrayDeserialization() throws Exception {
+    public void testSingleValueArrayDeserialization() throws Exception
+    {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.enable(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS);
         final IOException exp;
@@ -108,13 +109,31 @@ public class ExceptionDeserializationTest
             exp = internal;
         }
         final String value = "[" + mapper.writeValueAsString(exp) + "]";
-        
+
         final IOException cloned = mapper.readValue(value, IOException.class);
-        assertEquals(exp.getMessage(), cloned.getMessage());    
-        
-        assertEquals(exp.getStackTrace().length, cloned.getStackTrace().length);
-        for (int i = 0; i < exp.getStackTrace().length; i ++) {
-            _assertEquality(i, exp.getStackTrace()[i], cloned.getStackTrace()[i]);
+        assertEquals(exp.getMessage(), cloned.getMessage());
+
+        _assertEquality(exp.getStackTrace(), cloned.getStackTrace());
+    }
+
+    public void testExceptionCauseDeserialization() throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+
+        final IOException exp = new IOException("the outer exception", new Throwable("the cause"));
+
+        final String value = mapper.writeValueAsString(exp);
+        final IOException act = mapper.readValue(value, IOException.class);
+
+        assertNotNull(act.getCause());
+        assertEquals(exp.getCause().getMessage(), act.getCause().getMessage());
+        _assertEquality(exp.getCause().getStackTrace(), act.getCause().getStackTrace());
+    }
+
+    private void _assertEquality(StackTraceElement[] exp, StackTraceElement[] act) {
+        assertEquals(exp.length, act.length);
+        for (int i = 0; i < exp.length; i++) {
+            _assertEquality(i, exp[i], act[i]);
         }
     }
 
@@ -145,7 +164,7 @@ public class ExceptionDeserializationTest
     public void testSingleValueArrayDeserializationException() throws Exception {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS);
-        
+
         final IOException exp;
         try {
             throw new IOException("testing");
@@ -153,7 +172,7 @@ public class ExceptionDeserializationTest
             exp = internal;
         }
         final String value = "[" + mapper.writeValueAsString(exp) + "]";
-        
+
         try {
             mapper.readValue(value, IOException.class);
             fail("Exception not thrown when attempting to deserialize an IOException wrapped in a single value array with UNWRAP_SINGLE_VALUE_ARRAYS disabled");

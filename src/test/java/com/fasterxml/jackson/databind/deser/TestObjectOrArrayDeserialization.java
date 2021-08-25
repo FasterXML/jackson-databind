9eb87aa2bd45bb3c402390a198998d2ed2f30419
quintin <quintin@orderlyhealth.com>
2021-08-24 22:46:17 -0400

    Added Tests for coercion of Blank string cases
    Added Tests for coercion of Blank string cases

--- src/test/java/com/fasterxml/jackson/databind/deser/TestObjectOrArrayDeserialization.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestObjectOrArrayDeserialization.java
@@ -3,10 +3,8 @@ package com.fasterxml.jackson.databind.deser;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.BaseMapTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.junit.Assert;
 
 public class TestObjectOrArrayDeserialization extends BaseMapTest
@@ -33,14 +31,40 @@ public class TestObjectOrArrayDeserialization extends BaseMapTest
     }
 
 
-    public void testCoercionOfBlankStrings() {
-        String s =  " ";
+    public void testCoercionOfBlankString() {
+        assertCoercion(" ");//, "0", "0 ", " 0 ", "", "    ", "\\n"});
+    }
 
+    public void testCoercionOfBlankStringWith0() {
+        assertCoercion("0");
+    }
+
+    public void testCoercionOfBlankStringWith0AndSpace() {
+        assertCoercion( "0 ");
+    }
+
+    public void testCoercion0withSpaces() {
+        assertCoercion(" 0 ");
+    }
+
+    public void testCoercionOfEmptyString() {
+        assertCoercion("");
+    }
+
+    public void testCoercionOfLongerBlankString() {
+        assertCoercion("    ");
+    }
+
+    public void testCoercionOfEndlineCharacter() {
+        assertCoercion("\\n");
+    }
+
+    private void assertCoercion (String s){
         final String json = "{ \"J\" : \"" + s + "\" }";
         try {
             final Jack jack = new ObjectMapper().readValue(json, Jack.class);
             System.out.println("j=" + jack.getJ());
-            Assert.assertEquals(0, jack.getJ());
+            Assert.assertEquals("Coercion failed for " + s, 0, jack.getJ());
         } catch (Throwable t) {
             t.printStackTrace();
             fail();

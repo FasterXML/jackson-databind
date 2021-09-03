810128e87c7ec49048b196cdec5253fa531c26fe
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-09-02 19:11:14 -0700

    Add override for `AtomicReference`s handling of "absent" creator properties (to become `null`s)

--- src/main/java/com/fasterxml/jackson/databind/deser/std/AtomicReferenceDeserializer.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/AtomicReferenceDeserializer.java
@@ -52,6 +52,14 @@ public class AtomicReferenceDeserializer
         return getNullValue(ctxt);
     }
 
+    /**
+     * Let's actually NOT coerce missing Creator parameters into empty value.
+     */
+    @Override // @since 2.13
+    public Object getAbsentValue(DeserializationContext ctxt) throws JsonMappingException {
+        return null;
+    }
+
     @Override
     public AtomicReference<Object> referenceValue(Object contents) {
         return new AtomicReference<Object>(contents);

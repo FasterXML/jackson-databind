d99dc22a8f78cf74e4f66e6b4d8fc9b132eb3a4f
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-02 19:14:21 +0200

    use BigDecimalParser (#3438)

--- src/main/java/com/fasterxml/jackson/databind/node/BigIntegerNode.java
+++ src/main/java/com/fasterxml/jackson/databind/node/BigIntegerNode.java
@@ -33,7 +33,7 @@ public class BigIntegerNode
 
     /* 
     /**********************************************************
-    /* Overrridden JsonNode methods
+    /* Overridden JsonNode methods
     /**********************************************************
      */
 

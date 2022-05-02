866d6dacf38a032df0f791d379f66927fabd457f
PJ Fanning <pjfanning@users.noreply.github.com>
2022-05-02 23:15:46 +0200

    add broken test that fails because floats are first parsed as doubles (#3470)

--- src/test/java/com/fasterxml/jackson/databind/deser/TestFloats.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/TestFloats.java
@@ -3,6 +3,8 @@ package com.fasterxml.jackson.databind.deser;
 import com.fasterxml.jackson.databind.BaseMapTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.InputStream;
+
 public class TestFloats extends BaseMapTest
 {
 
@@ -49,6 +51,19 @@ public class TestFloats extends BaseMapTest
         assertEquals(3.4028235e38f, floats[2]);
         assertEquals("1.4E-45", Float.toString(floats[3])); //this assertion fails unless toString is used
     }
+
+    public void testBigArrayOfFloatPrimitives() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        try (InputStream stream = TestFloats.class.getResourceAsStream("/data/float-array.txt")) {
+            float[] floats = mapper.readValue(stream, float[].class);
+            assertEquals(1004, floats.length);
+            assertEquals(7.038531e-26f, floats[0]);
+            assertEquals(1.1999999f, floats[1]);
+            assertEquals(3.4028235e38f, floats[2]);
+            assertEquals("1.4E-45", Float.toString(floats[3])); //this assertion fails unless toString is used
+        }
+    }
+
 
     public void testArrayOfFloats() throws Exception
     {

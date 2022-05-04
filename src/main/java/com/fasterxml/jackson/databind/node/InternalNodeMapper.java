50f3789e4db9cb6d1f23be716e0c1a21e7e8921b
Tatu Saloranta <tatu.saloranta@iki.fi>
2022-05-04 06:59:26 -0700

    ...

--- src/main/java/com/fasterxml/jackson/databind/node/InternalNodeMapper.java
+++ src/main/java/com/fasterxml/jackson/databind/node/InternalNodeMapper.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 

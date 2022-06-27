bfc530240dc1184deae7a3db32ff90889af5f317
PJ Fanning <pjfanning@users.noreply.github.com>
2022-06-27 20:05:39 +0100

    extra tests - some fail and will be investigated in coming days

---
+++ src/test/java/com/fasterxml/jackson/databind/util/concurrentlinkedhashmap/CLHMTestlibTests.java
@@ -0,0 +1,21 @@
+package com.fasterxml.jackson.databind.util.concurrentlinkedhashmap;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public final class CLHMTestlibTests extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        addCLHMViewTests(suite);
+        return suite;
+    }
+
+    private static void addCLHMViewTests(TestSuite suite) {
+        suite.addTest(MapTestFactory.suite("CLHMView", MapTestFactory.synchronousGenerator(
+                () -> new ConcurrentLinkedHashMap.Builder<String, String>()
+                    .maximumWeightedCapacity(10).build()))
+        );
+    }
+}

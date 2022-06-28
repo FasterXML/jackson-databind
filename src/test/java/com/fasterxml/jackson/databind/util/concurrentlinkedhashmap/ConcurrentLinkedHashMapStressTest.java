b2c9b081c929e8961b11597579717c2d17ccb41c
PJ Fanning <pjfanning@users.noreply.github.com>
2022-06-28 15:29:39 +0100

    Create ConcurrentLinkedHashMapStressTest.java

---
+++ src/test/java/com/fasterxml/jackson/databind/util/concurrentlinkedhashmap/ConcurrentLinkedHashMapStressTest.java
@@ -0,0 +1,63 @@
+package com.fasterxml.jackson.databind.util.concurrentlinkedhashmap;
+
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConcurrentLinkedHashMapStressTest {
+
+    //increase these to increase the stress
+    private static int iterations = 100000;
+    private static int threads = 20;
+    private static int waitSeconds = 60;
+
+    @Test
+    public void testManyEntries() throws Exception {
+        final int maxEntries = 30;
+        final int maxKey = 100;
+        final Random rnd = new Random();
+        final ConcurrentLinkedHashMap<Integer, UUID> clhm =
+                new ConcurrentLinkedHashMap.Builder<Integer, UUID>().maximumWeightedCapacity(maxEntries).build();
+        final Map<Integer, UUID> map = new ConcurrentHashMap<>();
+        final ExecutorService executor = Executors.newFixedThreadPool(threads);
+        try {
+            for (int i = 0; i < maxKey; i++) {
+                final int key = i;
+                executor.submit(() -> {
+                    UUID uuid = UUID.randomUUID();
+                    clhm.put(key, uuid);
+                    map.put(key, uuid);
+                });
+            }
+            for (int i = 0; i < iterations; i++) {
+                executor.submit(() -> {
+                    int key = rnd.nextInt(maxKey);
+                    UUID uuid = UUID.randomUUID();
+                    clhm.put(key, uuid);
+                    map.put(key, uuid);
+                });
+            }
+        } finally {
+            executor.shutdown();
+        }
+        executor.awaitTermination(waitSeconds, TimeUnit.SECONDS);
+        assertEquals(maxEntries, clhm.size());
+        int matched = 0;
+        for (int i = 0; i < maxKey; i++) {
+            UUID uuid = clhm.get(i);
+            if (uuid != null) {
+                matched++;
+                assertEquals(map.get(i), uuid);
+            }
+        }
+        assertEquals(maxEntries, matched);
+    }
+}

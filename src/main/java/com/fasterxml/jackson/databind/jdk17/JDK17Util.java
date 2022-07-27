4417418a70f48ad801ec9913032ffe3ae4b2c6af
Andy Boothe <andy.boothe@gmail.com>
2022-07-27 08:50:21 -0500

    spacing

--- src/main/java/com/fasterxml/jackson/databind/jdk17/JDK17Util.java
+++ src/main/java/com/fasterxml/jackson/databind/jdk17/JDK17Util.java
@@ -12,74 +12,74 @@ import com.fasterxml.jackson.databind.util.NativeImageUtil;
  * @since 2.14
  */
 public class JDK17Util {
-  public static Boolean isSealed(Class<?> type) {
-    return SealedClassAccessor.instance().isSealed(type);
-  }
-
-  public static Class<?>[] getPermittedSubclasses(Class<?> sealedType) {
-    return SealedClassAccessor.instance().getPermittedSubclasses(sealedType);
-  }
-
-  static class SealedClassAccessor {
-    private final Method SEALED_IS_SEALED;
-    private final Method SEALED_GET_PERMITTED_SUBCLASSES;
-
-    private final static SealedClassAccessor INSTANCE;
-    private final static RuntimeException PROBLEM;
-
-    static {
-      RuntimeException prob = null;
-      SealedClassAccessor inst = null;
-      try {
-        inst = new SealedClassAccessor();
-      } catch (RuntimeException e) {
-        prob = e;
-      }
-      INSTANCE = inst;
-      PROBLEM = prob;
+    public static Boolean isSealed(Class<?> type) {
+        return SealedClassAccessor.instance().isSealed(type);
     }
 
-    private SealedClassAccessor() throws RuntimeException {
-      try {
-        SEALED_IS_SEALED = Class.class.getMethod("isSealed");
-        SEALED_GET_PERMITTED_SUBCLASSES = Class.class.getMethod("getPermittedSubclasses");
-      } catch (Exception e) {
-        throw new RuntimeException(
-            String.format("Failed to access Methods needed to support sealed classes: (%s) %s",
-                e.getClass().getName(), e.getMessage()),
-            e);
-      }
+    public static Class<?>[] getPermittedSubclasses(Class<?> sealedType) {
+        return SealedClassAccessor.instance().getPermittedSubclasses(sealedType);
     }
 
-    public static SealedClassAccessor instance() {
-      if (PROBLEM != null) {
-        throw PROBLEM;
-      }
-      return INSTANCE;
-    }
+    static class SealedClassAccessor {
+        private final Method SEALED_IS_SEALED;
+        private final Method SEALED_GET_PERMITTED_SUBCLASSES;
 
-    public Boolean isSealed(Class<?> type) throws IllegalArgumentException {
-      try {
-        return (Boolean) SEALED_IS_SEALED.invoke(type);
-      } catch (Exception e) {
-        if (NativeImageUtil.isUnsupportedFeatureError(e)) {
-          return null;
+        private final static SealedClassAccessor INSTANCE;
+        private final static RuntimeException PROBLEM;
+
+        static {
+            RuntimeException prob = null;
+            SealedClassAccessor inst = null;
+            try {
+                inst = new SealedClassAccessor();
+            } catch (RuntimeException e) {
+                prob = e;
+            }
+            INSTANCE = inst;
+            PROBLEM = prob;
         }
-        throw new IllegalArgumentException(
-            "Failed to access sealedness of type " + ClassUtil.nameOf(type));
-      }
-    }
 
-    public Class<?>[] getPermittedSubclasses(Class<?> sealedType) throws IllegalArgumentException {
-      try {
-        return (Class<?>[]) SEALED_GET_PERMITTED_SUBCLASSES.invoke(sealedType);
-      } catch (Exception e) {
-        if (NativeImageUtil.isUnsupportedFeatureError(e)) {
-          return null;
+        private SealedClassAccessor() throws RuntimeException {
+            try {
+                SEALED_IS_SEALED = Class.class.getMethod("isSealed");
+                SEALED_GET_PERMITTED_SUBCLASSES = Class.class.getMethod("getPermittedSubclasses");
+            } catch (Exception e) {
+                throw new RuntimeException(
+                    String.format("Failed to access Methods needed to support sealed classes: (%s) %s",
+                        e.getClass().getName(), e.getMessage()),
+                    e);
+            }
         }
-        throw new IllegalArgumentException(
-            "Failed to access permitted subclasses of type " + ClassUtil.nameOf(sealedType));
-      }
+
+        public static SealedClassAccessor instance() {
+            if (PROBLEM != null) {
+                throw PROBLEM;
+            }
+            return INSTANCE;
+        }
+
+        public Boolean isSealed(Class<?> type) throws IllegalArgumentException {
+            try {
+                return (Boolean) SEALED_IS_SEALED.invoke(type);
+            } catch (Exception e) {
+                if (NativeImageUtil.isUnsupportedFeatureError(e)) {
+                    return null;
+                }
+                throw new IllegalArgumentException(
+                    "Failed to access sealedness of type " + ClassUtil.nameOf(type));
+            }
+        }
+
+        public Class<?>[] getPermittedSubclasses(Class<?> sealedType) throws IllegalArgumentException {
+            try {
+                return (Class<?>[]) SEALED_GET_PERMITTED_SUBCLASSES.invoke(sealedType);
+            } catch (Exception e) {
+                if (NativeImageUtil.isUnsupportedFeatureError(e)) {
+                    return null;
+                }
+                throw new IllegalArgumentException(
+                    "Failed to access permitted subclasses of type " + ClassUtil.nameOf(sealedType));
+            }
+        }
     }
-  }
 }

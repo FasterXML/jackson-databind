59f0de519a97f9f521eac908cb4d5d4261df98b3
FelixVaughan01 <104791446+FelixVaughan01@users.noreply.github.com>
2022-10-17 19:58:54 -0600

    Implementation for "Provide method ObjectMapper.copyWith(JsonFactory)" Closes  #3212 (#3626)

--- src/main/java/com/fasterxml/jackson/databind/ObjectMapper.java
+++ src/main/java/com/fasterxml/jackson/databind/ObjectMapper.java
@@ -592,7 +592,12 @@ public class ObjectMapper
      */
     protected ObjectMapper(ObjectMapper src)
     {
-        _jsonFactory = src._jsonFactory.copy();
+        this(src, null);
+    }
+
+    protected ObjectMapper(ObjectMapper src, JsonFactory factory)
+    {
+        _jsonFactory = factory != null ? factory : src._jsonFactory.copy();
         _jsonFactory.setCodec(this);
         _subtypeResolver = src._subtypeResolver.copy();
         _typeFactory = src._typeFactory;
@@ -603,10 +608,10 @@ public class ObjectMapper
 
         RootNameLookup rootNames = new RootNameLookup();
         _serializationConfig = new SerializationConfig(src._serializationConfig,
-                _subtypeResolver, _mixIns, rootNames, _configOverrides);
+        _subtypeResolver, _mixIns, rootNames, _configOverrides);
         _deserializationConfig = new DeserializationConfig(src._deserializationConfig,
-                _subtypeResolver, _mixIns, rootNames, _configOverrides,
-                _coercionConfigs);
+        _subtypeResolver, _mixIns, rootNames, _configOverrides,
+        _coercionConfigs);
         _serializerProvider = src._serializerProvider.copy();
         _deserializationContext = src._deserializationContext.copy();
 
@@ -713,6 +718,11 @@ public class ObjectMapper
     public ObjectMapper copy() {
         _checkInvalidCopy(ObjectMapper.class);
         return new ObjectMapper(this);
+    }
+
+    public ObjectMapper copyWith(JsonFactory factory) {
+        _checkInvalidCopy(ObjectMapper.class);
+        return new ObjectMapper(this, factory);
     }
 
     /**
@@ -1140,6 +1150,7 @@ public class ObjectMapper
     public ObjectMapper findAndRegisterModules() {
         return registerModules(findModules());
     }
+
 
     /*
     /**********************************************************
--- src/test/java/com/fasterxml/jackson/databind/ObjectMapperTest.java
+++ src/test/java/com/fasterxml/jackson/databind/ObjectMapperTest.java
@@ -204,6 +204,46 @@ public class ObjectMapperTest extends BaseMapTest
         assertNotNull(result);
     }
 
+    public void testCopyWith() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        //configuring some settings to non-defaults
+        mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true);
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        mapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, true);
+        JsonFactory newFactory = JsonFactory.builder()
+            .configure(JsonFactory.Feature.USE_THREAD_LOCAL_FOR_BUFFER_RECYCLING, false)
+            .build();
+        ObjectMapper copiedMapper = mapper.copyWith(newFactory);
+        String json = "{ \"color\" : \"Black\", \"free\" : \"true\", \"pages\" : \"204.04\" }";
+        JsonNode readResult = copiedMapper.readTree(json);
+        //validate functionality
+        assertEquals("Black", readResult.get("color").asText());
+        assertEquals(true, readResult.get("free").asBoolean());
+        assertEquals(204, readResult.get("pages").asInt());
+        String readResultAsString = "{\n  \"color\" : \"Black\",\n  \"free\" : \"true\",\n  \"pages\" : \"204.04\"\n}";
+        System.out.println(mapper.writeValueAsString(readResult));
+        assertEquals(readResultAsString, mapper.writeValueAsString(readResult));
+
+        //validate properties
+        Boolean mapperConfig1 = mapper._deserializationConfig.isEnabled(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
+        Boolean copiedMapperConfig1 = copiedMapper._deserializationConfig.isEnabled(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES);
+        Boolean mapperConfig2 = mapper._deserializationConfig.isEnabled(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+        Boolean copiedMapperConfig2 = copiedMapper._deserializationConfig.isEnabled(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+        Boolean mapperConfig3 = mapper._serializationConfig.isEnabled(SerializationFeature.INDENT_OUTPUT);
+        Boolean copiedMapperConfig3 = copiedMapper._serializationConfig.isEnabled(SerializationFeature.INDENT_OUTPUT);
+        Boolean mapperConfig4 = mapper._serializationConfig.isEnabled(SerializationFeature.FAIL_ON_SELF_REFERENCES);
+        Boolean copiedMapperConfig4 = copiedMapper._serializationConfig.isEnabled(SerializationFeature.FAIL_ON_SELF_REFERENCES);
+        assertNotSame(mapper.getFactory(), copiedMapper.getFactory());
+        assertSame(mapperConfig1, copiedMapperConfig1);
+        assertSame(mapperConfig2, copiedMapperConfig2);
+        assertSame(mapperConfig3, copiedMapperConfig3);
+        assertSame(mapperConfig4, copiedMapperConfig4);
+        assertNotSame(mapper.getFactory().isEnabled(JsonFactory.Feature.USE_THREAD_LOCAL_FOR_BUFFER_RECYCLING),
+            copiedMapper.getFactory().isEnabled(JsonFactory.Feature.USE_THREAD_LOCAL_FOR_BUFFER_RECYCLING)
+        );
+    }
+
     public void testFailedCopy() throws Exception
     {
         NoCopyMapper src = new NoCopyMapper();

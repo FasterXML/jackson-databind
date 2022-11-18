01159102ba3d2b62343d3b635328cfb9456e8af5
yawkat <jonas.konrad@oracle.com>
2022-11-18 12:06:57 +0100

    print footprint before the assertion so i can figure out the test failure...

--- src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
+++ src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
@@ -3,13 +3,14 @@ package com.fasterxml.jackson.databind;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openjdk.jol.info.GraphLayout;
+import org.openjdk.jol.vm.VM;
 
 public class MapperFootprintTest {
     @Test
     public void testMapperFootprint() {
         // memory footprint limit for the ObjectMapper
         GraphLayout mapperLayout = GraphLayout.parseInstance(new ObjectMapper());
-        Assert.assertTrue(mapperLayout.totalSize() < 50000);
         System.out.println(mapperLayout.toFootprint());
+        Assert.assertTrue(mapperLayout.totalSize() < 50000);
     }
 }

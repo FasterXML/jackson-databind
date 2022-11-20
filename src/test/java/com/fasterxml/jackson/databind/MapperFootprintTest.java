a2c7ce49bfca5a5df590c08b10896dcfdd7ba2ff
Jonas Konrad <me@yawk.at>
2022-11-20 07:38:12 +0100

    fix test again

--- src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
+++ src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
@@ -14,8 +14,15 @@ public class MapperFootprintTest {
             System.gc();
             Thread.sleep(100);
         }
-        GraphLayout mapperLayout = GraphLayout.parseInstance(new ObjectMapper())
+        // do this calculation twice. If there's a GC in one case, and the subtract call doesn't work well because of
+        // this, we can fall back to the other.
+        GraphLayout mapperLayoutA = GraphLayout.parseInstance(new ObjectMapper())
                 .subtract(GraphLayout.parseInstance(new ObjectMapper()));
+        GraphLayout mapperLayoutB = GraphLayout.parseInstance(new ObjectMapper())
+                .subtract(GraphLayout.parseInstance(new ObjectMapper()));
+        GraphLayout mapperLayout = mapperLayoutA.totalSize() > mapperLayoutB.totalSize() ?
+                mapperLayoutB : mapperLayoutA;
+
         Assert.assertTrue(
                 "ObjectMapper memory footprint exceeded limit. Footprint details: " + mapperLayout.toFootprint(),
                 mapperLayout.totalSize() < 10000);

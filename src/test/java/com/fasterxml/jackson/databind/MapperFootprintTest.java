890d4a9c04e5bea2237bdf33f82df7809997efee
Jonas Konrad <me@yawk.at>
2022-11-20 08:13:18 +0100

    try guava awaitFullGc

--- src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
+++ src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
@@ -1,21 +1,17 @@
 package com.fasterxml.jackson.databind;
 
+import com.google.common.testing.GcFinalization;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openjdk.jol.info.GraphLayout;
 
 public class MapperFootprintTest {
     @Test
-    @Ignore
     public void testMapperFootprint() throws InterruptedException {
         // memory footprint limit for the ObjectMapper
 
         // force gc (see javadoc of GraphLayout.subtract)
-        for (int i = 0; i < 5; i++) {
-            System.gc();
-            Thread.sleep(100);
-        }
+        GcFinalization.awaitFullGc();
         // do this calculation twice. If there's a GC in one case, and the subtract call doesn't work well because of
         // this, we can fall back to the other.
         GraphLayout mapperLayoutA = GraphLayout.parseInstance(new ObjectMapper())

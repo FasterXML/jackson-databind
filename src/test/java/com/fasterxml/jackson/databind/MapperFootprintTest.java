59443b9966e998ceac66083d4888678be1e0e6a3
Jonas Konrad <me@yawk.at>
2022-11-20 07:34:15 +0100

    fix test again

--- src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
+++ src/test/java/com/fasterxml/jackson/databind/MapperFootprintTest.java
@@ -1,18 +1,23 @@
 package com.fasterxml.jackson.databind;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openjdk.jol.info.GraphLayout;
 
 public class MapperFootprintTest {
     @Test
-    @Ignore
-    public void testMapperFootprint() {
+    public void testMapperFootprint() throws InterruptedException {
         // memory footprint limit for the ObjectMapper
+
+        // force gc (see javadoc of GraphLayout.subtract)
+        for (int i = 0; i < 5; i++) {
+            System.gc();
+            Thread.sleep(100);
+        }
         GraphLayout mapperLayout = GraphLayout.parseInstance(new ObjectMapper())
                 .subtract(GraphLayout.parseInstance(new ObjectMapper()));
-        System.out.println(mapperLayout.toFootprint());
-        Assert.assertTrue(mapperLayout.totalSize() < 50000);
+        Assert.assertTrue(
+                "ObjectMapper memory footprint exceeded limit. Footprint details: " + mapperLayout.toFootprint(),
+                mapperLayout.totalSize() < 10000);
     }
 }

23ea48c0abb466aaac2020be802f243f242c4c9c
PJ Fanning <pjfanning@users.noreply.github.com>
2023-01-27 23:16:47 +0000

    change TokenBuffer parser getDecimalValue to avoid using lossy double parsing (#3751)

--- src/main/java/com/fasterxml/jackson/databind/util/TokenBuffer.java
+++ src/main/java/com/fasterxml/jackson/databind/util/TokenBuffer.java
@@ -1795,7 +1795,7 @@ sb.append("NativeObjectIds=").append(_hasNativeObjectIds).append(",");
         @Override
         public BigInteger getBigIntegerValue() throws IOException
         {
-            Number n = getNumberValue();
+            Number n = getNumberValue(true);
             if (n instanceof BigInteger) {
                 return (BigInteger) n;
             } else if (n instanceof BigDecimal) {
@@ -1808,7 +1808,7 @@ sb.append("NativeObjectIds=").append(_hasNativeObjectIds).append(",");
         @Override
         public BigDecimal getDecimalValue() throws IOException
         {
-            Number n = getNumberValue();
+            Number n = getNumberValue(true);
             if (n instanceof BigDecimal) {
                 return (BigDecimal) n;
             } else if (n instanceof Integer) {
@@ -1869,6 +1869,10 @@ sb.append("NativeObjectIds=").append(_hasNativeObjectIds).append(",");
 
         @Override
         public final Number getNumberValue() throws IOException {
+            return getNumberValue(false);
+        }
+
+        private Number getNumberValue(final boolean preferBigNumbers) throws IOException {
             _checkIsNumber();
             Object value = _currentObject();
             if (value instanceof Number) {
@@ -1880,7 +1884,13 @@ sb.append("NativeObjectIds=").append(_hasNativeObjectIds).append(",");
             if (value instanceof String) {
                 String str = (String) value;
                 if (str.indexOf('.') >= 0) {
+                    if (preferBigNumbers) {
+                        return NumberInput.parseBigDecimal(str,
+                                isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
+                    }
                     return NumberInput.parseDouble(str, isEnabled(StreamReadFeature.USE_FAST_DOUBLE_PARSER));
+                } else if (preferBigNumbers) {
+                    return NumberInput.parseBigInteger(str, isEnabled(StreamReadFeature.USE_FAST_BIG_NUMBER_PARSER));
                 }
                 return NumberInput.parseLong(str);
             }

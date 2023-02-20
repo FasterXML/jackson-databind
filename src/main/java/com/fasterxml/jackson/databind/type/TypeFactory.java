16f44d795935bc58946394d8463ae3036da36758
Kim, Joo Hyuk <beanskobe@gmail.com>
2023-02-20 09:23:31 +0900

    Doc :  Improve JavaDoc of `TypeFactory.constructMapLikeType` methods (#3790)

--- src/main/java/com/fasterxml/jackson/databind/type/TypeFactory.java
+++ src/main/java/com/fasterxml/jackson/databind/type/TypeFactory.java
@@ -997,8 +997,13 @@ public class TypeFactory // note: was final in 2.9, removed from 2.10
     }
 
     /**
-     * Method for constructing a {@link MapLikeType} instance
-     *<p>
+     * Method for constructing a {@link MapLikeType} instance.
+     * <p>
+     * Do not use this method to create a true Map type -- use {@link #constructMapType} instead.
+     * Map-like types are only meant for supporting things that do not implement Map interface
+     * and as such cannot use standard Map handlers.
+     * </p>
+     * <p>
      * NOTE: type modifiers are NOT called on constructed type itself; but are called
      * for contained types.
      */
@@ -1010,7 +1015,12 @@ public class TypeFactory // note: was final in 2.9, removed from 2.10
 
     /**
      * Method for constructing a {@link MapLikeType} instance
-     *<p>
+     * <p>
+     * Do not use this method to create a true Map type -- use {@link #constructMapType} instead.
+     * Map-like types are only meant for supporting things that do not implement Map interface
+     * and as such cannot use standard Map handlers.
+     * </p>
+     * <p>
      * NOTE: type modifiers are NOT called on constructed type itself.
      */
     public MapLikeType constructMapLikeType(Class<?> mapClass, JavaType keyType, JavaType valueType) {

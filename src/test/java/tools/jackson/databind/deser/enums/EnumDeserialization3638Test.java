6b9556c213e8a2b55c073d361a8f37ee99f770be
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-03-02 20:17:48 -0800

    Add accidentally rm'ed new test

---
+++ src/test/java/tools/jackson/databind/deser/enums/EnumDeserialization3638Test.java
@@ -0,0 +1,57 @@
+package tools.jackson.databind.deser.enums;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import tools.jackson.databind.*;
+
+public class EnumDeserialization3638Test extends BaseMapTest
+{
+    /*
+    /**********************************************************
+    /* Set up
+    /**********************************************************
+     */
+
+    static enum Member
+    {
+        FIRST_MEMBER,
+        SECOND_MEMBER;
+    }
+
+    static class SensitiveBean
+    {
+        @JsonFormat(without = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+        public Member enumValue;
+    }
+
+    static class InsensitiveBean
+    {
+        @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+        public Member enumValue;
+    }
+
+    private final ObjectMapper MAPPER = newJsonMapper();
+    
+    /*
+    /**********************************************************
+    /* Tests
+    /**********************************************************
+     */
+
+    public void testCaseSensitive() throws Exception {
+        String json = a2q("{'enumValue':'1'}");
+
+        SensitiveBean sensitiveBean = MAPPER.readValue(json, SensitiveBean.class);
+
+        assertEquals(Member.SECOND_MEMBER, sensitiveBean.enumValue);
+    }
+
+
+    public void testCaseInsensitive() throws Exception {
+        String json = a2q("{'enumValue':'1'}");
+
+        InsensitiveBean insensitiveBean = MAPPER.readValue(json, InsensitiveBean.class);
+
+        assertEquals(Member.SECOND_MEMBER, insensitiveBean.enumValue);
+    }
+}

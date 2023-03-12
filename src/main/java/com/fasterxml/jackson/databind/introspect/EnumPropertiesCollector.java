6799d2d27b1dd670c92d06d494ef8e7edb156a25
joohyukkim <beanskobe@gmail.com>
2023-03-12 10:25:12 +0900

    Clean EnumPropertiesCollector

--- src/main/java/com/fasterxml/jackson/databind/introspect/EnumPropertiesCollector.java
+++ src/main/java/com/fasterxml/jackson/databind/introspect/EnumPropertiesCollector.java
@@ -22,16 +22,16 @@ public class EnumPropertiesCollector {
     /**
      * Factory method for creating an instance of {@link EnumNamingStrategy} from a provided {@code namingDef}.
      *
-     * @param namingDef subclass of {@link EnumNamingStrategy} to initialize an instance of.
+     * @param namingDef                  subclass of {@link EnumNamingStrategy} to initialize an instance of.
      * @param canOverrideAccessModifiers whether to override access modifiers when instantiating the naming strategy.
      *
-     * @throws IllegalArgumentException if {@code namingDef} is not an instance of {@link java.lang.Class} or
-     *              not a subclass of {@link EnumNamingStrategy}.
-     *
      * @return an instance of {@link EnumNamingStrategy} if {@code namingDef} is a subclass of {@link EnumNamingStrategy},
-     *         {@code null} if {@code namingDef} is {@code null},
-     *         and an instance of {@link EnumNamingStrategy} if {@code namingDef} already is one.
+     * {@code null} if {@code namingDef} is {@code null},
+     * and an instance of {@link EnumNamingStrategy} if {@code namingDef} already is one.
      *
+     * @throws IllegalArgumentException if {@code namingDef} is not an instance of {@link java.lang.Class} or
+     *                                  not a subclass of {@link EnumNamingStrategy}.
+     *                                  
      * @since 2.15
      */
     public static EnumNamingStrategy createEnumNamingStrategyInstance(Object namingDef, boolean canOverrideAccessModifiers) {
@@ -42,8 +42,9 @@ public class EnumPropertiesCollector {
             return (EnumNamingStrategy) namingDef;
         }
         if (!(namingDef instanceof Class)) {
-            reportProblem("AnnotationIntrospector returned EnumNamingStrategy definition of type %s"
-                + "; expected type `Class<EnumNamingStrategy>` instead", ClassUtil.classNameOf(namingDef));
+            throw new IllegalArgumentException(String.format(
+                "AnnotationIntrospector returned EnumNamingStrategy definition of type %s; " +
+                    "expected type `Class<EnumNamingStrategy>` instead", ClassUtil.classNameOf(namingDef)));
         }
 
         Class<?> namingClass = (Class<?>) namingDef;
@@ -52,28 +53,10 @@ public class EnumPropertiesCollector {
             return null;
         }
         if (!EnumNamingStrategy.class.isAssignableFrom(namingClass)) {
-            reportProblem("AnnotationIntrospector returned Class %s; expected `Class<EnumNamingStrategy>`",
-                ClassUtil.classNameOf(namingClass));
+            throw new IllegalArgumentException(String.format(
+                "Problem with AnnotationIntrospector returned Class %s; " +
+                    "expected `Class<EnumNamingStrategy>`", ClassUtil.classNameOf(namingClass)));
         }
         return (EnumNamingStrategy) ClassUtil.createInstance(namingClass, canOverrideAccessModifiers);
     }
-
-    /*
-     *********************************************************
-     * Internal methods; helpers
-     **********************************************************
-     */
-
-    /**
-     * @throws IllegalArgumentException with provided message.
-     *
-     * @since 2.15
-     */
-    protected static void reportProblem(String msg, Object... args) {
-        if (args.length > 0) {
-            msg = String.format(msg, args);
-        }
-        throw new IllegalArgumentException("Problem with " + msg);
-    }
-
 }

282507840282a17965b8d37cf0b9f187f206b17c
PJ Fanning <pjfanning@users.noreply.github.com>
2023-03-14 01:21:21 +0100

    Create ExceptionUtilTest.java

---
+++ src/test/java/com/fasterxml/jackson/databind/util/ExceptionUtilTest.java
@@ -0,0 +1,28 @@
+package com.fasterxml.jackson.databind.util;
+
+import com.fasterxml.jackson.databind.BaseTest;
+
+public class ExceptionUtilTest extends BaseTest {
+    public void testNoClassDefError() {
+        //next line should be a no-op
+        ExceptionUtil.rethrowIfFatal(new NoClassDefFoundError("fake"));
+    }
+
+    public void testOutOfMemoryError() {
+        try {
+            ExceptionUtil.rethrowIfFatal(new OutOfMemoryError("fake"));
+            fail("expected OutOfMemoryError");
+        } catch (OutOfMemoryError err) {
+            assertEquals("fake", err.getMessage());
+        }
+    }
+
+    public void testVerifyError() {
+        try {
+            ExceptionUtil.rethrowIfFatal(new VerifyError("fake"));
+            fail("expected VerifyError");
+        } catch (VerifyError err) {
+            assertEquals("fake", err.getMessage());
+        }
+    }
+}

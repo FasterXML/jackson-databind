6682ef3a871c46f5f9a8ce7bb3ba09b13d48690d
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-04-18 15:55:52 -0700

    Minor streamlining wrt #3888

--- src/test/java/com/fasterxml/jackson/failing/JsonValueIgnore3647Test.java
+++ src/test/java/com/fasterxml/jackson/failing/JsonValueIgnore3647Test.java
@@ -9,53 +9,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonValueIgnore3647Test extends BaseMapTest
 {
     // for [databind#3647]
-    static class Foo {
-        String p1 = "hello";
-        String p2 = "world";
-
-        public String getP1() {
-            return p1;
-        }
-
-        public void setP1(String p1) {
-            this.p1 = p1;
-        }
-
-        public String getP2() {
-            return p2;
-        }
-
-        public void setP2(String p2) {
-            this.p2 = p2;
-        }
+    static class Foo3647 {
+        public String p1 = "hello";
+        public String p2 = "world";
     }
 
-    // for [databind#3647]
-    static class Bar {
+    static class Bar3647 {
         @JsonValue
         @JsonIgnoreProperties("p1")
-        Foo foo = new Foo();
-        public Foo getFoo() {
-            return foo;
-        }
-        public void setFoo(Foo foo) {
-            this.foo = foo;
+        public Foo3647 getFoo() {
+            return new Foo3647();
         }
     }
 
-    private final ObjectMapper MAPPER = new ObjectMapper();
-
     /*
-    /**********************************************************
+    /**********************************************************************
     /* Test methods
-    /**********************************************************
-    */
+    /**********************************************************************
+     */
+
+    private final ObjectMapper MAPPER = newJsonMapper();
 
     // [databind#3647]
     public void testIgnorePropsAnnotatedJsonValue() throws Exception
     {
-        final String result = "{\"p2\":\"world\"}";
-        final String jsonStringWithIgnoredProps = MAPPER.writeValueAsString(new Bar());
-        assertEquals(result, jsonStringWithIgnoredProps);
+        assertEquals("{\"p2\":\"world\"}", MAPPER.writeValueAsString(new Bar3647()));
     }
 }

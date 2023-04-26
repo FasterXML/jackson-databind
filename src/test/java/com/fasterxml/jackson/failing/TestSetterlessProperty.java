29b488366bb5140e7e1aaf4a51c9e2922479f638
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-26 11:29:20 +0200

    Create TestSetterlessProperty.java

---
+++ src/test/java/com/fasterxml/jackson/failing/TestSetterlessProperty.java
@@ -0,0 +1,39 @@
+package com.fasterxml.jackson.failing;
+
+import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.testutil.NoCheckSubTypeValidator;
+
+public class TestSetterlessProperty
+    extends BaseMapTest
+{
+    static class ImmutableId {
+        private final int id;
+
+        public ImmutableId(int id) { this.id = id; }
+
+        public int getId() {
+            return id;
+        }
+    }
+
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+
+    // For [databind#501]
+    public void testSetterlessProperty() throws Exception
+    {
+        ImmutableId input = new ImmutableId(13);
+        ObjectMapper m = new ObjectMapper();
+        String json = m.writerWithDefaultPrettyPrinter().writeValueAsString(input);
+
+        ImmutableId output = m.readValue(json, ImmutableId.class);
+        assertNotNull(output);
+
+        assertEquals(input.id, output.id);
+    }
+}

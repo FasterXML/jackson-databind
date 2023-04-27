b3954a0807b77b6206bdf8b2c88afeba1354299b
joohyukkim <beanskobe@gmail.com>
2023-04-28 00:19:03 +0900

    Update RecordDeserialization3897Test.java

--- src/test/java/com/fasterxml/jackson/failing/RecordDeserialization3897Test.java
+++ src/test/java/com/fasterxml/jackson/failing/RecordDeserialization3897Test.java
@@ -3,7 +3,8 @@ package com.fasterxml.jackson.failing;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.BaseMapTest;
 
-// [databinding#3897] record class breaks deserialization when there's only a single field, and marked Access.WRITE_ONLY
+// [databinding#3897] This is failing test for `Record` class deserialization with single field annotated with
+// `JsonProperty.Access.WRITE_ONLY`. Regression from Jackson 2.14.2
 public class RecordDeserialization3897Test extends BaseMapTest {
 
     public record Example(
@@ -11,11 +12,12 @@ public class RecordDeserialization3897Test extends BaseMapTest {
         String value
     ) {}
 
+    // Passes in 2.14.2, but does not in 2.15.0 
     public void testRecordWithWriteOnly() throws Exception {
         final String JSON = a2q("{'value':'foo'}");
 
         Example result = newJsonMapper().readValue(JSON, Example.class);
-        
+
         assertEquals("foo", result.value());
     }
 }

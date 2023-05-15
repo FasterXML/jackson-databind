3d60f6ffa9bb1d72fd791923f0d61e39bb0f9ce3
PJ Fanning <pjfanning@users.noreply.github.com>
2023-05-15 14:02:45 +0100

    Update SerializationOrderTest.java

--- src/test/java/com/fasterxml/jackson/databind/ser/SerializationOrderTest.java
+++ src/test/java/com/fasterxml/jackson/databind/ser/SerializationOrderTest.java
@@ -134,9 +134,51 @@ public class SerializationOrderTest
     static class NestedClassOne {
         private String id;
         private String name;
+
+        @JsonProperty(value = "nestedProperty")
         private NestedClassTwo nestedClassTwo;
 
         NestedClassOne(String id,
+                       String name,
+                       @JsonProperty(value = "nestedProperty") NestedClassTwo nestedClassTwo) {
+            this.id = id;
+            this.name = name;
+            this.nestedClassTwo = nestedClassTwo;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public NestedClassTwo getNestedClassTwo() {
+            return nestedClassTwo;
+        }
+
+        public void setNestedClassTwo(NestedClassTwo nestedClassTwo) {
+            this.nestedClassTwo = nestedClassTwo;
+        }
+    }
+
+    static class NestedClassOneWithDuplicatedJsonPropertyAnnotations {
+        private String id;
+        private String name;
+
+        @JsonProperty(value = "nestedProperty")
+        private NestedClassTwo nestedClassTwo;
+
+        NestedClassOneWithDuplicatedJsonPropertyAnnotations(String id,
                        String name,
                        @JsonProperty(value = "nestedProperty") NestedClassTwo nestedClassTwo) {
             this.id = id;
@@ -293,6 +335,14 @@ public class SerializationOrderTest
     }
 
     public void testSerializationOrderWithJsonProperty() throws Exception {
+        NestedClassTwo nestedTwo = new NestedClassTwo("2", "111110");
+        NestedClassOne nestedOne = new NestedClassOne("1", "test@records.com", nestedTwo);
+        final String output = MAPPER.writeValueAsString(nestedOne);
+        final String expected = "{\"id\":\"1\",\"email\":\"test@records.com\",\"nestedProperty\":{\"id\":\"2\",\"passport\":\"111110\"}}";
+        assertEquals(expected, output);
+    }
+
+    public void testSerializationOrderWithDuplicatedJsonProperty() throws Exception {
         NestedClassTwo nestedTwo = new NestedClassTwo("2", "111110");
         NestedClassOne nestedOne = new NestedClassOne("1", "test@records.com", nestedTwo);
         final String output = MAPPER.writeValueAsString(nestedOne);

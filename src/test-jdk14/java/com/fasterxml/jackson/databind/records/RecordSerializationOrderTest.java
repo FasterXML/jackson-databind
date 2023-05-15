b59164466289d3c2e87dc4eeb1e49fe956680542
PJ Fanning <pjfanning@users.noreply.github.com>
2023-05-15 14:13:08 +0100

    Update RecordSerializationOrderTest.java

--- src/test-jdk14/java/com/fasterxml/jackson/databind/records/RecordSerializationOrderTest.java
+++ src/test-jdk14/java/com/fasterxml/jackson/databind/records/RecordSerializationOrderTest.java
@@ -1,6 +1,7 @@
 package com.fasterxml.jackson.databind.records;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.BaseMapTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -9,9 +10,15 @@ public class RecordSerializationOrderTest extends BaseMapTest
     record NestedRecordOne(String id, String email, NestedRecordTwo nestedRecordTwo) {}
     record NestedRecordOneWithJsonProperty(String id, String email,
                                            @JsonProperty("nestedProperty") NestedRecordTwo nestedRecordTwo) {}
-    record NestedRecordOneWithJsonPropertyOrder(@JsonProperty(index = 2) String id,
+    record NestedRecordOneWithJsonPropertyIndex(@JsonProperty(index = 2) String id,
                                                 @JsonProperty(index = 0) String email,
                                                 @JsonProperty(value = "nestedProperty", index = 1) NestedRecordTwo nestedRecordTwo) {}
+
+    @JsonPropertyOrder({"email", "nestedProperty", "id"})
+    record NestedRecordOneWithJsonPropertyOrder(String id,
+                                                String email,
+                                                @JsonProperty(value = "nestedProperty") NestedRecordTwo nestedRecordTwo) {}
+
     record NestedRecordTwo(String id, String passport) {}
 
     private final ObjectMapper MAPPER = newJsonMapper();
@@ -36,6 +43,15 @@ public class RecordSerializationOrderTest extends BaseMapTest
                 new NestedRecordOneWithJsonProperty("1", "test@records.com", nestedRecordTwo);
         final String output = MAPPER.writeValueAsString(nestedRecordOne);
         final String expected = "{\"id\":\"1\",\"email\":\"test@records.com\",\"nestedProperty\":{\"id\":\"2\",\"passport\":\"111110\"}}";
+        assertEquals(expected, output);
+    }
+
+    public void testSerializationOrderWithJsonPropertyIndexes() throws Exception {
+        NestedRecordTwo nestedRecordTwo = new NestedRecordTwo("2", "111110");
+        NestedRecordOneWithJsonPropertyIndex nestedRecordOne =
+                new NestedRecordOneWithJsonPropertyIndex("1", "test@records.com", nestedRecordTwo);
+        final String output = MAPPER.writeValueAsString(nestedRecordOne);
+        final String expected = "{\"email\":\"test@records.com\",\"nestedProperty\":{\"id\":\"2\",\"passport\":\"111110\"},\"id\":\"1\"}";
         assertEquals(expected, output);
     }
 

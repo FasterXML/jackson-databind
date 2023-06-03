64594292901d82faad4e78fbad658e457e104202
joohyukkim <beanskobe@gmail.com>
2023-06-03 17:44:09 +0900

    Fix test

--- src/main/java/com/fasterxml/jackson/databind/deser/std/StdDeserializer.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/StdDeserializer.java
@@ -1421,7 +1421,6 @@ public abstract class StdDeserializer<T>
             return ob.toString();
         // 29-Jun-2020, tatu: New! "Scalar from Object" (mostly for XML)
         case JsonTokenId.ID_START_OBJECT:
-            
             return ctxt.extractScalarFromObject(p, this, rawTargetType);
         case JsonTokenId.ID_NUMBER_INT:
             act = _checkIntToStringCoercion(p, ctxt, rawTargetType);

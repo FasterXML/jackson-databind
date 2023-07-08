0c0ce4fcaf2cc90cb3f354eeeba647ef566e5fff
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-07-08 16:33:17 -0700

    Fix failing test (on master)

--- src/test/java/tools/jackson/databind/deser/dos/DeepJsonNodeSerTest.java
+++ src/test/java/tools/jackson/databind/deser/dos/DeepJsonNodeSerTest.java
@@ -1,6 +1,7 @@
 package tools.jackson.databind.deser.dos;
 
 import tools.jackson.core.StreamReadConstraints;
+import tools.jackson.core.StreamWriteConstraints;
 import tools.jackson.core.json.JsonFactory;
 import tools.jackson.databind.BaseMapTest;
 import tools.jackson.databind.JsonNode;
@@ -11,12 +12,15 @@ public class DeepJsonNodeSerTest extends BaseMapTest
 {
     private final JsonFactory jsonFactory = JsonFactory.builder()
             .streamReadConstraints(StreamReadConstraints.builder().maxNestingDepth(Integer.MAX_VALUE).build())
+            .streamWriteConstraints(StreamWriteConstraints.builder().maxNestingDepth(Integer.MAX_VALUE).build())
             .build();
     private final ObjectMapper MAPPER = JsonMapper.builder(jsonFactory).build();
 
     public void testVeryDeepNodeSer() throws Exception
     {
-        int depth = 9000;
+        // Keep within suitable limits; above default max (1000) but below serialization-side
+        // StackOverflow limits
+        int depth = 4000;
         StringBuilder jsonString = new StringBuilder();
         jsonString.append("{");
 
@@ -31,7 +35,7 @@ public class DeepJsonNodeSerTest extends BaseMapTest
         jsonString.append("}");
 
         JsonNode jsonNode = MAPPER.readTree(jsonString.toString());
-        String json = jsonNode.toString();
+        String json = MAPPER.writeValueAsString(jsonNode);
         assertNotNull(json);
     }
 }

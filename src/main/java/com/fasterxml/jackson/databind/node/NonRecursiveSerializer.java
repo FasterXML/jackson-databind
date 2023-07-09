7ada5e6552b292b18cf664ef921b3f1781def4e0
PJ Fanning <pjfanning@users.noreply.github.com>
2023-07-09 22:52:11 +0100

    Update NonRecursiveSerializer.java

--- src/main/java/com/fasterxml/jackson/databind/node/NonRecursiveSerializer.java
+++ src/main/java/com/fasterxml/jackson/databind/node/NonRecursiveSerializer.java
@@ -7,7 +7,13 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 import static com.fasterxml.jackson.core.JsonToken.*;
 

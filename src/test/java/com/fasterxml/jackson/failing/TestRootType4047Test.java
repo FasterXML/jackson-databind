86684a5f74a1996dd4dbfa4d382c31d74a6cf9bc
joohyukkim <beanskobe@gmail.com>
2023-07-22 16:18:39 +0900

    Update TestRootType4047Test.java

--- src/test/java/com/fasterxml/jackson/failing/TestRootType4047Test.java
+++ src/test/java/com/fasterxml/jackson/failing/TestRootType4047Test.java
@@ -3,6 +3,7 @@ package com.fasterxml.jackson.failing;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import com.fasterxml.jackson.databind.BaseMapTest;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
@@ -24,8 +25,7 @@ public class TestRootType4047Test extends BaseMapTest {
 
     private final String EVENT_JSON = "{\"event\":{\"id\":1,\"name\":\"foo\"}}";
 
-    public void testValueToTree() throws Exception 
-    {
+    public void testValueToTree() throws Exception {
         // Arrange
         final ObjectMapper WRAP_ROOT_MAPPER = jsonMapperBuilder()
                 .enable(SerializationFeature.WRAP_ROOT_VALUE)
@@ -44,29 +44,26 @@ public class TestRootType4047Test extends BaseMapTest {
     }
 
 
-    public void testTreeToValue() throws Exception 
-    {
+    public void testTreeToValue() throws Exception {
         // Arrange
         final ObjectMapper UNWRAP_MAPPER = jsonMapperBuilder()
                 .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
                 .build();
-
         Event value = new Event();
         value.id = 1L;
         value.name = "foo";
 
         // Act & Assert
-
         // (1) works
         Event event1 = UNWRAP_MAPPER.readValue(EVENT_JSON, Event.class);
         assertEquals(value.id, event1.id);
         assertEquals(value.name, event1.name);
 
-        // (2) fails (but shouldn't)
         try {
-            UNWRAP_MAPPER.treeToValue(
-                    UNWRAP_MAPPER.readTree(EVENT_JSON),
-                    Event.class);
+            // (2) fails (but shouldn't)
+            JsonNode tree = UNWRAP_MAPPER.readTree(EVENT_JSON);
+            // doesn't read here....
+            UNWRAP_MAPPER.treeToValue(tree, Event.class);
         } catch (MismatchedInputException e) {
             verifyException(e, "Root name ('event') does not match expected ('JsonNode')");
             throw e; // <-- should not throw

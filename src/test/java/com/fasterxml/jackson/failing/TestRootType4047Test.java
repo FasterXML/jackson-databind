c84165d75dc3ba1a0cdb292992ecdf90e5c3936b
joohyukkim <beanskobe@gmail.com>
2023-07-22 16:10:38 +0900

    Add test case for `UNWRAP_ROOT_VALUE`

--- src/test/java/com/fasterxml/jackson/failing/TestRootType4047Test.java
+++ src/test/java/com/fasterxml/jackson/failing/TestRootType4047Test.java
@@ -2,12 +2,13 @@ package com.fasterxml.jackson.failing;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
 import com.fasterxml.jackson.databind.BaseMapTest;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
 // [databind#4047] : ObjectMapper.valueToTree will ignore the configuration SerializationFeature.WRAP_ROOT_VALUE
-public class TestRootType4047Test extends BaseMapTest 
-{
+public class TestRootType4047Test extends BaseMapTest {
 
     @JsonRootName("event")
     static class Event {
@@ -21,22 +22,54 @@ public class TestRootType4047Test extends BaseMapTest
     /**********************************************************
      */
 
-    private final ObjectMapper WRAP_ROOT_MAPPER = jsonMapperBuilder()
-            .enable(SerializationFeature.WRAP_ROOT_VALUE)
-            .build();
+    private final String EVENT_JSON = "{\"event\":{\"id\":1,\"name\":\"foo\"}}";
 
     public void testValueToTree() throws Exception 
     {
+        // Arrange
+        final ObjectMapper WRAP_ROOT_MAPPER = jsonMapperBuilder()
+                .enable(SerializationFeature.WRAP_ROOT_VALUE)
+                .build();
         Event value = new Event();
         value.id = 1L;
         value.name = "foo";
 
-        String expected = "{\"event\":{\"id\":1,\"name\":\"foo\"}}";
-        
-        // if this one passes... (it does)
-        assertEquals(expected, WRAP_ROOT_MAPPER.writeValueAsString(value));
+        // (1) works
+        assertEquals(EVENT_JSON,
+                WRAP_ROOT_MAPPER.writeValueAsString(value));
 
-        // this one should pass also (but fails)
-        assertEquals(expected, WRAP_ROOT_MAPPER.valueToTree(value).toString());
+        // (2) fails w/ {"id":1,"name":"foo"}
+        assertEquals(EVENT_JSON,
+                WRAP_ROOT_MAPPER.valueToTree(value).toString());
+    }
+
+
+    public void testTreeToValue() throws Exception 
+    {
+        // Arrange
+        final ObjectMapper UNWRAP_MAPPER = jsonMapperBuilder()
+                .enable(DeserializationFeature.UNWRAP_ROOT_VALUE)
+                .build();
+
+        Event value = new Event();
+        value.id = 1L;
+        value.name = "foo";
+
+        // Act & Assert
+
+        // (1) works
+        Event event1 = UNWRAP_MAPPER.readValue(EVENT_JSON, Event.class);
+        assertEquals(value.id, event1.id);
+        assertEquals(value.name, event1.name);
+
+        // (2) fails (but shouldn't)
+        try {
+            UNWRAP_MAPPER.treeToValue(
+                    UNWRAP_MAPPER.readTree(EVENT_JSON),
+                    Event.class);
+        } catch (MismatchedInputException e) {
+            verifyException(e, "Root name ('event') does not match expected ('JsonNode')");
+            throw e; // <-- should not throw
+        }
     }
 }

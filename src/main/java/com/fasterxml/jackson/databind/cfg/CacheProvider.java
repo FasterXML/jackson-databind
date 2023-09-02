6345e36731f4dbfb36c72efe8977b9bc7ec53ab4
joohyukkim <beanskobe@gmail.com>
2023-09-02 12:01:36 +0900

    Sync changes with sub-PRs
    Update CacheProvider.java

--- src/main/java/com/fasterxml/jackson/databind/cfg/CacheProvider.java
+++ src/main/java/com/fasterxml/jackson/databind/cfg/CacheProvider.java
@@ -6,14 +6,15 @@ import com.fasterxml.jackson.databind.deser.DeserializerCache;
 import com.fasterxml.jackson.databind.util.LookupCache;
 
 /**
- * Interface that defines API for custom Cache configuration that overrides default cache implementations used.
+ * Interface that defines API Jackson uses for constructing various internal
+ * caches. This allows configuring custom caches and cache configurations.
  * A {@link CacheProvider} instance will be configured through a builder such as
  * {@link com.fasterxml.jackson.databind.json.JsonMapper.Builder#cacheProvider(CacheProvider)}
- * 
+ *
  * @since 2.16
  */
 public interface CacheProvider
-        extends java.io.Serializable
+    extends java.io.Serializable
 {
     /**
      * Method to provide a {@link LookupCache} instance for {@link DeserializerCache}

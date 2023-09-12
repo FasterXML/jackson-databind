3e4ab840d940d3378258c2ac20d6ca4d8e5cd64b
Kim, Joo Hyuk <beanskobe@gmail.com>
2023-09-12 09:03:09 +0900

    Improve JavaDoc regarding `MutableCoercionConfig` (#4112)

--- src/main/java/com/fasterxml/jackson/databind/cfg/MutableCoercionConfig.java
+++ src/main/java/com/fasterxml/jackson/databind/cfg/MutableCoercionConfig.java
@@ -1,9 +1,14 @@
 package com.fasterxml.jackson.databind.cfg;
 
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.type.LogicalType;
+
+import java.util.function.Consumer;
+
 /**
  * Mutable version of {@link CoercionConfig} (or rather, extended API)
  * exposed during configuration phase of {@link com.fasterxml.jackson.databind.ObjectMapper}
- * construction (via Builder).
+ * construction (via {@link JsonMapper#builder()}).
  *
  * @since 2.12
  */
@@ -23,6 +28,15 @@ public class MutableCoercionConfig
         return new MutableCoercionConfig(this);
     }
 
+    /**
+     * Method to set coercions to target type or class during builder-style mapper construction with
+     * <ul>
+     *     <li>{@link MapperBuilder#withCoercionConfig(Class, Consumer)},</li>
+     *     <li>{@link MapperBuilder#withCoercionConfig(LogicalType, Consumer)} and</li>
+     *     <li>{@link MapperBuilder#withCoercionConfigDefaults(Consumer)}</li>
+     * </ul>
+     * ... these builder methods. Refrain from using this method outside of builder phase.
+     */
     public MutableCoercionConfig setCoercion(CoercionInputShape shape,
             CoercionAction action) {
         _coercionsByShape[shape.ordinal()] = action;

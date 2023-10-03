254eb262df798450feb7775f7126accb706b0ed9
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-10-03 14:31:40 -0700

    ...

--- src/test/java/com/fasterxml/jackson/databind/util/ByteBufferUtilsTest.java
+++ src/test/java/com/fasterxml/jackson/databind/util/ByteBufferUtilsTest.java
@@ -16,22 +16,24 @@ public class ByteBufferUtilsTest extends BaseMapTest
 
     public void testByteBufferInput() throws Exception {
         byte[] input = new byte[] { 1, 2, 3 };
-        ByteBufferBackedInputStream wrapped = new ByteBufferBackedInputStream(ByteBuffer.wrap(input));
-        assertEquals(3, wrapped.available());
-        assertEquals(1, wrapped.read());
-        byte[] buffer = new byte[10];
-        assertEquals(2, wrapped.read(buffer, 0, 5));
-        wrapped.close();
+        try (ByteBufferBackedInputStream wrapped = new ByteBufferBackedInputStream(ByteBuffer.wrap(input))) {
+            assertEquals(3, wrapped.available());
+            assertEquals(1, wrapped.read());
+            byte[] buffer = new byte[10];
+            assertEquals(2, wrapped.read(buffer, 0, 5));
+            // And now ought to get -1
+            assertEquals(-1, wrapped.read(buffer, 0, 5));
+        }
     }
 
     public void testByteBufferOutput() throws Exception {
         ByteBuffer b = ByteBuffer.wrap(new byte[10]);
-        ByteBufferBackedOutputStream wrappedOut = new ByteBufferBackedOutputStream(b);
-        wrappedOut.write(1);
-        wrappedOut.write(new byte[] { 2, 3 });
-        assertEquals(3, b.position());
-        assertEquals(7, b.remaining());
-        wrappedOut.close();
+        try (ByteBufferBackedOutputStream wrappedOut = new ByteBufferBackedOutputStream(b)) {
+            wrappedOut.write(1);
+            wrappedOut.write(new byte[] { 2, 3 });
+            assertEquals(3, b.position());
+            assertEquals(7, b.remaining());
+        }
     }
 
     public void testReadFromByteBuffer() throws Exception

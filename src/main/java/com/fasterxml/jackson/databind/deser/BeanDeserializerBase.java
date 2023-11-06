066369f8ddd54798634c8fa78b0ba8ef02b00a92
Kim, Joo Hyuk <beanskobe@gmail.com>
2023-11-06 11:02:02 +0900

    Fix for 4185 (#4189)

--- src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerBase.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/BeanDeserializerBase.java
@@ -186,7 +186,7 @@ public abstract class BeanDeserializerBase
     protected UnwrappedPropertyHandler _unwrappedPropertyHandler;
 
     /**
-     * Handler that we need iff any of properties uses external
+     * Handler that we need if any of properties uses external
      * type id.
      */
     protected ExternalTypeHandler _externalTypeIdHandler;
@@ -292,6 +292,8 @@ public abstract class BeanDeserializerBase
         _serializationShape = src._serializationShape;
 
         _vanillaProcessing = src._vanillaProcessing;
+
+        _externalTypeIdHandler = src._externalTypeIdHandler;
     }
 
     protected BeanDeserializerBase(BeanDeserializerBase src, NameTransformer unwrapper)
@@ -332,6 +334,8 @@ public abstract class BeanDeserializerBase
 
         // probably adds a twist, so:
         _vanillaProcessing = false;
+
+        _externalTypeIdHandler = src._externalTypeIdHandler;
     }
 
     public BeanDeserializerBase(BeanDeserializerBase src, ObjectIdReader oir)
@@ -371,6 +375,8 @@ public abstract class BeanDeserializerBase
             _beanProperties = src._beanProperties.withProperty(idProp);
             _vanillaProcessing = false;
         }
+
+        _externalTypeIdHandler = src._externalTypeIdHandler;
     }
 
     /**
@@ -405,6 +411,8 @@ public abstract class BeanDeserializerBase
         // 01-May-2016, tatu: [databind#1217]: Remove properties from mapping,
         //    to avoid them being deserialized
         _beanProperties = src._beanProperties.withoutProperties(ignorableProps, includableProps);
+
+        _externalTypeIdHandler = src._externalTypeIdHandler;
     }
 
     /**
@@ -435,6 +443,8 @@ public abstract class BeanDeserializerBase
         _serializationShape = src._serializationShape;
 
         _vanillaProcessing = src._vanillaProcessing;
+
+        _externalTypeIdHandler = src._externalTypeIdHandler;
     }
 
     @Deprecated // since 2.12

f4005b6de48ee83b53ba0df7a891b4da4b9270a5
Kim, Joo Hyuk <beanskobe@gmail.com>
2023-11-11 17:16:11 +0900

    Update Reproduce4158Test.java

--- src/test/java/com/fasterxml/jackson/databind/Reproduce4158Test.java
+++ src/test/java/com/fasterxml/jackson/databind/Reproduce4158Test.java
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.failing;
+package com.fasterxml.jackson.databind;
 
 import static org.junit.Assert.assertEquals;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;

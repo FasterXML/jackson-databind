45c844a728939d5d36b363c0f3e6292e715af619
Kim, Joo Hyuk <beanskobe@gmail.com>
2023-11-13 16:36:53 +0900

    Add comment explaining things

--- src/main/java/com/fasterxml/jackson/databind/deser/impl/PropertyValueBuffer.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/impl/PropertyValueBuffer.java
@@ -210,6 +210,9 @@ public class PropertyValueBuffer
             }
 
             // Fourth: default value
+            // [databind#4201] One may argue that.... required validation should come only
+            // "after" absent value and default value are accessed.
+            // But let's keep things to work as before while addressing issue here.
             JsonDeserializer<Object> deser = prop.getValueDeserializer();
             return deser.getAbsentValue(_context);
         } catch (DatabindException e) {

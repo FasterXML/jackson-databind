1f9e0a1888ed45491eca1221d4aa1c923affee5b
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-11-27 17:51:00 -0800

    ...

--- src/test/java/com/fasterxml/jackson/databind/deser/enums/EnumSetPolymorphicDeser4214Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/enums/EnumSetPolymorphicDeser4214Test.java
@@ -12,38 +12,35 @@ import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 // For [databind#4214]
 public class EnumSetPolymorphicDeser4214Test extends BaseMapTest
 {
-	static enum MyEnum {
-		ITEM_A, ITEM_B;
-	}
+    static enum MyEnum {
+        ITEM_A, ITEM_B;
+    }
 
-	static class EnumSetHolder {
-		public Set<MyEnum> enumSet; // use Set instead of EnumSet for type of this
+    static class EnumSetHolder {
+        public Set<MyEnum> enumSet; // use Set instead of EnumSet for type of this
 
-		@Override
-		public boolean equals(Object o) {
-			if (!(o instanceof EnumSetHolder)) {
-				return false;
-			}
-			EnumSetHolder eh = (EnumSetHolder) o;
-			return Objects.equals(enumSet, eh.enumSet);
-		}
-	}
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof EnumSetHolder)) {
+                return false;
+            }
+            EnumSetHolder eh = (EnumSetHolder) o;
+            return Objects.equals(enumSet, eh.enumSet);
+        }
+    }
 
-	public void testSerialization() throws Exception
-	{
-		ObjectMapper mapper = jsonMapperBuilder()
-				// this type information is needed in json string, for this test
-				.activateDefaultTyping(BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
-						DefaultTyping.NON_FINAL_AND_ENUMS)
-				.build();
+    public void testPolymorphicDeserialization4214() throws Exception
+    {
+        // Need to use Default Typing to trigger issue
+        ObjectMapper mapper = jsonMapperBuilder()
+                .activateDefaultTyping(BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
+                        DefaultTyping.NON_FINAL_AND_ENUMS)
+                .build();
 
-		EnumSetHolder enumSetHolder = new EnumSetHolder();
-		enumSetHolder.enumSet = EnumSet.allOf(MyEnum.class);
-		String json = mapper.writeValueAsString(enumSetHolder);
-
-		//System.err.println("JSON = \n"+json);
-
-		EnumSetHolder result = mapper.readValue(json, EnumSetHolder.class);
-		assertEquals(result, enumSetHolder);
-	}
+        EnumSetHolder enumSetHolder = new EnumSetHolder();
+        enumSetHolder.enumSet = EnumSet.allOf(MyEnum.class);
+        String json = mapper.writeValueAsString(enumSetHolder);
+        EnumSetHolder result = mapper.readValue(json, EnumSetHolder.class);
+        assertEquals(result, enumSetHolder);
+    }
 }

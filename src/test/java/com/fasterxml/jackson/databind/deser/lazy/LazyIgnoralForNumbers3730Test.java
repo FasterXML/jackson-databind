ce9c629bef5631cf5bed6e8750df35ba72596186
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-12-09 16:52:16 -0800

    Add pre-validation for parsing "stringified" Floating-Point numbers, missing length checks (#4253)

--- src/test/java/com/fasterxml/jackson/databind/deser/lazy/LazyIgnoralForNumbers3730Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/lazy/LazyIgnoralForNumbers3730Test.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -27,6 +28,7 @@ import java.math.BigDecimal;
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(fullyQualifiedNames = "com.fasterxml.jackson.core.io.NumberInput")
+@Ignore("Powermock stopped working with NumberUtil refactoring see [databind#4250]")
 public class LazyIgnoralForNumbers3730Test extends BaseMapTest
 {
     static class ExtractFieldsNoDefaultConstructor3730 {

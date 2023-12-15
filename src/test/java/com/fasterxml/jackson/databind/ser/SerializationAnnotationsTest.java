909ff8412bbc863627e06f7740defbc1d00cc503
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-12-14 16:51:37 -0800

    Test refactoring

--- src/test/java/com/fasterxml/jackson/databind/ser/SerializationAnnotationsTest.java
+++ src/test/java/com/fasterxml/jackson/databind/ser/SerializationAnnotationsTest.java
@@ -3,7 +3,6 @@ package com.fasterxml.jackson.databind.ser;
 import java.io.*;
 import java.util.*;
 
-
 import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.core.*;
 
@@ -11,19 +10,13 @@ import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 /**
- * This unit test suite tests use of Annotations for
+ * This unit test suite tests use of some basic Jackson annotations for
  * bean serialization.
  */
 public class SerializationAnnotationsTest
     extends BaseMapTest
 {
-    /*
-    /**********************************************************
-    /* Helper classes
-    /**********************************************************
-     */
-
-    /// Class for testing {@link JsonProperty} annotations with getters
+    // Class for testing {@link JsonProperty} annotations with getters
     final static class SizeClassGetter
     {
         @JsonProperty public int size() { return 3; }
@@ -48,7 +41,6 @@ public class SerializationAnnotationsTest
         // Should be considered property "y" even tho non-public
         @JsonSerialize protected int getY() { return 8; }
     }
-
 
     /**
      * Class for testing {@link JsonSerializer} annotation
@@ -97,36 +89,6 @@ public class SerializationAnnotationsTest
 
     static class SubClassBean extends BaseBean {
         public int getZ() { return 3; }
-    }
-
-    // For [JACKSON-666] ("SerializationFeature of the Beast!")
-    @JsonPropertyOrder(alphabetic=true)
-    static class GettersWithoutSetters
-    {
-        public int d = 0;
-
-        @JsonCreator
-        public GettersWithoutSetters(@JsonProperty("a") int a) { }
-
-        // included, since there is a constructor property
-        public int getA() { return 3; }
-
-        // not included, as there's nothing matching
-        public int getB() { return 4; }
-
-        // include as there is setter
-        public int getC() { return 5; }
-        public void setC(int v) { }
-
-        // and included, as there is a field
-        public int getD() { return 6; }
-    }
-
-    // [JACKSON-806]: override 'need-setter' with explicit annotation
-    static class GettersWithoutSetters2
-    {
-        @JsonProperty
-        public int getA() { return 123; }
     }
 
     /*
@@ -234,28 +196,4 @@ public class SerializationAnnotationsTest
         assertEquals("{\"x\":8}", json);
     }
 
-    public void testGettersWithoutSetters() throws Exception
-    {
-        ObjectMapper m = new ObjectMapper();
-        GettersWithoutSetters bean = new GettersWithoutSetters(123);
-        assertFalse(m.isEnabled(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS));
-
-        // by default, all 4 found:
-        assertEquals("{\"a\":3,\"b\":4,\"c\":5,\"d\":6}", m.writeValueAsString(bean));
-
-        // but 3 if we require mutator:
-        m = jsonMapperBuilder()
-                .enable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS)
-                .build();
-        assertEquals("{\"a\":3,\"c\":5,\"d\":6}", m.writeValueAsString(bean));
-    }
-
-    public void testGettersWithoutSettersOverride() throws Exception
-    {
-        GettersWithoutSetters2 bean = new GettersWithoutSetters2();
-        ObjectMapper m = jsonMapperBuilder()
-                .enable(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS)
-                .build();
-        assertEquals("{\"a\":123}", m.writeValueAsString(bean));
-    }
 }

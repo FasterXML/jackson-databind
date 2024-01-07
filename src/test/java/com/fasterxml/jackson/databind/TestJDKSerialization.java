617f0ba287323237e0024dd4b9aaa80f10f18598
Kim, Joo Hyuk <beanskobe@gmail.com>
2024-01-07 12:41:33 +0900

    Merge test into existing one.

--- src/test/java/com/fasterxml/jackson/databind/TestJDKSerialization.java
+++ src/test/java/com/fasterxml/jackson/databind/TestJDKSerialization.java
@@ -3,9 +3,8 @@ package com.fasterxml.jackson.databind;
 import java.io.*;
 import java.util.*;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.*;
+import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
@@ -58,6 +57,37 @@ public class TestJDKSerialization extends BaseMapTest
             return _map;
         }
     }
+
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+    @JsonSubTypes({@JsonSubTypes.Type(value = FooClassImpl.class)})
+    public class FooClass { }
+    class FooClassImpl extends FooClass { }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+    @JsonSubTypes({@JsonSubTypes.Type(value = FooDeductionImpl.class)})
+    public class FooDeduction { }
+    class FooDeductionImpl extends FooDeduction { }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+    @JsonSubTypes({@JsonSubTypes.Type(value = FooNoneImpl.class)})
+    public class FooNone { }
+    class FooNoneImpl extends FooNone { }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM)
+    @JsonSubTypes({@JsonSubTypes.Type(value = FooCustomImpl.class)})
+    public class FooCustom { }
+    class FooCustomImpl extends FooCustom { }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS)
+    @JsonSubTypes({@JsonSubTypes.Type(value = FooMinimalClassImpl.class)})
+    public class FooMinimalClass { }
+    class FooMinimalClassImpl extends FooMinimalClass { }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+    @JsonSubTypes({@JsonSubTypes.Type(value = FooNameImpl.class)})
+    public class FooName { }
+    class FooNameImpl extends FooName { }
 
     /*
     /**********************************************************
@@ -190,5 +220,29 @@ public class TestJDKSerialization extends BaseMapTest
         assertNotNull(result);
         t = orig.constructType(JavaType.class);
         assertEquals(JavaType.class, t.getRawClass());
+    }
+
+    // [databind#4303]
+    public void testObjectReaderSerializationWithPolymorphism()
+        throws Exception
+    {
+        Class<?>[] classes = new Class<?>[] {
+            FooClass.class,
+            FooDeduction.class,
+            FooNone.class,
+            FooCustom.class,
+            FooMinimalClass.class,
+            FooName.class
+        };
+
+        for (Class<?> clazz : classes) {
+            ObjectReader reader = newJsonMapper()
+                .readerFor(clazz);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(reader); // This line should throw NotSerializableException
+            oos.close();
+        }
     }
 }

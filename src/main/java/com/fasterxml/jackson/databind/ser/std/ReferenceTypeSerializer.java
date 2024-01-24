a4c0a59f60eda35466558efe1fa6ca2f940291ee
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-23 19:24:13 -0800

    Second half of #4337: deserializers

--- src/main/java/com/fasterxml/jackson/databind/ser/std/ReferenceTypeSerializer.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/std/ReferenceTypeSerializer.java
@@ -216,7 +216,7 @@ public abstract class ReferenceTypeSerializer<T>
                 ser = provider.handlePrimaryContextualization(ser, property);
             }
         }
-        // 23-Jan-2024, tatu: May have a content converter:
+        // 23-Jan-2024, tatu: [databind#4337]: May have a content converter
         ser = findContextualConvertingSerializer(provider, property, ser);
 
         // First, resolve wrt property, resolved serializers

b3947a3157e52bb0fcb79f5492a9d580fac97d38
Kim, Joo Hyuk <beanskobe@gmail.com>
2024-03-03 15:59:12 +0900

    Add more comment

--- src/main/java/com/fasterxml/jackson/databind/ser/BeanSerializerFactory.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/BeanSerializerFactory.java
@@ -454,6 +454,7 @@ public class BeanSerializerFactory
             BeanProperty.Std anyProp = new BeanProperty.Std(name, valueType, null,
                     anyGetter, PropertyMetadata.STD_OPTIONAL);
 
+            // Check if there is a accessor exposed for the anyGetter
             BeanPropertyWriter anyGetterProp = null;
             int anyGetterIndex = -1;
             for (int i = 0; i < props.size(); i++) {
@@ -469,11 +470,12 @@ public class BeanSerializerFactory
                 }
             }
             if (anyGetterIndex != -1) {
-                // Prop is already in place, just need to replace it
+                // There is prop is already in place, just need to replace it
                 AnyGetterWriter anyGetterWriter = new AnyGetterWriter(anyGetterProp, anyProp, anyGetter, anySer);
                 props.set(anyGetterIndex, anyGetterWriter);
             } else {
-                // Otherwise add it at the end, but won't be sorted...
+                // Otherwise just add it at the end, but won't be sorted...
+                // This is case where JsonAnyGetter is private/protected,
                 BeanPropertyDefinition anyGetterPropDef = SimpleBeanPropertyDefinition.construct(config, anyGetter, name);
                 BeanPropertyWriter anyPropWriter = _constructWriter(prov, anyGetterPropDef, new PropertyBuilder(config, beanDesc), staticTyping, anyGetter);
                 props.add(new AnyGetterWriter(anyPropWriter, anyProp, anyGetter, anySer));

c00590a0e91d7284a3823ce1410862672d0f2d0e
Kim, Joo Hyuk <beanskobe@gmail.com>
2024-04-04 23:13:41 +0900

    Update PropertyBuilder.java

--- src/main/java/com/fasterxml/jackson/databind/ser/PropertyBuilder.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/PropertyBuilder.java
@@ -182,7 +182,7 @@ public class PropertyBuilder
             }
             if (valueToSuppress == null) {
                 suppressNulls = true;
-                // [databind#4464] Since 2.15, NON_DEFAULT should be extension of NON_EMPTY
+                // [databind#4464] Since 2.15, NON_DEFAULT does not work with NON_EMPTY for custom serializer
                 valueToSuppress = BeanPropertyWriter.MARKER_FOR_EMPTY;
             } else {
                 if (valueToSuppress.getClass().isArray()) {

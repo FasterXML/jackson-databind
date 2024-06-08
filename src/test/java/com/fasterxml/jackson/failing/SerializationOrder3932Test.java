bf44874dd9e0d78ce2fddf21453436b1203b408b
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-06-08 09:28:16 -0700

    ...

--- src/test/java/com/fasterxml/jackson/failing/SerializationOrder3932Test.java
+++ src/test/java/com/fasterxml/jackson/failing/SerializationOrder3932Test.java
@@ -1,9 +1,10 @@
 package com.fasterxml.jackson.failing;
 
+import org.junit.jupiter.api.Test;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 

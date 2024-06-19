b7e56292611fe5c28175b7e25ee792bc3a5209b9
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-06-18 22:46:42 -0700

    Fix some tests

--- src/test-jdk17/java/tools/jackson/databind/records/RecordSerializationOrderTest.java
+++ src/test-jdk17/java/tools/jackson/databind/records/RecordSerializationOrderTest.java
@@ -52,7 +52,7 @@ public class RecordSerializationOrderTest extends DatabindTestUtil
         NestedRecordOneWithJsonProperty nestedRecordOne =
                 new NestedRecordOneWithJsonProperty("1", "test@records.com", nestedRecordTwo);
         final String output = MAPPER.writeValueAsString(nestedRecordOne);
-        final String expected = "{\"email\":\"test@records.com\",\"id\":\"1\",\"nestedProperty\":{\"id\":\"2\",\"passport\":\"111110\"}}";
+        final String expected = a2q("{'email':'test@records.com','id':'1','nestedProperty':{'id':'2','passport':'111110'}}");
         assertEquals(expected, output);
     }
 
--- src/test/java/tools/jackson/databind/ser/SerializationOrderTest.java
+++ src/test/java/tools/jackson/databind/ser/SerializationOrderTest.java
@@ -22,7 +22,8 @@ public class SerializationOrderTest
         public int b;
         public int c;
 
-        @JsonCreator public BeanWithCreator(@JsonProperty("c") int c, @JsonProperty("a") int a) {
+        @JsonCreator
+        public BeanWithCreator(@JsonProperty("c") int c, @JsonProperty("a") int a) {
             this.a = a;
             this.c = c;
         }
@@ -150,7 +151,7 @@ public class SerializationOrderTest
 
     @Test
     public void testImplicitOrderByCreator() throws Exception {
-        assertEquals("{\"a\":2,\"c\":1,\"b\":0}",
+        assertEquals("{\"c\":1,\"a\":2,\"b\":0}",
                 MAPPER.writeValueAsString(new BeanWithCreator(1, 2)));
     }
 
@@ -233,7 +234,7 @@ public class SerializationOrderTest
         // BUT are sorted within their own category
         assertTrue(ALPHA_MAPPER.isEnabled(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY));
         assertTrue(ALPHA_MAPPER.isEnabled(MapperFeature.SORT_CREATOR_PROPERTIES_FIRST));
-        assertEquals(a2q("{'a':3,'c':2,'b':0}"),
+        assertEquals(a2q("{'c':2,'a':3,'b':0}"),
                 ALPHA_MAPPER.writeValueAsString(new BeanForStrictOrdering(2, 3)));
 
         // but can change that

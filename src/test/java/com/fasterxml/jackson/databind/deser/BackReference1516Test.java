c3419c5f6700982658c3f907ded3bcdb2c127a7f
PJ Fanning <pjfanning@users.noreply.github.com>
2024-07-25 19:12:06 +0100

    enable backref test

--- src/test/java/com/fasterxml/jackson/databind/deser/BackReference1516Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/BackReference1516Test.java
@@ -1,43 +1,34 @@
-package com.fasterxml.jackson.failing;
-
-import java.beans.ConstructorProperties;
-
-import org.junit.jupiter.api.Test;
+package com.fasterxml.jackson.databind.deser;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
+import org.junit.jupiter.api.Test;
+
+import java.beans.ConstructorProperties;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 class BackReference1516Test extends DatabindTestUtil {
-    static class ParentWithCreator {
-        String id, name;
+
+    static class ParentWithoutCreator {
+        public String id, name;
 
         @JsonManagedReference
-        ChildObject1 child;
-
-        @ConstructorProperties({"id", "name", "child"})
-        public ParentWithCreator(String id, String name,
-                                 ChildObject1 child) {
-            this.id = id;
-            this.name = name;
-            this.child = child;
-        }
+        public ChildObject2 child;
     }
 
-    static class ChildObject1 {
+    static class ChildObject2 {
         public String id, name;
 
         @JsonBackReference
-        public ParentWithCreator parent;
+        public ParentWithoutCreator parent;
 
         @ConstructorProperties({"id", "name", "parent"})
-        public ChildObject1(String id, String name,
-                            ParentWithCreator parent) {
+        public ChildObject2(String id, String name,
+                            ParentWithoutCreator parent) {
             this.id = id;
             this.name = name;
             this.parent = parent;
@@ -53,9 +44,9 @@ class BackReference1516Test extends DatabindTestUtil {
                     "}");
 
     @Test
-    void withParentCreator() throws Exception {
-        ParentWithCreator result = MAPPER.readValue(PARENT_CHILD_JSON,
-                ParentWithCreator.class);
+    void withParentNoCreator() throws Exception {
+        ParentWithoutCreator result = MAPPER.readValue(PARENT_CHILD_JSON,
+                ParentWithoutCreator.class);
         assertNotNull(result);
         assertNotNull(result.child);
         assertSame(result, result.child.parent);

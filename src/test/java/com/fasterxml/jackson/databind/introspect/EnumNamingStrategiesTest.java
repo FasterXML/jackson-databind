3862a3de65d038b63809aebc24b0eda84c8a157f
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-08-15 17:23:15 -0700

    Revert "Add `UpperCamelCase` `EnumNamingStrategy` (#4660)" (#4665)
    This reverts commit 5bf17d921ae0cd3523e378ffe57e2b83b5aa08b5.

--- src/test/java/com/fasterxml/jackson/databind/introspect/EnumNamingStrategiesTest.java
+++ src/test/java/com/fasterxml/jackson/databind/introspect/EnumNamingStrategiesTest.java
@@ -1,14 +1,13 @@
 package com.fasterxml.jackson.databind.introspect;
 
-import com.fasterxml.jackson.databind.EnumNamingStrategy;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.EnumNamingStrategies;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
-import static com.fasterxml.jackson.databind.EnumNamingStrategies.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -18,126 +17,120 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  *
  * @since 2.15
  */
-class EnumNamingStrategiesTest extends DatabindTestUtil {
+public class EnumNamingStrategiesTest extends DatabindTestUtil {
+
     /**
-     * Test cases for {@link com.fasterxml.jackson.databind.EnumNamingStrategies}.
+     * Test casess for {@link com.fasterxml.jackson.databind.EnumNamingStrategies.CamelCaseStrategy}.
      *
-     * @since 2.19
+     * <p>
+     * Each <code>Object[]</code> element is composed of <code>{input, expectedOutput}</code>.
+     *
+     * @since 2.15
      */
-    private static Stream<Arguments> enumNameConversionTestCases() {
-        return Stream.of(
-                // Empty values
-                Arguments.of(LOWER_CAMEL_CASE, null, null),
-                Arguments.of(UPPER_CAMEL_CASE, null, null),
-                Arguments.of(SNAKE_CASE, null, null),
-                Arguments.of(UPPER_SNAKE_CASE, null, null),
-                Arguments.of(LOWER_CASE, null, null),
-                Arguments.of(KEBAB_CASE, null, null),
-                Arguments.of(LOWER_DOT_CASE, null, null),
-                Arguments.of(LOWER_CAMEL_CASE, "", ""),
+    final static List<String[]> CAMEL_CASE_NAME_TRANSLATIONS = Arrays.asList(new String[][]{
+            // Empty values
+            {null, null},
+            {"", ""},
 
-                // input values with no underscores
-                Arguments.of(LOWER_CAMEL_CASE, "a", "a"),
-                Arguments.of(LOWER_CAMEL_CASE, "abc", "abc"),
-                Arguments.of(LOWER_CAMEL_CASE, "A", "a"),
-                Arguments.of(LOWER_CAMEL_CASE, "A1", "a1"),
-                Arguments.of(LOWER_CAMEL_CASE, "1A", "1a"),
-                Arguments.of(LOWER_CAMEL_CASE, "ABC", "abc"),
-                Arguments.of(LOWER_CAMEL_CASE, "User", "user"),
-                Arguments.of(LOWER_CAMEL_CASE, "Results", "results"),
-                Arguments.of(LOWER_CAMEL_CASE, "WWW", "www"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER", "user"),
-                Arguments.of(LOWER_CAMEL_CASE, "userName", "username"),
-                Arguments.of(LOWER_CAMEL_CASE, "someURI", "someuri"),
-                Arguments.of(LOWER_CAMEL_CASE, "someURIs", "someuris"),
-                Arguments.of(LOWER_CAMEL_CASE, "theWWW", "thewww"),
-                Arguments.of(LOWER_CAMEL_CASE, "uId", "uid"),
-                Arguments.of(LOWER_CAMEL_CASE, "usId", "usid"),
-                Arguments.of(LOWER_CAMEL_CASE, "UserName", "username"),
-                Arguments.of(KEBAB_CASE, "UserName", "username"),
-                Arguments.of(LOWER_CAMEL_CASE, "user", "user"),
-                Arguments.of(LOWER_CAMEL_CASE, "xCoordinate", "xcoordinate"),
+            // input values with no underscores
+            {"a", "a"},
+            {"abc", "abc"},
+            {"A", "a"},
+            {"A1", "a1"},
+            {"1A", "1a"},
+            {"ABC", "abc"},
+            {"User", "user"},
+            {"Results", "results"},
+            {"WWW", "www"},
+            {"USER", "user"},
+            {"userName", "username"},
+            {"someURI", "someuri"},
+            {"someURIs", "someuris"},
+            {"theWWW", "thewww"},
+            {"uId", "uid"},
+            {"usId", "usid"},
+            {"UserName", "username"},
+            {"user", "user"},
+            {"xCoordinate", "xcoordinate"},
 
-                // input values with single underscores
-                Arguments.of(LOWER_CAMEL_CASE, "a_", "a"),
-                Arguments.of(LOWER_CAMEL_CASE, "_A", "A"),
-                Arguments.of(LOWER_CAMEL_CASE, "_a", "A"),
-                Arguments.of(LOWER_CAMEL_CASE, "a_A", "aA"),
-                Arguments.of(LOWER_CAMEL_CASE, "a_a", "aA"),
-                Arguments.of(LOWER_CAMEL_CASE, "A_A", "aA"),
-                Arguments.of(LOWER_CAMEL_CASE, "A_a", "aA"),
-                Arguments.of(LOWER_CAMEL_CASE, "BARS_", "bars"),
-                Arguments.of(LOWER_CAMEL_CASE, "BARS", "bars"),
-                Arguments.of(LOWER_CAMEL_CASE, "THE_WWW", "theWww"),
-                Arguments.of(LOWER_CAMEL_CASE, "U_ID", "uId"),
-                Arguments.of(LOWER_CAMEL_CASE, "US_ID", "usId"),
-                Arguments.of(LOWER_CAMEL_CASE, "X_COORDINATE", "xCoordinate"),
+            // input values with single underscores
+            {"a_", "a"},
+            {"_A", "A"},
+            {"_a", "A"},
+            {"a_A", "aA"},
+            {"a_a", "aA"},
+            {"A_A", "aA"},
+            {"A_a", "aA"},
+            {"BARS_", "bars"},
+            {"BARS", "bars"},
+            {"THE_WWW", "theWww"},
+            {"U_ID", "uId"},
+            {"US_ID", "usId"},
+            {"X_COORDINATE", "xCoordinate"},
 
-                // heavy "username" example
-                Arguments.of(LOWER_CAMEL_CASE, "USERNAME_", "username"),
-                Arguments.of(LOWER_CAMEL_CASE, "_User_Name", "UserName"),
-                Arguments.of(LOWER_CAMEL_CASE, "_UserName", "Username"),
-                Arguments.of(LOWER_CAMEL_CASE, "_Username", "Username"),
-                Arguments.of(LOWER_CAMEL_CASE, "_user_name", "UserName"),
-                Arguments.of(LOWER_CAMEL_CASE, "_USERNAME", "Username"),
-                Arguments.of(LOWER_CAMEL_CASE, "__USERNAME", "Username"),
-                Arguments.of(LOWER_CAMEL_CASE, "__Username", "Username"),
-                Arguments.of(LOWER_CAMEL_CASE, "__username", "Username"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER______NAME", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER_NAME", "userName"),
-                Arguments.of(UPPER_CAMEL_CASE, "USER_NAME", "UserName"),
-                Arguments.of(SNAKE_CASE, "USER_NAME", "user_name"),
-                Arguments.of(UPPER_SNAKE_CASE, "USER_NAME", "USER_NAME"),
-                Arguments.of(LOWER_CASE, "USER_NAME", "username"),
-                Arguments.of(KEBAB_CASE, "USER_NAME", "user-name"),
-                Arguments.of(LOWER_DOT_CASE, "USER_NAME", "user.name"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER__NAME", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER_NAME_", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "User__Name", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER_NAME_S", "userNameS"),
-                Arguments.of(LOWER_CAMEL_CASE, "_user_name_s", "UserNameS"),
-                Arguments.of(LOWER_CAMEL_CASE, "USER_NAME_S", "userNameS"),
-                Arguments.of(LOWER_CAMEL_CASE, "user__name", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "user_name", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "USERNAME", "username"),
-                Arguments.of(LOWER_CAMEL_CASE, "username", "username"),
-                Arguments.of(LOWER_CAMEL_CASE, "User_Name", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "User_Name_", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "User_Name_", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "User_Name__", "userName"),
-                Arguments.of(UPPER_SNAKE_CASE, "User_Name__", "USER_NAME"),
-                Arguments.of(LOWER_CAMEL_CASE, "user_name_", "userName"),
-                Arguments.of(LOWER_CAMEL_CASE, "user_name__", "userName"),
+            // heavy "username" example
+            {"USERNAME_", "username"},
+            {"_User_Name", "UserName"},
+            {"_UserName", "Username"},
+            {"_Username", "Username"},
+            {"_user_name", "UserName"},
+            {"_USERNAME", "Username"},
+            {"__USERNAME", "Username"},
+            {"__Username", "Username"},
+            {"__username", "Username"},
+            {"USER______NAME", "userName"},
+            {"USER_NAME", "userName"},
+            {"USER__NAME", "userName"},
+            {"USER_NAME_", "userName"},
+            {"User__Name", "userName"},
+            {"USER_NAME_S", "userNameS"},
+            {"_user_name_s", "UserNameS"},
+            {"USER_NAME_S", "userNameS"},
+            {"user__name", "userName"},
+            {"user_name", "userName"},
+            {"USERNAME", "username"},
+            {"username", "username"},
+            {"User_Name", "userName"},
+            {"User_Name_", "userName"},
+            {"User_Name_", "userName"},
+            {"User_Name__", "userName"},
+            {"user_name_", "userName"},
+            {"user_name__", "userName"},
 
-                // additional variations
-                Arguments.of(LOWER_CAMEL_CASE, "a$a", "a$a"),
-                Arguments.of(LOWER_CAMEL_CASE, "A$A", "a$a"),
-                Arguments.of(LOWER_CAMEL_CASE, "a_$", "a$"),
-                Arguments.of(LOWER_CAMEL_CASE, "a$", "a$"),
-                Arguments.of(LOWER_CAMEL_CASE, "a1", "a1"),
-                Arguments.of(LOWER_CAMEL_CASE, "$", "$"),
-                Arguments.of(LOWER_CAMEL_CASE, "A$", "a$"),
-                Arguments.of(LOWER_CAMEL_CASE, "1", "1"),
-                Arguments.of(LOWER_CAMEL_CASE, "$_A", "$A"),
-                Arguments.of(LOWER_CAMEL_CASE, "$_a", "$A"),
-                Arguments.of(LOWER_CAMEL_CASE, "1_A", "1A"),
-                Arguments.of(LOWER_CAMEL_CASE, "1a", "1a"),
-                Arguments.of(LOWER_CAMEL_CASE, "A_$", "a$"),
-                Arguments.of(LOWER_CAMEL_CASE, "_123_41", "12341")
-        );
-    }
+            // additional variations
+            {"a$a", "a$a"},
+            {"A$A", "a$a"},
+            {"a_$", "a$"},
+            {"a$", "a$"},
+            {"a1", "a1"},
+            {"$", "$"},
+            {"A$", "a$"},
+            {"1", "1"},
+            {"$_A", "$A"},
+            {"$_a", "$A"},
+            {"1_A", "1A"},
+            {"1a", "1a"},
+            {"A_$", "a$"},
+            {"_123_41", "12341"},
+    });
 
     /**
-     * Unit test to verify the implementations of
-     * {@link com.fasterxml.jackson.databind.EnumNamingStrategy#convertEnumToExternalName(String)}
+     * Unit test to verify the implementation of
+     * {@link com.fasterxml.jackson.databind.EnumNamingStrategies.CamelCaseStrategy#convertEnumToExternalName(String)}
      * without the context of an ObjectMapper.
      *
-     * @since 2.19
+     * @since 2.15
      */
-    @ParameterizedTest
-    @MethodSource("enumNameConversionTestCases")
-    void testEnumNameConversions(EnumNamingStrategy strategy, String input, String output) {
-        String actual = strategy.convertEnumToExternalName(input);
-        assertEquals(output, actual);
+    @Test
+    public void testCamelCaseStrategyStandAlone() {
+        for (String[] pair : CAMEL_CASE_NAME_TRANSLATIONS) {
+            final String input = pair[0];
+            final String expected = pair[1];
+
+            String actual = EnumNamingStrategies.CamelCaseStrategy.INSTANCE
+                    .convertEnumToExternalName(input);
+
+            assertEquals(expected, actual);
+        }
     }
 }

f777d1ab5c80030137c8efd8319bd399a38b3d65
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-09-20 14:16:11 -0700

    Minor javadoc change

--- src/main/java/com/fasterxml/jackson/databind/util/JacksonCollectors.java
+++ src/main/java/com/fasterxml/jackson/databind/util/JacksonCollectors.java
@@ -1,10 +1,11 @@
 package com.fasterxml.jackson.databind.util;
 
+import java.util.stream.Collector;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeCreator;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import java.util.stream.Collector;
 
 /**
  * Utility class that provides custom {@link Collector} implementations to support Stream operations.
@@ -21,6 +22,9 @@ public abstract class JacksonCollectors {
      * This method uses a {@link JsonNodeFactory} to create an empty {@link ArrayNode} and then adds each
      * {@link JsonNode} to it.
      * </p>
+     *<p>
+     * Short-cut to
+     *{@code toArrayNode(JsonNodeFactory.instance}}
      *
      * @return a {@link Collector} that collects {@link JsonNode} elements into an {@link ArrayNode}
      */
@@ -28,6 +32,17 @@ public abstract class JacksonCollectors {
         return toArrayNode(JsonNodeFactory.instance);
     }
 
+    /**
+     * Creates a {@link Collector} that collects {@link JsonNode} elements into an {@link ArrayNode}.
+     * <p>
+     * This method uses a {@link JsonNodeFactory} to create an empty {@link ArrayNode} and then adds each
+     * {@link JsonNode} to it.
+     * </p>
+     *
+     * @param nodeCreator Factory for constructing {@link ArrayNode} to contain nodes in
+     *
+     * @return a {@link Collector} that collects {@link JsonNode} elements into an {@link ArrayNode}
+     */
     public static Collector<JsonNode, ArrayNode, ArrayNode> toArrayNode(JsonNodeCreator nodeCreator) {
         return Collector.of(
                 nodeCreator::arrayNode, // supplier

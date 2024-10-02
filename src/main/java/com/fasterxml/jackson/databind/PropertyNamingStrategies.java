953ee8981fbbda98350763b178c75a7d108fd7cb
larsbenedetto <lbenedetto@toogoodtogo.com>
2024-10-01 17:22:51 -0700

    Add more `EnumNamingStrategies`

--- src/main/java/com/fasterxml/jackson/databind/PropertyNamingStrategies.java
+++ src/main/java/com/fasterxml/jackson/databind/PropertyNamingStrategies.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.fasterxml.jackson.databind.introspect.AnnotatedField;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
 import com.fasterxml.jackson.databind.introspect.AnnotatedParameter;
+import com.fasterxml.jackson.databind.util.NamingStrategy;
 
 /**
  * Container for standard {@link PropertyNamingStrategy} implementations
@@ -124,42 +125,6 @@ public abstract class PropertyNamingStrategies
         }
 
         public abstract String translate(String propertyName);
-
-        /**
-         * Helper method to share implementation between snake and dotted case.
-         */
-        protected String translateLowerCaseWithSeparator(final String input, final char separator)
-        {
-            if (input == null || input.isEmpty()) {
-                return input;
-            }
-
-            final int length = input.length();
-            final StringBuilder result = new StringBuilder(length + (length >> 1));
-            int upperCount = 0;
-            for (int i = 0; i < length; ++i) {
-                char ch = input.charAt(i);
-                char lc = Character.toLowerCase(ch);
-
-                if (lc == ch) { // lower-case letter means we can get new word
-                    // but need to check for multi-letter upper-case (acronym), where assumption
-                    // is that the last upper-case char is start of a new word
-                    if (upperCount > 1) {
-                        // so insert hyphen before the last character now
-                        result.insert(result.length() - 1, separator);
-                    }
-                    upperCount = 0;
-                } else {
-                    // Otherwise starts new word, unless beginning of string
-                    if ((upperCount == 0) && (i > 0)) {
-                        result.append(separator);
-                    }
-                    ++upperCount;
-                }
-                result.append(lc);
-            }
-            return result.toString();
-        }
     }
 
     /*
@@ -230,35 +195,7 @@ public abstract class PropertyNamingStrategies
         @Override
         public String translate(String input)
         {
-            if (input == null) return input; // garbage in, garbage out
-            int length = input.length();
-            StringBuilder result = new StringBuilder(length * 2);
-            int resultLength = 0;
-            boolean wasPrevTranslated = false;
-            for (int i = 0; i < length; i++)
-            {
-                char c = input.charAt(i);
-                if (i > 0 || c != '_') // skip first starting underscore
-                {
-                    if (Character.isUpperCase(c))
-                    {
-                        if (!wasPrevTranslated && resultLength > 0 && result.charAt(resultLength - 1) != '_')
-                        {
-                            result.append('_');
-                            resultLength++;
-                        }
-                        c = Character.toLowerCase(c);
-                        wasPrevTranslated = true;
-                    }
-                    else
-                    {
-                        wasPrevTranslated = false;
-                    }
-                    result.append(c);
-                    resultLength++;
-                }
-            }
-            return resultLength > 0 ? result.toString() : input;
+            return NamingStrategy.SNAKE_CASE.translate(input);
         }
     }
 
@@ -281,11 +218,7 @@ public abstract class PropertyNamingStrategies
 
         @Override
         public String translate(String input) {
-            String output = super.translate(input);
-            if (output == null) {
-                return null;
-            }
-            return output.toUpperCase();
+            return NamingStrategy.UPPER_SNAKE_CASE.translate(input);
         }
     }
 
@@ -305,7 +238,7 @@ public abstract class PropertyNamingStrategies
 
         @Override
         public String translate(String input) {
-            return input;
+            return NamingStrategy.LOWER_CAMEL_CASE.translate(input);
         }
     }
 
@@ -343,18 +276,7 @@ public abstract class PropertyNamingStrategies
          */
         @Override
         public String translate(String input) {
-            if (input == null || input.isEmpty()){
-                return input; // garbage in, garbage out
-            }
-            // Replace first lower-case letter with upper-case equivalent
-            char c = input.charAt(0);
-            char uc = Character.toUpperCase(c);
-            if (c == uc) {
-                return input;
-            }
-            StringBuilder sb = new StringBuilder(input);
-            sb.setCharAt(0, uc);
-            return sb.toString();
+            return NamingStrategy.UPPER_CAMEL_CASE.translate(input);
         }
     }
 
@@ -376,10 +298,7 @@ public abstract class PropertyNamingStrategies
 
         @Override
         public String translate(String input) {
-            if (input == null || input.isEmpty()) {
-                return input;
-            }
-            return input.toLowerCase();
+            return NamingStrategy.LOWER_CASE.translate(input);
         }
     }
 
@@ -401,7 +320,7 @@ public abstract class PropertyNamingStrategies
 
         @Override
         public String translate(String input) {
-            return translateLowerCaseWithSeparator(input, '-');
+            return NamingStrategy.KEBAB_CASE.translate(input);
         }
     }
 
@@ -422,7 +341,7 @@ public abstract class PropertyNamingStrategies
 
         @Override
         public String translate(String input){
-            return translateLowerCaseWithSeparator(input, '.');
+            return NamingStrategy.LOWER_DOT_CASE.translate(input);
         }
     }
 }

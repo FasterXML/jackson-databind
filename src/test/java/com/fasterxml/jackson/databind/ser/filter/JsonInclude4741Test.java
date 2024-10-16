abb659a5daff21de3d49b150be36240f5345c0f3
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-10-15 17:24:13 -0700

    Add release notes, simplify test a bit

--- src/test/java/com/fasterxml/jackson/databind/ser/filter/JsonInclude4741Test.java
+++ src/test/java/com/fasterxml/jackson/databind/ser/filter/JsonInclude4741Test.java
@@ -1,44 +1,34 @@
 package com.fasterxml.jackson.databind.ser.filter;
 
+import org.junit.jupiter.api.Test;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
-
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonInclude4741Test
-        extends DatabindTestUtil
+    extends DatabindTestUtil
 {
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    public static class MyString {
+        public String value = null;
+    }
 
     private ObjectMapper MAPPER = newJsonMapper();
 
     @Test
-    void testSerialization() throws JsonProcessingException
+    void testSerialization() throws Exception
     {
         MyString input = new MyString();
-        input.setValue("");
+        input.value = "";
 
         String json = MAPPER.writeValueAsString(input);
+        assertEquals(a2q("{'value':''}"), json);
+
         MyString output = MAPPER.readValue(json, MyString.class);
 
-        assertEquals(input.getValue(), output.getValue());
-    }
-
-    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
-    public static class MyString {
-        private String value = null;
-
-        // Getter
-        public String getValue() {
-            return value;
-        }
-
-        // Setter
-        public void setValue(String value) {
-            this.value = value;
-        }
+        assertEquals(input.value, output.value);
     }
 }

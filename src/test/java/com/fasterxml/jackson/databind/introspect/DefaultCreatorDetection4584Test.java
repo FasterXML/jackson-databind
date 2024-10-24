760cf8f2371bef06ec44a3be200199c0441d74e8
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-10-23 21:37:10 -0700

    Merge some more parts of #4759

--- src/test/java/com/fasterxml/jackson/databind/introspect/DefaultCreatorDetection4584Test.java
+++ src/test/java/com/fasterxml/jackson/databind/introspect/DefaultCreatorDetection4584Test.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Tests for [databind#4584]: extension point for discovering "Default"
 // Creator (primary Creator, usually constructor, used in case no creator

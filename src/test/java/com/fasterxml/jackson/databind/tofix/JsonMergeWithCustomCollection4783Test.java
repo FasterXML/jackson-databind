6289ba397edc377c8944ec0f7e1b4695a3db0a4c
joohyukkim <beanskobe@gmail.com>
2024-11-06 18:15:18 +0900

    Modify test

--- src/test/java/com/fasterxml/jackson/databind/tofix/JsonMergeWithCustomCollection4783Test.java
+++ src/test/java/com/fasterxml/jackson/databind/tofix/JsonMergeWithCustomCollection4783Test.java
@@ -6,63 +6,62 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.annotation.JsonMerge;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.testutil.failure.JacksonTestFailureExpected;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // [databind#4783] Test to verify that JsonMerge also works for custom list
-public class JsonMergeWithCustomCollection4783Test {
+public class JsonMergeWithCustomCollection4783Test
+{
 
-    static class CustomList4783<T> extends ArrayList<T> {
-        static int constructorCount = 0;
-
-        public CustomList4783() {
-            if (constructorCount > 0) {
-                throw new Error("Should only be called once");
-            }
-            ++constructorCount;
-        }
-
-        public static void reset() {
-            constructorCount = 0;
-        }
+    private static class MyArrayListJDK<T> extends ArrayList<T> implements List<T> { }
+    static class MergeListJDK {
+        @JsonMerge
+        @JsonProperty
+        public List<String> values = create();
+        { values.add("a");}
     }
 
-    static class MergeList4783 {
-        @JsonMerge
-        public List<String> values;
+    static <T> List<T> create() {
+        return new MyArrayListJDK<T>();
+    }
 
-        public MergeList4783() {
-            this.values = new CustomList4783<>();
-            this.values.add("a");
-        }
+    interface MyListCustom<T> extends List<T> { }
+    private static class MyArrayListCustom<T> extends ArrayList<T> implements MyListCustom<T> { }
+    static class MergeListCustom {
+        @JsonMerge
+        @JsonProperty
+        public MyListCustom<String> values = createCustom();
+        { values.add("a"); }
+    }
+
+    static <T> MyListCustom<T> createCustom() {
+        return new MyArrayListCustom<T>();
     }
 
     private final ObjectMapper MAPPER = JsonMapper.builder().build();
 
     @Test
-    void testSimpleMergingWithCustomCollection() throws Exception {
-        _verifyMergeList(
-                MAPPER.readerForUpdating(new MergeList4783())
-        );
-        CustomList4783.reset();
-
-        _verifyMergeList(
-                MAPPER.readerFor(MergeList4783.class)
-                        .withValueToUpdate(new MergeList4783())
-        );
-        CustomList4783.reset();
-    }
-
-    private void _verifyMergeList(ObjectReader reader) throws Exception {
-        MergeList4783 result = reader.readValue("{\"values\":[\"x\"]}", MergeList4783.class);
+    void testJDKMapperReading() throws Exception {
+        MergeListJDK result = MAPPER.readValue("{\"values\":[\"x\"]}", MergeListJDK.class);
 
         assertEquals(2, result.values.size());
         assertTrue(result.values.contains("x"));
         assertTrue(result.values.contains("a"));
-        assertTrue(CustomList4783.constructorCount == 1);
     }
+
+    @JacksonTestFailureExpected
+    @Test
+    void testCustomMapperReading() throws Exception {
+        MergeListCustom result = MAPPER.readValue("{\"values\":[\"x\"]}", MergeListCustom.class);
+
+        assertEquals(2, result.values.size());
+        assertTrue(result.values.contains("x"));
+        assertTrue(result.values.contains("a"));
+    }
+
 }

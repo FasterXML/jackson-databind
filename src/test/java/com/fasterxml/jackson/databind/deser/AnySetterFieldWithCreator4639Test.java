2d67b15ed2e724c3bcee0205429537564a2429f0
Kim, Joo Hyuk <beanskobe@gmail.com>
2024-11-09 08:26:53 +0900

    Fix `@JsonAnySetter` issue with "setter" method (related to #4639) (#4790)

--- src/test/java/com/fasterxml/jackson/databind/deser/AnySetterFieldWithCreator4639Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/AnySetterFieldWithCreator4639Test.java
@@ -1,13 +1,15 @@
 package com.fasterxml.jackson.databind.deser;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
-
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,13 +32,49 @@ public class AnySetterFieldWithCreator4639Test
         }
     }
 
+    public static class SomeBean {
+        final int b;
+        final int d;
+        final Map<String, Object> any = new HashMap<>();
+
+        @JsonCreator
+        public SomeBean(@JsonProperty("b") int b, @JsonProperty("d") int d) {
+            this.b = b;
+            this.d = d;
+        }
+
+        @JsonAnySetter
+        public void setAny(String name, Object value) {
+            any.put(name, value);
+        }
+    }
+
+    final ObjectMapper MAPPER = newJsonMapper();
+
     @Test
     public void testJsonAnySetter()
             throws Exception
     {
         String json = "{\"a\":1,\"b\":2,\"c\":3,\"d\":4,\"e\":5,\"f\":6}";
 
-        Bean bean = newJsonMapper().readValue(json, Bean.class);
+        Bean bean = MAPPER.readValue(json, Bean.class);
+        assertEquals(2, bean.b);
+        assertEquals(4, bean.d);
+
+        // failed with:
+        // org.opentest4j.AssertionFailedError:
+        // Expected :{b=2, c=3, e=5, f=6}
+        // Actual   :{e=5, f=6}
+        assertEquals(mapOf("a", 1, "c", 3, "e", 5, "f", 6), bean.any);
+    }
+
+    @Test
+    public void testJsonAnySetterWithField()
+            throws Exception
+    {
+        String json = "{\"a\":1,\"b\":2,\"c\":3,\"d\":4,\"e\":5,\"f\":6}";
+
+        SomeBean bean = MAPPER.readValue(json, SomeBean.class);
         assertEquals(2, bean.b);
         assertEquals(4, bean.d);
 

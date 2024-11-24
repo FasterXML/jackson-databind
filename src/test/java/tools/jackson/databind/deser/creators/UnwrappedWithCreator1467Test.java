474536218138b1739d38e74611dbe45c47b80efa
joohyukkim <beanskobe@gmail.com>
2024-11-24 13:41:37 +0900

    Move test package
    move test classes

--- src/test/java/tools/jackson/databind/deser/creators/UnwrappedWithCreator1467Test.java
+++ src/test/java/tools/jackson/databind/deser/creators/UnwrappedWithCreator1467Test.java
@@ -1,4 +1,4 @@
-package tools.jackson.databind.tofix;
+package tools.jackson.databind.deser.creators;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.*;
 
 import tools.jackson.databind.*;
 import tools.jackson.databind.testutil.DatabindTestUtil;
-import tools.jackson.databind.testutil.failure.JacksonTestFailureExpected;
 
 import static org.junit.jupiter.api.Assertions.*;
 

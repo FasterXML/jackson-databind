757e6393ab640bb61093f377d956dd6c291c484b
joohyukkim <beanskobe@gmail.com>
2024-12-05 23:10:34 +0900

    test : Add failing test wrt #3355

---
+++ src/test/java/com/fasterxml/jackson/databind/tofix/DeserializationWithCreatorPropertyOrder3355Test.java
@@ -0,0 +1,75 @@
+package com.fasterxml.jackson.databind.tofix;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
+import com.fasterxml.jackson.databind.testutil.failure.JacksonTestFailureExpected;
+import org.junit.jupiter.api.Test;
+
+public class DeserializationWithCreatorPropertyOrder3355Test
+        extends DatabindTestUtil {
+
+    public static class Common3355 {
+        private final String property;
+        private final ContainerFail3355 container;
+
+        @JsonCreator
+        public Common3355(@JsonProperty("property") final String property,
+                          @JsonProperty("container") final ContainerFail3355 container) {
+            this.property = property;
+            this.container = container;
+        }
+
+        public String getProperty() {
+            return property;
+        }
+
+        public ContainerFail3355 getContainer() {
+            return container;
+        }
+    }
+
+    public static class ContainerFail3355 {
+        private final Common3355 common;
+
+        @JsonCreator
+        public ContainerFail3355(@JsonProperty("common") final Common3355 common) {
+            this.common = common;
+        }
+
+        @JsonIgnoreProperties("container")
+        public Common3355 getCommon() {
+            return common;
+        }
+    }
+
+    @JacksonTestFailureExpected
+    @Test
+    public void testDeserFailing() throws Exception {
+        final String objectJson = "{ \"property\": \"valueOne\" }";
+        final String containersJson = "{ \"common\": { \"property\": \"valueTwo\" } }";
+
+        final ObjectMapper objectMapper = newJsonMapper();
+
+        // If we deserialize inner object first, outer object FAILS
+        final Common3355 object = objectMapper.readValue(objectJson, Common3355.class);
+        final ContainerFail3355 container = objectMapper.readValue(containersJson, ContainerFail3355.class);
+        System.out.println();
+    }
+
+    @Test
+    public void testDeserPassing() throws Exception {
+        final String objectJson = "{ \"property\": \"valueOne\" }";
+        final String containersJson = "{ \"common\": { \"property\": \"valueTwo\" } }";
+
+        final ObjectMapper objectMapper = newJsonMapper();
+
+        // If we deserialize outer object first, it WORKS
+        final ContainerFail3355 container = objectMapper.readValue(containersJson, ContainerFail3355.class);
+        final Common3355 object = objectMapper.readValue(objectJson, Common3355.class);
+        System.out.println();
+    }
+
+}

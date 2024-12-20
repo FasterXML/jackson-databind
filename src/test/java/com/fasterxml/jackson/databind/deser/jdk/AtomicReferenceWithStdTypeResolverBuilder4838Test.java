de925686e54db60c91e4010934c71f0a796d0a51
joohyukkim <beanskobe@gmail.com>
2024-12-20 21:37:43 +0900

    Modify test to use inclusion as expected

--- src/test/java/com/fasterxml/jackson/databind/deser/jdk/AtomicReferenceWithStdTypeResolverBuilder4838Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/jdk/AtomicReferenceWithStdTypeResolverBuilder4838Test.java
@@ -69,7 +69,7 @@ public class AtomicReferenceWithStdTypeResolverBuilder4838Test
         }
     }
 
-    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.WRAPPER_ARRAY)
     static abstract class Animal2 {
         public String name;
 
@@ -92,18 +92,27 @@ public class AtomicReferenceWithStdTypeResolverBuilder4838Test
 
     @Test
     public void testRoundTrip() throws Exception {
-        _test(new AtomicReference<>("MyName"), String.class);
-        _test(new AtomicReference<>(42), Integer.class);
-        _test(new AtomicReference<>(Pojo86.valueOf("PojoName")), Pojo86.class);
+        _test(new AtomicReference<>("MyName"), String.class,
+                STD_RESOLVER_MAPPER_WRAPPER_OBJECT);
+        _test(new AtomicReference<>(42), Integer.class,
+                STD_RESOLVER_MAPPER_WRAPPER_OBJECT);
+        _test(new AtomicReference<>(Pojo86.valueOf("PojoName")), Pojo86.class,
+                STD_RESOLVER_MAPPER_WRAPPER_OBJECT);
     }
 
     @Test
     public void testPolymorphic() throws Exception {
-        _test(new AtomicReference<>(new Dog("Buddy")), Animal.class);
-        _test(new AtomicReference<>(new Dog2("Buttercup")), Animal2.class);
+        _test(new AtomicReference<>(new Dog("Buddy")), Animal.class,
+                STD_RESOLVER_MAPPER_WRAPPER_OBJECT);
     }
 
-    private final ObjectMapper STD_RESOLVER_MAPPER = jsonMapperBuilder()
+    @Test
+    public void testPolymorphic2() throws Exception {
+        _test(new AtomicReference<>(new Dog2("Buttercup")), Animal2.class,
+                STD_RESOLVER_MAPPER_WRAPPER_ARRAY);
+    }
+
+    private final ObjectMapper STD_RESOLVER_MAPPER_WRAPPER_OBJECT = jsonMapperBuilder()
             // this is what's causing failure in later versions.....
             .setDefaultTyping(
                 new StdTypeResolverBuilder()
@@ -111,15 +120,25 @@ public class AtomicReferenceWithStdTypeResolverBuilder4838Test
                         .inclusion(JsonTypeInfo.As.WRAPPER_OBJECT)
             ).build();
 
-    private <T> void _test(AtomicReference<T> value, Class<?> type) throws Exception {
+    private final ObjectMapper STD_RESOLVER_MAPPER_WRAPPER_ARRAY = jsonMapperBuilder()
+            // this is what's causing failure in later versions.....
+            .setDefaultTyping(
+                    new StdTypeResolverBuilder()
+                            .init(JsonTypeInfo.Id.CLASS, null)
+                            .inclusion(JsonTypeInfo.As.WRAPPER_ARRAY)
+            ).build();
+
+    private <T> void _test(AtomicReference<T> value, Class<?> type, ObjectMapper mapper)
+            throws Exception
+    {
         // Serialize
         Foo<T> foo = new Foo<>();
         foo.value = value;
-        String json = STD_RESOLVER_MAPPER.writeValueAsString(foo);
+        String json = mapper.writeValueAsString(foo);
 
         // Deserialize
-        Foo<T> bean = STD_RESOLVER_MAPPER.readValue(json,
-                STD_RESOLVER_MAPPER.getTypeFactory().constructParametricType(Foo.class, type));
+        Foo<T> bean = mapper.readValue(json,
+                mapper.getTypeFactory().constructParametricType(Foo.class, type));
 
         // Compare the underlying values of AtomicReference
         assertEquals(foo.value.get(), bean.value.get());

702b911be67ecd6edd3491aecba42d7d8763a096
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-12-28 12:20:34 -0800

    Javadoc tweak

--- src/main/java/com/fasterxml/jackson/databind/node/ObjectNode.java
+++ src/main/java/com/fasterxml/jackson/databind/node/ObjectNode.java
@@ -315,7 +315,10 @@ child.getClass().getName(), propName, OverwriteMode.NULLS);
     /**
      * Method to use for accessing all properties (with both names
      * and values) of this JSON Object.
+     *
+     * @deprecated since 2.19 Use instead {@link #properties()}.
      */
+    @Deprecated // since 2.19
     @Override
     public Iterator<Map.Entry<String, JsonNode>> fields() {
         return _children.entrySet().iterator();

309715e8b554a5f9a83753ef1d528e61b8410e5e
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-02-07 21:10:53 -0800

    ...

--- src/test/java/com/fasterxml/jackson/databind/deser/jdk/ThreadGroupDeserTest.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/jdk/ThreadGroupDeserTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ThreadGroupDeserTest extends DatabindTestUtil
@@ -16,5 +17,14 @@ public class ThreadGroupDeserTest extends DatabindTestUtil
     public void deserThreadGroupFromEmpty() throws Exception {
         ThreadGroup tg = MAPPER.readValue("{}", ThreadGroup.class);
         assertNotNull(tg);
+    }
+
+    @Test
+    public void roundtripThreadGroup() throws Exception {
+        ThreadGroup tg = new ThreadGroup("testTG");
+        String json = MAPPER.writeValueAsString(tg);
+        ThreadGroup tg2 = MAPPER.readValue(json, ThreadGroup.class);
+        assertNotNull(tg2);
+        assertEquals(tg.getName(), tg2.getName());
     }
 }

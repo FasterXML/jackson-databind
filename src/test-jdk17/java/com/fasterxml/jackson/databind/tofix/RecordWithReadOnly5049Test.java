95ae8a6c0f1beb3cf7b0c5caf0a5453c17ed8a4c
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-04-01 17:58:05 -0700

    Add POJO test (which passes)

--- src/test-jdk17/java/com/fasterxml/jackson/databind/tofix/RecordWithReadOnly5049Test.java
+++ src/test-jdk17/java/com/fasterxml/jackson/databind/tofix/RecordWithReadOnly5049Test.java
@@ -8,20 +8,51 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class RecordWithReadOnly5049Test extends DatabindTestUtil
 {
-    record ReadOnly5049(
+    record ReadOnly5049Record(
             @JsonProperty(value = "a", access = JsonProperty.Access.READ_ONLY) String a,
             @JsonProperty(value = "b", access = JsonProperty.Access.READ_ONLY) String b) {
+    }
+
+    static class ReadOnly5049Pojo
+    {
+        protected String a, b;
+
+        ReadOnly5049Pojo(
+            @JsonProperty(value = "a", access = JsonProperty.Access.READ_ONLY) String a,
+            @JsonProperty(value = "b", access = JsonProperty.Access.READ_ONLY) String b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public String getA() { return a; }
+        public String getB() { return b; }
     }
 
     private final ObjectMapper MAPPER = newJsonMapper();
 
     @Test
-    void testRoundtrip() throws Exception
+    void testRoundtripRecord() throws Exception
     {
-        String string = MAPPER.writeValueAsString(new ReadOnly5049 ("hello", "world"));
-        assertNotNull(MAPPER.readerFor(ReadOnly5049.class).readValue(string));
+        String json = MAPPER.writeValueAsString(new ReadOnly5049Record("hello", "world"));
+        //System.err.println("JSON/record: "+json);
+        ReadOnly5049Record record = MAPPER.readerFor(ReadOnly5049Record.class).readValue(json);
+        assertNotNull(record);
+        assertNull(record.a());
+        assertNull(record.b());
+    } 
+
+    @Test
+    void testRoundtripPOJO() throws Exception
+    {
+        String json = MAPPER.writeValueAsString(new ReadOnly5049Pojo("hello", "world"));
+        //System.err.println("JSON/pojo: "+json);
+        ReadOnly5049Pojo pojo = MAPPER.readerFor(ReadOnly5049Pojo.class).readValue(json);
+        assertNotNull(pojo);
+        assertNull(pojo.a);
+        assertNull(pojo.b);
     } 
 }

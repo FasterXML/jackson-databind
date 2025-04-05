25e1135e96cd78b775fb6de70beacad3e87d4800
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-04-05 16:28:33 -0700

    Add release notes for #5069

--- src/main/java/com/fasterxml/jackson/databind/MappingIterator.java
+++ src/main/java/com/fasterxml/jackson/databind/MappingIterator.java
@@ -164,7 +164,9 @@ public class MappingIterator<T> implements Iterator<T>, Closeable
 
     /**
      * Copy-constructor that sub-classes can use when creating new instances
-     * by fluent-style construction
+     * by fluent-style construction.
+     *
+     * @since 2.19
      */
     protected MappingIterator(MappingIterator<T> src) {
         this(src._type, src._parser, src._context, src._deserializer, src._closeParser, src._updatedValue);

3d4c0407ddcf4ef9e52eeadd56e666d401e8efbb
wrongwrong <k163377@users.noreply.github.com>
2025-04-06 08:27:03 +0900

    Add copy-constructor for MappingIterator (#5069)

--- src/main/java/com/fasterxml/jackson/databind/MappingIterator.java
+++ src/main/java/com/fasterxml/jackson/databind/MappingIterator.java
@@ -163,6 +163,14 @@ public class MappingIterator<T> implements Iterator<T>, Closeable
     }
 
     /**
+     * Copy-constructor that sub-classes can use when creating new instances
+     * by fluent-style construction
+     */
+    protected MappingIterator(MappingIterator<T> src) {
+        this(src._type, src._parser, src._context, src._deserializer, src._closeParser, src._updatedValue);
+    }
+
+    /**
      * Method for getting an "empty" iterator instance: one that never
      * has more values; may be freely shared.
      *

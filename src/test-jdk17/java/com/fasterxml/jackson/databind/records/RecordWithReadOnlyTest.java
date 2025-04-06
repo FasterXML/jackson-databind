f0ac8466053950a1c90830625d60156e3a86eff0
Fawzi Essam <iifawzie@gmail.com>
2025-04-06 18:10:40 +0200

    fix tests
    Signed-off-by: Fawzi Essam <iifawzie@gmail.com>

--- src/test-jdk17/java/com/fasterxml/jackson/databind/records/RecordWithReadOnlyTest.java
+++ src/test-jdk17/java/com/fasterxml/jackson/databind/records/RecordWithReadOnlyTest.java
@@ -55,6 +55,27 @@ public class RecordWithReadOnlyTest extends DatabindTestUtil
         }
     }
 
+
+    static class ReadOnly5049Pojo
+    {
+        protected String a, b;
+
+        ReadOnly5049Pojo(
+                @JsonProperty(value = "a", access = JsonProperty.Access.READ_ONLY) String a,
+                @JsonProperty(value = "b", access = JsonProperty.Access.READ_ONLY) String b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public String getA() { return a; }
+        public String getB() { return b; }
+    }
+
+    record ReadOnly5049Record(
+            @JsonProperty(value = "a", access = JsonProperty.Access.READ_ONLY) String a,
+            @JsonProperty(value = "b", access = JsonProperty.Access.READ_ONLY) String b) {
+    }
+
     private final ObjectMapper MAPPER = newJsonMapper();
 
     /*
@@ -97,43 +118,23 @@ public class RecordWithReadOnlyTest extends DatabindTestUtil
     }
 
     @Test
-    void testRoundtripRecord() throws Exception
-    {
-        record ReadOnly5049Record(
-                @JsonProperty(value = "a", access = JsonProperty.Access.READ_ONLY) String a,
-                @JsonProperty(value = "b", access = JsonProperty.Access.READ_ONLY) String b
-        ) { }
-
-        String json = MAPPER.writeValueAsString(new ReadOnly5049Record("hello", "world"));
-        ReadOnly5049Record record = MAPPER.readValue(json, ReadOnly5049Record.class);
-        assertNotNull(record);
-        assertNull(record.a());
-        assertNull(record.b());
-    }
-
-    @Test
     void testRoundtripPOJO() throws Exception
     {
-        class ReadOnly5049Pojo
-        {
-            protected String a, b;
-
-            ReadOnly5049Pojo(
-                    @JsonProperty(value = "a", access = JsonProperty.Access.READ_ONLY) String a,
-                    @JsonProperty(value = "b", access = JsonProperty.Access.READ_ONLY) String b) {
-                this.a = a;
-                this.b = b;
-            }
-
-            public String getA() { return a; }
-            public String getB() { return b; }
-        }
-
         String json = MAPPER.writeValueAsString(new ReadOnly5049Pojo("hello", "world"));
         ReadOnly5049Pojo pojo = MAPPER.readerFor(ReadOnly5049Pojo.class).readValue(json);
         assertNotNull(pojo);
         assertNull(pojo.a);
         assertNull(pojo.b);
+    }
+
+    @Test
+    void testRoundtripRecord() throws Exception
+    {
+        String json = MAPPER.writeValueAsString(new ReadOnly5049Record("hello", "world"));
+        ReadOnly5049Record record = MAPPER.readValue(json, ReadOnly5049Record.class);
+        assertNotNull(record);
+        assertNull(record.a());
+        assertNull(record.b());
     }
 
     /*

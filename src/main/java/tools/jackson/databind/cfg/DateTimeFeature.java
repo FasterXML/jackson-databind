0b0756f5f9e7e802264642bf12e4d20d33e47e06
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-04-08 17:47:41 -0700

    Reorder `DateTimeFeature`s alphabetically

--- src/main/java/tools/jackson/databind/cfg/DateTimeFeature.java
+++ src/main/java/tools/jackson/databind/cfg/DateTimeFeature.java
@@ -11,29 +11,6 @@ package tools.jackson.databind.cfg;
 public enum DateTimeFeature implements DatatypeFeature
 {
     /**
-     * Feature that determines whether {@link java.time.ZoneId} is normalized
-     * (via call to {@code java.time.ZoneId#normalized()}) when deserializing
-     * types like {@link java.time.ZonedDateTime}.
-     *<p>
-     * Default setting is enabled, for backwards-compatibility with
-     * Jackson 2.15.
-     */
-    NORMALIZE_DESERIALIZED_ZONE_ID(true),
-
-    /**
-     * Feature that determines whether the {@link java.util.TimeZone} of the
-     * {@link tools.jackson.databind.DeserializationContext} is used
-     * when leniently deserializing {@link java.time.LocalDate} or
-     * {@link java.time.LocalDateTime} from the UTC/ISO instant format.
-     * <p>
-     * Default setting is disabled, for backwards-compatibility with
-     * Jackson 2.18.
-     *
-     * @since 2.19
-     */
-    USE_TIME_ZONE_FOR_LENIENT_DATE_PARSING(false),
-
-    /**
      * Feature that controls whether stringified numbers (Strings that without
      * quotes would be legal JSON Numbers) may be interpreted as
      * timestamps (enabled) or not (disabled), in case where there is an
@@ -47,13 +24,35 @@ public enum DateTimeFeature implements DatatypeFeature
     ALWAYS_ALLOW_STRINGIFIED_DATE_TIMESTAMPS(false),
 
     /**
+     * Feature that determines whether {@link java.time.ZoneId} is normalized
+     * (via call to {@code java.time.ZoneId#normalized()}) when deserializing
+     * types like {@link java.time.ZonedDateTime}.
+     *<p>
+     * Default setting is enabled, for backwards-compatibility with
+     * Jackson 2.15.
+     */
+    NORMALIZE_DESERIALIZED_ZONE_ID(true),
+
+    /**
      * Feature that determines whether {@link java.time.Month} is serialized
      * and deserialized as using a zero-based index (FALSE) or a one-based index (TRUE).
      * For example, "1" would be serialized/deserialized as Month.JANUARY if TRUE and Month.FEBRUARY if FALSE.
      *<p>
      * Default setting is false, meaning that Month is serialized/deserialized as a zero-based index.
      */
-    ONE_BASED_MONTHS(false)
+    ONE_BASED_MONTHS(false),
+
+    /**
+     * Feature that determines whether the {@link java.util.TimeZone} of the
+     * {@link tools.jackson.databind.DeserializationContext} is used
+     * when leniently deserializing {@link java.time.LocalDate} or
+     * {@link java.time.LocalDateTime} from the UTC/ISO instant format.
+     * <p>
+     * Default setting is disabled, for backwards-compatibility with
+     * Jackson 2.18.
+     */
+    USE_TIME_ZONE_FOR_LENIENT_DATE_PARSING(false)
+
     ;
 
     private final static int FEATURE_INDEX = DatatypeFeatures.FEATURE_INDEX_DATETIME;

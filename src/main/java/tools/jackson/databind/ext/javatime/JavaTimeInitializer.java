d693b45aa4b2b4afd956c0ddfa373109b9ad2f95
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-04-08 18:48:06 -0700

    Javadoc improvements

--- src/main/java/tools/jackson/databind/ext/javatime/JavaTimeInitializer.java
+++ src/main/java/tools/jackson/databind/ext/javatime/JavaTimeInitializer.java
@@ -35,13 +35,14 @@ import tools.jackson.databind.module.SimpleSerializers;
 import tools.jackson.databind.ser.std.ToStringSerializer;
 
 /**
- * Class that registers capability of serializing {@code java.time} objects with the Jackson core.
+ * Class that registers capability of deserializing and serializing (reading and writing)
+ * {@code java.time} values with the Jackson core.
  *<p>
  * In Jackson 3, the module is embedded in databind and handlers are automatically
  * registered: approach is similar to one used by full {@link JacksonModule}s.
  *<p>
  * Most {@code java.time} types are serialized as numbers (integers or decimals as appropriate) if the
- * {@link tools.jackson.databind.SerializationFeature#WRITE_DATES_AS_TIMESTAMPS} feature is enabled
+ * {@link SerializationFeature#WRITE_DATES_AS_TIMESTAMPS} feature is enabled
  * (or, for {@link Duration}, {@link tools.jackson.databind.SerializationFeature#WRITE_DURATIONS_AS_TIMESTAMPS}),
  * and otherwise are serialized in standard
  * <a href="http://en.wikipedia.org/wiki/ISO_8601" target="_blank">ISO-8601</a> string representation.
@@ -50,8 +51,8 @@ import tools.jackson.databind.ser.std.ToStringSerializer;
  * have built-in translation to and from ISO-8601 formats.
  * <p>
  * Granularity of timestamps is controlled through the companion features
- * {@link tools.jackson.databind.SerializationFeature#WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS} and
- * {@link tools.jackson.databind.DeserializationFeature#READ_DATE_TIMESTAMPS_AS_NANOSECONDS}. For serialization, timestamps are
+ * {@link SerializationFeature#WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS} and
+ * {@link DeserializationFeature#READ_DATE_TIMESTAMPS_AS_NANOSECONDS}. For serialization, timestamps are
  * written as fractional numbers (decimals), where the number is seconds and the decimal is fractional seconds, if
  * {@code WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS} is enabled (it is by default), with resolution as fine as nanoseconds depending on the
  * underlying JDK implementation. If {@code WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS} is disabled, timestamps are written as a whole number of
@@ -61,13 +62,19 @@ import tools.jackson.databind.ser.std.ToStringSerializer;
  * <p>
  * Some exceptions to this standard serialization/deserialization rule:
  * <ul>
- * <li>{@link Period}, which always results in an ISO-8601 format because Periods must be represented in years, months, and/or days.</li>
- * <li>{@link Year}, which only contains a year and cannot be represented with a timestamp.</li>
- * <li>{@link YearMonth}, which only contains a year and a month and cannot be represented with a timestamp.</li>
- * <li>{@link MonthDay}, which only contains a month and a day and cannot be represented with a timestamp.</li>
- * <li>{@link ZoneId} and {@link ZoneOffset}, which do not actually store dates and times but are supported with this module nonetheless.</li>
+ * <li>{@link Period}, which always results in an ISO-8601 format because Periods must be represented in years, months, and/or days.
+ *  </li>
+ * <li>{@link Year}, which only contains a year and cannot be represented with a timestamp.
+ *  </li>
+ * <li>{@link YearMonth}, which only contains a year and a month and cannot be represented with a timestamp.
+ *  </li>
+ * <li>{@link MonthDay}, which only contains a month and a day and cannot be represented with a timestamp.
+ *  </li>
+ * <li>{@link ZoneId} and {@link ZoneOffset}, which do not actually store dates and times but are supported with this module nonetheless.
+ *  </li>
  * <li>{@link LocalDate}, {@link LocalTime}, {@link LocalDateTime}, and {@link OffsetTime}, which cannot portably be converted to timestamps
- * and are instead represented as arrays when WRITE_DATES_AS_TIMESTAMPS is enabled.</li>
+ * and are instead represented as arrays when WRITE_DATES_AS_TIMESTAMPS is enabled.
+ *  </li>
  * </ul>
  */
 public final class JavaTimeInitializer

c41b4af9a4bcaf96206cf6e59fc180da9e87ca58
wrongwrong <boranti1995@gmail.com>
2025-05-03 23:55:57 +0900

    Add test cases
    for https://github.com/FasterXML/jackson-databind/issues/5139

---
+++ src/test/java/com/fasterxml/jackson/databind/deser/std/CollectionDeserializer5139Test.java
@@ -0,0 +1,49 @@
+package com.fasterxml.jackson.databind.deser.std;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidNullException;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollectionDeserializer5139Test {
+    static class Dst {
+        private List<Integer> list;
+
+        public List<Integer> getList() {
+            return list;
+        }
+
+        public void setList(List<Integer> list) {
+            this.list = list;
+        }
+    }
+
+    @Test
+    public void nullsFailTest() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configOverride(List.class).setSetterInfo(JsonSetter.Value.forContentNulls(Nulls.FAIL));
+
+        assertThrows(
+                InvalidNullException.class,
+                () -> mapper.readValue("{\"list\":[\"\"]}", new TypeReference<Dst>(){})
+        );
+    }
+
+    @Test
+    public void nullsSkipTest() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configOverride(List.class).setSetterInfo(JsonSetter.Value.forContentNulls(Nulls.SKIP));
+
+        Dst dst = mapper.readValue("{\"list\":[\"\"]}", new TypeReference<>() {});
+
+        assertTrue(dst.getList().isEmpty());
+    }
+}

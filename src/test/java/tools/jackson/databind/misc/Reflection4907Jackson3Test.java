9531d02f65439e9a91dfcbf66f4af3c81c2ae3fc
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-05-21 15:47:52 -0700

    Minor addition to test

--- src/test/java/tools/jackson/databind/misc/Reflection4907Jackson3Test.java
+++ src/test/java/tools/jackson/databind/misc/Reflection4907Jackson3Test.java
@@ -7,11 +7,15 @@ import tools.jackson.databind.testutil.DatabindTestUtil;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Reflection4907Jackson3Test extends DatabindTestUtil
 {
     static class SqlDatePojo {
         public String name;
         public java.sql.Date date;
+        public List<String> tags;
 
         public SqlDatePojo() {
         }
@@ -32,6 +36,10 @@ public class Reflection4907Jackson3Test extends DatabindTestUtil
         public void setDate(java.sql.Date date) {
             this.date = date;
         }
+
+        public List<String> getTags() {
+            return tags;
+        }
     }
 
     private final ObjectMapper MAPPER = newJsonMapper();
@@ -39,23 +47,41 @@ public class Reflection4907Jackson3Test extends DatabindTestUtil
     /*
     @Test
     public void test4907ReadPojo() throws Exception {
-System.err.println("<testRead>");
+System.err.println("<testReadPojo>");
         SqlDatePojo pojo = MAPPER.readValue(a2q("{'date':'2000-01-01', 'name':'foo'}"),
                 SqlDatePojo.class);
-System.err.println("</testRead>");
+System.err.println("</testReadPojo>");
         assertNotNull(pojo);
     }
 
     @Test
     public void test4907WritePojo() throws Exception {
-System.err.println("<testWrite>");
+System.err.println("<testWritePojo>");
          String json = MAPPER.writeValueAsString(new SqlDatePojo("foobar",
                  java.sql.Date.valueOf("2000-01-01")));
- System.err.println("</testWrite>");
+ System.err.println("</testWritePojo>");
          assertNotNull(json);
     }
     */
 
+    @Test
+    public void test4907ReadTags() throws Exception {
+System.err.println("<testReadTags>");
+        List<?> tags = MAPPER.readValue(a2q("['abc', 'def']"),
+                List.class);
+System.err.println("</testReadTags>");
+        assertNotNull(tags);
+    }
+
+    @Test
+    public void test4907WriteTags() throws Exception {
+System.err.println("<testWriteTags>");
+         String json = MAPPER.writeValueAsString(Arrays.asList("abc", "def"));
+ System.err.println("</testWriteTags>");
+         assertNotNull(json);
+    }
+
+    /*
     @Test
     public void test4907ReadDate() throws Exception {
 System.err.println("<testReadDate>");
@@ -72,4 +98,5 @@ System.err.println("<testWriteDate>");
  System.err.println("</testWriteDate>");
          assertNotNull(json);
     }
+    */
 }

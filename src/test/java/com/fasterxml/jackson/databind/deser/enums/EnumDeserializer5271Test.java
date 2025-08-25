676c3c236c95cca44887c10adce1a3cbba9e40f1
Kim, Joo Hyuk <beanskobe@gmail.com>
2025-08-26 04:01:02 +0900

    Fix 5271

--- src/test/java/com/fasterxml/jackson/databind/deser/enums/EnumDeserializer5271Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/enums/EnumDeserializer5271Test.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.databind.testutil.DatabindTestUtil;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class EnumDeserializer5271Test
-        extends DatabindTestUtil
 {
     public enum MyEnum {
         T10("10%"), T20("20%"), T30("30%");
@@ -29,11 +27,10 @@ public class EnumDeserializer5271Test
 
     @Test
     void convertStringToEnum() {
-        _testConvert(
-                jsonMapperBuilder().disable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
+        _testConvert(JsonMapper.builder().disable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                         .build()
         );
-        _testConvert(jsonMapperBuilder().enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
+        _testConvert(JsonMapper.builder().enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                 .build()
         );
     }

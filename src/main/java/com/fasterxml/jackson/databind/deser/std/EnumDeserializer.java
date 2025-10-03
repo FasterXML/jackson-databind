a9466e035f88662817c7120956e7f8373d3d8518
Kim, Joo Hyuk <beanskobe@gmail.com>
2025-10-04 02:09:18 +0900

    Revert "Fix NPE"
    This reverts commit ac68a08afdc4b3a38cba6aa1d45dcabe623de311.

--- src/main/java/com/fasterxml/jackson/databind/deser/std/EnumDeserializer.java
+++ src/main/java/com/fasterxml/jackson/databind/deser/std/EnumDeserializer.java
@@ -338,9 +338,6 @@ public class EnumDeserializer
         throws IOException
     {
         CompactStringObjectMap lookup = _resolveCurrentLookup(ctxt);
-        if (lookup == null) {
-            return null;
-        }
         Object result = lookup.find(text);
         if (result == null) {
             String trimmed = text.trim();

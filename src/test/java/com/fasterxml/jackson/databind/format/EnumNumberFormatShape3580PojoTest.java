3176f8a1130320418abc620312abbc5ec4f79173
Kim, Joo Hyuk <beanskobe@gmail.com>
2025-11-03 23:56:26 +0900

    Move test file to earlier jdk version

--- src/test/java/com/fasterxml/jackson/databind/format/EnumNumberFormatShape3580PojoTest.java
+++ src/test/java/com/fasterxml/jackson/databind/format/EnumNumberFormatShape3580PojoTest.java
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.databind.records;
+package com.fasterxml.jackson.databind.format;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonValue;

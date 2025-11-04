de8d9db1f6c18a7ce8483412ec775be7184727e4
Severin Kistler <kistlerseverin@gmail.com>
2025-11-04 09:17:46 +0100

    adapt test for reproduction of #3580 to actually do coercion of enum value from String to int

--- src/test-jdk17/java/com/fasterxml/jackson/databind/records/EnumNumberFormatShapeRecord3580Test.java
+++ src/test-jdk17/java/com/fasterxml/jackson/databind/records/EnumNumberFormatShapeRecord3580Test.java
@@ -16,22 +16,45 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class EnumNumberFormatShapeRecord3580Test
         extends DatabindTestUtil
 {
+    public record RecordString3580(RecordState3580 state) {}
+
     public record RecordNumber3580(@JsonFormat(shape = JsonFormat.Shape.NUMBER) RecordState3580 state) {}
 
     public record RecordNumberInt3580(@JsonFormat(shape = JsonFormat.Shape.NUMBER_INT) RecordState3580 state) {}
 
     public enum RecordState3580 {
-        OFF(17),
-        ON(31),
-        UNKNOWN(99);
+        OFF("17"),
+        ON("31"),
+        UNKNOWN("99");
 
-        private int value;
+        private String value;
 
-        RecordState3580(int value) { this.value = value; }
+        RecordState3580(String value) { this.value = value; }
 
         @JsonValue
-        public int value() {return this.value;}
+        public String value() {return this.value;}
     }
+
+    @Test
+    public void testEnumStringFormatShapeRecord3580()
+            throws Exception
+    {
+        ObjectMapper mapper = JsonMapper.builder().build();
+
+        // Serialize
+        assertEquals("{\"state\":\"17\"}", mapper.writeValueAsString(new RecordString3580(RecordState3580.OFF))); //
+        assertEquals("{\"state\":\"31\"}", mapper.writeValueAsString(new RecordString3580(RecordState3580.ON))); //
+        assertEquals("{\"state\":\"99\"}", mapper.writeValueAsString(new RecordString3580(RecordState3580.UNKNOWN))); //
+
+        // Pass Deserialize
+        assertEquals(RecordState3580.OFF, mapper.readValue("{\"state\":\"17\"}", RecordString3580.class).state); // Pojo[state=OFF]
+        assertEquals(RecordState3580.ON, mapper.readValue("{\"state\":\"31\"}", RecordString3580.class).state); // Pojo[state=OFF]
+        assertEquals(RecordState3580.UNKNOWN, mapper.readValue("{\"state\":\"99\"}", RecordString3580.class).state); // Pojo[state=OFF]
+
+        // Fail : Try to use ordinal number
+        assertThrows(MismatchedInputException.class, () -> mapper.readValue("{\"state\":0}", RecordString3580.class));
+    }
+
 
     @Test
     public void testEnumNumberFormatShapeRecord3580()

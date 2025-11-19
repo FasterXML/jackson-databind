9f8a82590fb12781235a42111cb1a881fa5f16e5
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-11-18 17:14:30 -0800

    ...

--- src/main/java/tools/jackson/databind/ser/BeanPropertyWriter.java
+++ src/main/java/tools/jackson/databind/ser/BeanPropertyWriter.java
@@ -4,7 +4,6 @@ import java.lang.annotation.Annotation;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 
 import com.fasterxml.jackson.annotation.JsonInclude;

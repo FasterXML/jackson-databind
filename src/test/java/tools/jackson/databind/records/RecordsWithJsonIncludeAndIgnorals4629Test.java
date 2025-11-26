59a6b563b9ed61cb855526606e0a753391723fb0
Jackson <hyo961015@naver.com>
2025-11-27 00:30:33 +0900

    Move test class out of tofix package for #4629

--- src/test/java/tools/jackson/databind/records/RecordsWithJsonIncludeAndIgnorals4629Test.java
+++ src/test/java/tools/jackson/databind/records/RecordsWithJsonIncludeAndIgnorals4629Test.java
@@ -1,4 +1,4 @@
-package tools.jackson.databind.records.tofix;
+package tools.jackson.databind.records;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.testutil.DatabindTestUtil;
-import tools.jackson.databind.testutil.failure.JacksonTestFailureExpected;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 

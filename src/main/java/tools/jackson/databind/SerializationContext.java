dfc0978c0835dc17ecdcaf1a41e760d4df1f6493
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-12-12 19:23:45 -0800

    Add shape-shifting-check to the other code path.

--- src/main/java/tools/jackson/databind/SerializationContext.java
+++ src/main/java/tools/jackson/databind/SerializationContext.java
@@ -709,13 +709,25 @@ public abstract class SerializationContext
     public ValueSerializer<Object> findPrimaryPropertySerializer(Class<?> rawType,
             BeanProperty property)
     {
+        boolean checkShape = (property != null);
+        JavaType fullType = null;
+
         ValueSerializer<Object> ser = _knownSerializers.untypedValueSerializer(rawType);
         if (ser == null) {
-            JavaType fullType = _config.constructType(rawType);
+            fullType = _config.constructType(rawType);
             ser = _serializerCache.untypedValueSerializer(fullType);
             if (ser == null) {
+                checkShape = false; // because next call does it
                 ser = _createAndCachePropertySerializer(rawType, fullType, property);
             }
+        }
+        if (checkShape) {
+            if (fullType == null) {
+                fullType = _config.constructType(rawType);
+            }
+            BeanDescription.Supplier beanDescRef = lazyIntrospectBeanDescription(fullType);
+            // [databind#5405]: property-level @JsonFormat must be honored even with cached serializers
+            ser = _checkShapeShifting(fullType, beanDescRef, property, ser);
         }
         return handlePrimaryContextualization(ser, property);
     }

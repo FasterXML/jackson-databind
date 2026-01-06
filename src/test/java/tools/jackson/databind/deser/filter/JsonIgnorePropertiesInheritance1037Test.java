72d3ebd75a04dc47da41e48b2c2fedeea7de3119
Jackson <hyo961015@naver.com>
2026-01-06 19:35:08 +0900

    Add #1037: Test-case

---
+++ src/test/java/tools/jackson/databind/deser/filter/JsonIgnorePropertiesInheritance1037Test.java
@@ -0,0 +1,40 @@
+package tools.jackson.databind.deser.filter;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.testutil.DatabindTestUtil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// https://github.com/FasterXML/jackson-databind/issues/1037
+public class JsonIgnorePropertiesInheritance1037Test
+    extends DatabindTestUtil
+{
+    @JsonIgnoreProperties(value = {"generated"}, allowGetters = true)
+    static class BaseBean1037 {
+        public String getGenerated() {
+            return "http://bar.com";
+        }
+    }
+
+    @JsonIgnoreProperties(value = {"computed"}, allowGetters = true)
+    static class ReadOnlyBean1037 extends BaseBean1037 {
+        public int getComputed() {
+            return 32;
+        }
+    }
+
+    @Test
+    public void testReadOnlyProp() throws Exception
+    {
+        ObjectMapper m = newJsonMapper();
+        String json = m.writeValueAsString(new ReadOnlyBean1037());
+        assertTrue(json.contains("generated"));
+        assertTrue(json.contains("computed"));
+        ReadOnlyBean1037 bean = m.readValue(json, ReadOnlyBean1037.class);
+        assertNotNull(bean);
+    }
+}

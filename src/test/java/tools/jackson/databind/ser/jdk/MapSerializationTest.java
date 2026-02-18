995966dcf9a335ed286c04d4a3701d6ed76c27b8
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-02-18 10:52:35 -0800

    Tiny bit of unfactoring (split 1 test class in 2)

--- src/test/java/tools/jackson/databind/ser/jdk/MapSerializationTest.java
+++ src/test/java/tools/jackson/databind/ser/jdk/MapSerializationTest.java
@@ -64,39 +64,6 @@ public class MapSerializationTest extends DatabindTestUtil
         }
     }
 
-    // [databind#565]: Support ser/deser of Map.Entry
-    static class StringIntMapEntry implements Map.Entry<String,Integer> {
-        public final String k;
-        public final Integer v;
-        public StringIntMapEntry(String k, Integer v) {
-            this.k = k;
-            this.v = v;
-        }
-
-        @Override
-        public String getKey() {
-            return k;
-        }
-
-        @Override
-        public Integer getValue() {
-            return v;
-        }
-
-        @Override
-        public Integer setValue(Integer value) {
-            throw new UnsupportedOperationException();
-        }
-    }
-
-    static class StringIntMapEntryWrapper {
-        public StringIntMapEntry value;
-
-        public StringIntMapEntryWrapper(String k, Integer v) {
-            value = new StringIntMapEntry(k, v);
-        }
-    }
-
     // for [databind#691]
     @JsonTypeInfo(use=JsonTypeInfo.Id.NAME)
     @JsonTypeName("mymap")
@@ -407,36 +374,6 @@ public class MapSerializationTest extends DatabindTestUtil
         MapOrderingBean input = new MapOrderingBean("c", "b", "a");
         String json = MAPPER.writeValueAsString(input);
         assertEquals(a2q("{'map':{'a':3,'b':2,'c':1}}"), json);
-    }
-
-    // [Databind#565]
-    @Test
-    public void testMapEntry() throws IOException
-    {
-        StringIntMapEntry input = new StringIntMapEntry("answer", 42);
-        String json = MAPPER.writeValueAsString(input);
-        assertEquals(a2q("{'answer':42}"), json);
-
-        StringIntMapEntry[] array = new StringIntMapEntry[] { input };
-        json = MAPPER.writeValueAsString(array);
-        assertEquals(a2q("[{'answer':42}]"), json);
-
-        // and maybe with bit of extra typing?
-        ObjectMapper mapper = jsonMapperBuilder()
-                .activateDefaultTyping(NoCheckSubTypeValidator.instance,
-                        DefaultTyping.NON_FINAL)
-                .build();
-        json = mapper.writeValueAsString(input);
-        assertEquals(a2q("['"+StringIntMapEntry.class.getName()+"',{'answer':42}]"),
-                json);
-    }
-
-    @Test
-    public void testMapEntryWrapper() throws IOException
-    {
-        StringIntMapEntryWrapper input = new StringIntMapEntryWrapper("answer", 42);
-        String json = MAPPER.writeValueAsString(input);
-        assertEquals(a2q("{'value':{'answer':42}}"), json);
     }
 
     // [databind#691]

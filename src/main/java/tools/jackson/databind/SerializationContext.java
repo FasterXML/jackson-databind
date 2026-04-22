1fc56d3cf453ed1762cb6261af1a14e2753ed34f
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-04-22 11:07:42 -0700

    Add `SerializationContext.withActiveView()`

--- src/main/java/tools/jackson/databind/SerializationContext.java
+++ src/main/java/tools/jackson/databind/SerializationContext.java
@@ -105,8 +105,10 @@ public abstract class SerializationContext
 
     /**
      * View used for currently active serialization, if any.
+     *<p>
+     * NOTE: non-{@code final} since 3.2
      */
-    protected final Class<?> _activeView;
+    protected Class<?> _activeView;
 
     /*
     /**********************************************************************
@@ -224,6 +226,33 @@ public abstract class SerializationContext
         _attributes = src._attributes;
 
         _knownSerializers = src._knownSerializers;
+    }
+
+    /*
+    /**********************************************************************
+    /* State-changing
+    /**********************************************************************
+     */
+
+    /**
+     * Method for overriding active view during processing of given
+     * {@code callback} (and reverting to currently active view once
+     * that is complete)
+     *
+     * @param viewToApply Active view to use during {@code callback} execution
+     * @param callback Callback to call for processing with overridden view
+     *
+     * @since 3.2
+     */
+    public void withActiveView(Class<?> viewToApply, Runnable callback)
+    {
+        final Class<?> currentView = _activeView;
+        _activeView = viewToApply;
+        try {
+            callback.run();
+        } finally {
+            _activeView = currentView;
+        }
     }
 
     /*

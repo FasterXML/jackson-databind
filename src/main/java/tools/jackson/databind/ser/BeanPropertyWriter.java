aa88b49fe0835207a876717ff86d0d62133d6461
PJ Fanning <pjfanning@users.noreply.github.com>
2026-07-25 20:54:38 +0100

    remove code that throws exception that blocks us
    Update BeanPropertyWriter.java

--- src/main/java/tools/jackson/databind/ser/BeanPropertyWriter.java
+++ src/main/java/tools/jackson/databind/ser/BeanPropertyWriter.java
@@ -394,9 +394,6 @@ public class BeanPropertyWriter
      * Overridable factory method used by sub-classes
      */
     protected BeanPropertyWriter _new(PropertyName newName) {
-        if (getClass() != BeanPropertyWriter.class) {
-            throw new IllegalStateException("Method must be overridden by "+getClass());
-        }
         return new BeanPropertyWriter(this, newName);
     }
 
